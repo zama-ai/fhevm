7b85a6e8af449443bf6c1c005abbeeb2e93ca79d
Levent Demir <levent.demir@zama.ai>
2023-06-14 23:45:34 +0200

    chore(ci): fix path to evmos

--- run_local_test_from_evmos.sh
+++ run_local_test_from_evmos.sh
@@ -9,6 +9,6 @@ key=$1
 mkdir -p res/ct
 mkdir -p keys
 
-PRIVATE_KEY=$(docker compose -f ../evmos/docker-compose/docker-compose.local.yml exec evmosnodelocal evmosd --home /root/.evmosd keys unsafe-export-eth-key $key --keyring-backend test)
+PRIVATE_KEY=$(docker compose -f ../../docker-compose/docker-compose.local.yml exec evmosnodelocal evmosd --home /root/.evmosd keys unsafe-export-eth-key $key --keyring-backend test)
 echo "Get address from private key: $PRIVATE_KEY"
 docker compose -f docker-compose.yml run app python demo_test_high_level_fhe_tool.py $PRIVATE_KEY
