095360c937defa65fde10617f6d4099b82cd9b2a
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2023-06-27 11:10:19 +0200

    Update EncryptedERC20.sol (#57)
    * Update EncryptedERC20.sol
    
    Remove the comment indicating that a user's balance is re-encrypted under its FHE public key, which is automatically determined (this is not true anymore).
    
    * Update EncryptedERC20.sol

--- examples/EncryptedERC20.sol
+++ examples/EncryptedERC20.sol
@@ -58,8 +58,7 @@ contract EncryptedERC20 is EIP712WithModifier {
         return TFHE.reencrypt(totalSupply, publicKey);
     }
 
-    // Returns the balance of the caller under their public FHE key.
-    // The FHE public key is automatically determined based on the origin of the call.
+    // Returns the balance of the caller encrypted under the provided public key.
     function balanceOf(
         bytes32 publicKey,
         bytes calldata signature
