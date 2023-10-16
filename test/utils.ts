449650652ea57309b971aaa27a5dbc88c0a4af87
Clément 'birdy' Danjou <clement@danjou.io>
2023-10-16 16:09:08 +0200

    test() gas can't be above 10000000

--- test/utils.ts
+++ test/utils.ts
@@ -37,6 +37,9 @@ export const createTransaction = async <A extends [...{ [I in keyof A]-?: A[I] |
   ...params: A
 ) => {
   const gasLimit = await method.estimateGas(...params);
-  const updatedParams: ContractMethodArgs<A> = [...params, { gasLimit: Math.round(+gasLimit.toString() * 1.2) }];
+  const updatedParams: ContractMethodArgs<A> = [
+    ...params,
+    { gasLimit: Math.min(Math.round(+gasLimit.toString() * 1.2), 10000000) },
+  ];
   return method(...updatedParams);
 };
