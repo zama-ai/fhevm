297c5a563f7e79b57f7d7398dbee063e26335133
Joseph-André Turk <josephandre.turk@zama.ai>
2024-01-11 16:59:24 +0100

    Added name and symbol parameters in constructor of EncryptedERC20 and CompliantERC20

--- test/encryptedERC20/EncryptedERC20.fixture.ts
+++ test/encryptedERC20/EncryptedERC20.fixture.ts
@@ -7,7 +7,7 @@ export async function deployEncryptedERC20Fixture(): Promise<EncryptedERC20> {
   const signers = await getSigners();
 
   const contractFactory = await ethers.getContractFactory('EncryptedERC20');
-  const contract = await contractFactory.connect(signers.alice).deploy();
+  const contract = await contractFactory.connect(signers.alice).deploy('Naraggara', 'NARA'); // City of Zama's battle
   await contract.waitForDeployment();
 
   return contract;
--- test/identity/compliantERC20.fixture.ts
+++ test/identity/compliantERC20.fixture.ts
@@ -10,7 +10,9 @@ export async function deployCompliantERC20Fixture(
   const signers = await getSigners();
 
   const contractFactory = await ethers.getContractFactory('CompliantERC20');
-  const contract = await contractFactory.connect(signers.alice).deploy(identityAddress, erc20RulesAddress);
+  const contract = await contractFactory
+    .connect(signers.alice)
+    .deploy(identityAddress, erc20RulesAddress, 'CompliantToken', 'CTOK');
   await contract.waitForDeployment();
 
   return contract;
