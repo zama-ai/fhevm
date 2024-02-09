ecb915096a8f7bfd02a89b5576f9e81a613f15ee
Clément 'birdy' Danjou <clement@danjou.io>
2024-02-09 16:18:35 +0100

    fix: mock param conversion was not working correctly due to conversion to Uint8Array

--- test/instance.ts
+++ test/instance.ts
@@ -1,3 +1,4 @@
+import { toBufferBE } from 'bigint-buffer';
 import { Signer } from 'ethers';
 import fhevmjs, { FhevmInstance } from 'fhevmjs';
 import { ethers as hethers } from 'hardhat';
@@ -6,9 +7,9 @@ import { FHE_LIB_ADDRESS } from './generated';
 import type { Signers } from './signers';
 import { FhevmInstances } from './types';
 
-const network = process.env.HARDHAT_NETWORK;
+const HARDHAT_NETWORK = process.env.HARDHAT_NETWORK;
 
-let publicKey: string;
+let publicKey: string | undefined;
 let chainId: number;
 
 export const createInstances = async (
@@ -32,13 +33,12 @@ export const createInstances = async (
 };
 
 export const createInstance = async (contractAddress: string, account: Signer, ethers: typeof hethers) => {
-  if (network !== 'hardhat' && (!publicKey || !chainId)) {
-    // 1. Get chain id
-    const provider = ethers.provider;
+  // 1. Get chain id
+  const provider = ethers.provider;
 
-    const network = await provider.getNetwork();
-    chainId = +network.chainId.toString(); // Need to be a number
-
+  const network = await provider.getNetwork();
+  chainId = +network.chainId.toString(); // Need to be a number
+  try {
     // Get blockchain public key
     const ret = await provider.call({
       to: FHE_LIB_ADDRESS,
@@ -47,18 +47,18 @@ export const createInstance = async (contractAddress: string, account: Signer, e
     });
     const decoded = ethers.AbiCoder.defaultAbiCoder().decode(['bytes'], ret);
     publicKey = decoded[0];
+  } catch (e) {
+    publicKey = undefined;
   }
-  let instance = {} as FhevmInstance; // 31337 is hardhat node's default chainId
 
-  if (network === 'hardhat') {
+  const instance = await fhevmjs.createInstance({ chainId, publicKey });
+
+  if (HARDHAT_NETWORK === 'hardhat') {
     instance.encrypt8 = createUintToUint8ArrayFunction(8);
     instance.encrypt16 = createUintToUint8ArrayFunction(16);
     instance.encrypt32 = createUintToUint8ArrayFunction(32);
     instance.encrypt64 = createUintToUint8ArrayFunction(64);
-    instance.decrypt = (_, hexadecimalString) => Number(BigInt(hexadecimalString));
-    instance.generatePublicKey = () => ({ eip712: {} as any, publicKey: new Uint8Array() });
-  } else {
-    instance = await fhevmjs.createInstance({ chainId, publicKey });
+    instance.decrypt = (_, hexadecimalString) => BigInt(hexadecimalString);
   }
   await generatePublicKey(contractAddress, account, instance);
 
@@ -81,25 +81,8 @@ const generatePublicKey = async (contractAddress: string, signer: Signer, instan
 
 function createUintToUint8ArrayFunction(numBits: number) {
   const numBytes = Math.ceil(numBits / 8);
-  const maxValue = 2 ** numBits;
-  const totalBytes = 32; // Total size of the array is always 32 bytes
-
-  return function (uint: number) {
-    // Applying modulus operation to ensure the value fits in numBits
-    const value = uint % maxValue;
-
-    // Create a buffer of 32 bytes
-    const buffer = new ArrayBuffer(totalBytes);
-    const view = new DataView(buffer);
-
-    // Fill the relevant part of the buffer byte by byte
-    for (let i = 0; i < numBytes; i++) {
-      // Calculate the value for each byte and set it in the last numBytes of the buffer
-      const byteValue = (value >> (8 * (numBytes - i - 1))) & 0xff;
-      view.setUint8(totalBytes - numBytes + i, byteValue);
-    }
-
-    // Return the buffer as a Uint8Array
-    return new Uint8Array(buffer);
+  return function (uint: number | bigint) {
+    const buffer = toBufferBE(BigInt(uint), numBytes);
+    return buffer;
   };
 }
