8fe58dc012b6a2ba1b694932a21505cb50d5406b
Joseph-André Turk <josephandre.turk@zama.ai>
2024-04-12 00:03:40 +0200

    feat: new async-decrypt API

--- oracle/lib/Oracle.sol
+++ oracle/lib/Oracle.sol
@@ -12,66 +12,47 @@ library Oracle {
         return ORACLE_CONTRACT_PREDEPLOY_ADDRESS;
     }
 
-    function requestDecryption(
-        ebool[] memory ct,
-        bytes4 callbackSelector,
-        uint256 msgValue,
-        uint256 maxTimestamp
-    ) internal returns (uint256 initialCounter) {
-        initialCounter = oraclePredeploy.requestDecryptionEBool(ct, callbackSelector, msgValue, maxTimestamp);
+    function toCiphertext(ebool newCT) internal returns (Ciphertext memory ct) {
+        oraclePredeploy.approveEBool(newCT); // to make sure that ciphertext is honestly obtained
+        ct = Ciphertext({ctHandle: ebool.unwrap(newCT), ctType: CiphertextType.EBOOL});
+    }
+
+    function toCiphertext(euint4 newCT) internal returns (Ciphertext memory ct) {
+        oraclePredeploy.approveEUint4(newCT); // to make sure that ciphertext is honestly obtained
+        ct = Ciphertext({ctHandle: euint4.unwrap(newCT), ctType: CiphertextType.EUINT4});
+    }
+
+    function toCiphertext(euint8 newCT) internal returns (Ciphertext memory ct) {
+        oraclePredeploy.approveEUint8(newCT); // to make sure that ciphertext is honestly obtained
+        ct = Ciphertext({ctHandle: euint8.unwrap(newCT), ctType: CiphertextType.EUINT8});
+    }
+
+    function toCiphertext(euint16 newCT) internal returns (Ciphertext memory ct) {
+        oraclePredeploy.approveEUint16(newCT); // to make sure that ciphertext is honestly obtained
+        ct = Ciphertext({ctHandle: euint16.unwrap(newCT), ctType: CiphertextType.EUINT16});
+    }
+
+    function toCiphertext(euint32 newCT) internal returns (Ciphertext memory ct) {
+        oraclePredeploy.approveEUint32(newCT); // to make sure that ciphertext is honestly obtained
+        ct = Ciphertext({ctHandle: euint32.unwrap(newCT), ctType: CiphertextType.EUINT32});
+    }
+
+    function toCiphertext(euint64 newCT) internal returns (Ciphertext memory ct) {
+        oraclePredeploy.approveEUint64(newCT); // to make sure that ciphertext is honestly obtained
+        ct = Ciphertext({ctHandle: euint64.unwrap(newCT), ctType: CiphertextType.EUINT64});
+    }
+
+    function toCiphertext(eaddress newCT) internal returns (Ciphertext memory ct) {
+        oraclePredeploy.approveEAddress(newCT); // to make sure that ciphertext is honestly obtained
+        ct = Ciphertext({ctHandle: eaddress.unwrap(newCT), ctType: CiphertextType.EADDRESS});
     }
 
     function requestDecryption(
-        euint4[] memory ct,
+        Ciphertext[] memory cts,
         bytes4 callbackSelector,
         uint256 msgValue,
         uint256 maxTimestamp
-    ) internal returns (uint256 initialCounter) {
-        initialCounter = oraclePredeploy.requestDecryptionEUint4(ct, callbackSelector, msgValue, maxTimestamp);
-    }
-
-    function requestDecryption(
-        euint8[] memory ct,
-        bytes4 callbackSelector,
-        uint256 msgValue,
-        uint256 maxTimestamp
-    ) internal returns (uint256 initialCounter) {
-        initialCounter = oraclePredeploy.requestDecryptionEUint8(ct, callbackSelector, msgValue, maxTimestamp);
-    }
-
-    function requestDecryption(
-        euint16[] memory ct,
-        bytes4 callbackSelector,
-        uint256 msgValue,
-        uint256 maxTimestamp
-    ) internal returns (uint256 initialCounter) {
-        initialCounter = oraclePredeploy.requestDecryptionEUint16(ct, callbackSelector, msgValue, maxTimestamp);
-    }
-
-    function requestDecryption(
-        euint32[] memory ct,
-        bytes4 callbackSelector,
-        uint256 msgValue,
-        uint256 maxTimestamp
-    ) internal returns (uint256 initialCounter) {
-        initialCounter = oraclePredeploy.requestDecryptionEUint32(ct, callbackSelector, msgValue, maxTimestamp);
-    }
-
-    function requestDecryption(
-        euint64[] memory ct,
-        bytes4 callbackSelector,
-        uint256 msgValue,
-        uint256 maxTimestamp
-    ) internal returns (uint256 initialCounter) {
-        initialCounter = oraclePredeploy.requestDecryptionEUint64(ct, callbackSelector, msgValue, maxTimestamp);
-    }
-
-    function requestDecryption(
-        eaddress[] memory ct,
-        bytes4 callbackSelector,
-        uint256 msgValue,
-        uint256 maxTimestamp
-    ) internal returns (uint256 initialCounter) {
-        initialCounter = oraclePredeploy.requestDecryptionAddress(ct, callbackSelector, msgValue, maxTimestamp);
+    ) internal returns (uint256 requestID) {
+        requestID = oraclePredeploy.requestDecryption(cts, callbackSelector, msgValue, maxTimestamp);
     }
 }
