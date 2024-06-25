608c18cddee7f0f5860ce409c9e0da2ddda1fafe
Joseph-André Turk <josephandre.turk@zama.ai>
2024-06-25 11:46:53 +0200

    fix: setup script order
    chore: hardhat clean

--- setup-local-fhevm.sh
+++ setup-local-fhevm.sh
@@ -3,6 +3,9 @@
 # Assumes the following:
 # 1. A local and **fresh** fhEVM node is already running.
 # 2. All test addresses are funded (e.g. via the fund_test_addresses.sh script).
+npx hardhat clean
+npx hardhat compile:specific --contract lib
+npx hardhat compile:specific --contract gateway
 
 PRIVATE_KEY_GATEWAY_DEPLOYER=$(grep PRIVATE_KEY_GATEWAY_DEPLOYER .env | cut -d '"' -f 2)
 npx hardhat task:computePredeployAddress --private-key "$PRIVATE_KEY_GATEWAY_DEPLOYER"
@@ -13,8 +16,5 @@ npx hardhat task:computeKMSVerifierAddress
 npx hardhat task:deployACL
 npx hardhat task:deployTFHEExecutor
 npx hardhat task:deployKMSVerifier
-
-npx hardhat compile:specific --contract lib
-npx hardhat compile:specific --contract gateway
 
 npx hardhat task:launchFhevm --skip-get-coin true
