cb0f1c8fd11a1e745d25f1ce54b42c87f0796bb4
Joseph-André Turk <josephandre.turk@zama.ai>
2024-08-06 11:54:59 +0200

    fix: patch for mocked

--- lib/FhevmLib.sol
+++ lib/FhevmLib.sol
@@ -38,6 +38,6 @@ interface FhevmLib {
     function trivialEncrypt(uint256 ct, bytes1 toType) external pure returns (uint256 result);
     function fheIfThenElse(uint256 control, uint256 ifTrue, uint256 ifFalse) external pure returns (uint256 result);
     function fheArrayEq(uint256[] memory lhs, uint256[] memory rhs) external pure returns (uint256 result);
-    function fheRand(bytes1 randType, uint256 seed) external view returns (uint256 result);
-    function fheRandBounded(uint256 upperBound, bytes1 randType, uint256 seed) external view returns (uint256 result);
+    function fheRand(bytes1 randType, uint256 seed) external returns (uint256 result);
+    function fheRandBounded(uint256 upperBound, bytes1 randType, uint256 seed) external returns (uint256 result);
 }
