de0fe8fabd84e19fc77895e7061325e5f65c0093
David Kazlauskas <deividas.kazlauskas@zama.ai>
2024-09-16 17:34:58 +0300

    Fix protobuf build

--- fhevm-engine/coprocessor/build.rs
+++ fhevm-engine/coprocessor/build.rs
@@ -4,6 +4,7 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("coprocessor_descriptor.bin"))
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["../../proto/coprocessor.proto"], &["../../proto"])
         .unwrap();
 }
