759df9659f4c318a86463a65b0a3f75c956810c8
David Kazlauskas <deividas.kazlauskas@zama.ai>
2024-10-01 11:57:36 +0300

    Add missing tracing file

---
+++ fhevm-engine/coprocessor/src/tracing.rs
@@ -0,0 +1,12 @@
+pub fn setup_tracing() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let otlp_exporter = opentelemetry_otlp::new_exporter()
+        .tonic();
+    let trace_provider = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(otlp_exporter)
+        .install_batch(opentelemetry_sdk::runtime::Tokio)?;
+
+    opentelemetry::global::set_tracer_provider(trace_provider);
+
+    Ok(())
+}
