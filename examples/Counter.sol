5b5b8eef0cf8144134519c212d1adb3f00dd2941
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-10-04 17:23:41 +0800

    docs: update Counter.sol docs

--- examples/Counter.sol
+++ examples/Counter.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.24;
 
-// dummy contract for dummy transactions just to advance blocks
+// This is a simple contract named 'Counter' that maintains a single state variable 'value'.
+// It provides functionality to increment the 'value' and read its current value.
 contract Counter {
+    // State variable to keep track of the count.
     uint32 value;
 
+    // Increases the value by 1 each time this function is called.
     function increment() public {
         value += 1;
     }
 
+    // Returns the current value of the counter.
     function currentValue() public view returns (uint32) {
         return value;
     }
---
+++ examples/README.md
@@ -0,0 +1,5 @@
+> Here will go all information about specific contracts, how are they designed and what are the considerations
+
+
+# Counter.sol
+The `Counter` smart contract is a simple contract implemented in Solidity, designed to demonstrate basic state manipulation. Its main purpose is to maintain a counter (value) and provide functions to increment and view the current counter value.
