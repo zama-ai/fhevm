1ad6d872b629152193e1f29509b6f65331f68b7a
manoranjith <manoranjith.ponnuraj@zama.ai>
2024-10-10 09:30:30 +0200

    mark uint64 decrypt test as only

--- test/gatewayDecrypt/testAsyncDecrypt.ts
+++ test/gatewayDecrypt/testAsyncDecrypt.ts
@@ -225,7 +225,7 @@ describe('TestAsyncDecrypt', function () {
     }
   });
 
-  it('test async decrypt uint64', async function () {
+  it.only('test async decrypt uint64', async function () {
     const tx2 = await this.contract.connect(this.signers.carol).requestUint64({ gasLimit: 5_000_000 });
     await tx2.wait();
     await awaitAllDecryptionResults();
