9774e9aab81e0adc2584a70435b3e887119e017c
Levent DEMIR <levent.demir@zama.ai>
2024-11-15 02:24:35 +0100

    chore: update conf for dockerized gateway, rm env variables

--- scripts/prepare_test.sh
+++ scripts/prepare_test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-make run-full
+make run-kms
+sleep 4
+make init-db
 # Deploy ACL, Gateway ..., please wait until the end before testing!!!
 make prepare-e2e-test
 # This test will fail (first event catch is buggy - we are on it)
