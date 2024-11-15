b3ff991a8f11353ada15b1fec4dc34f1e0dace0f
Levent DEMIR <levent.demir@zama.ai>
2024-11-15 14:29:03 +0100

    chore: exist on failure

--- scripts/run_everything.sh
+++ scripts/run_everything.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Trap any error and print a custom message
+trap 'echo "Error: A command failed. Exiting."' ERR
+
 make run-kms
 sleep 4
 make init-db
@@ -7,3 +12,4 @@ make init-db
 make prepare-e2e-test
 # Run one test
 make run-async-test
+
