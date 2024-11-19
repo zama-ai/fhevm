04f12cdac444a5cc992938927ce322c71f04a998
Luis Montero <17201527+fd0r@users.noreply.github.com>
2024-11-19 18:41:00 +0100

    chore: fix createInput address

--- e2e/test/gateway/createInput.ts
+++ e2e/test/gateway/createInput.ts
@@ -3,16 +3,16 @@ import { expect } from "chai";
 import { createInstance } from "../instance";
 import { getSigners, initSigners } from "../signers";
 
-describe("Test input creation", function () {
-  before(async function () {
+describe("Test input creation", function() {
+  before(async function() {
     await initSigners();
     this.signers = await getSigners();
     this.fhevm = await createInstance();
   });
 
-  it("should create an input", async function () {
+  it("should create an input", async function() {
     const input = this.fhevm.createEncryptedInput(
-      "0x1337680e44eb49ad81a588fc8a33335d9413818e",
+      "0x1337AA343Db8D44238Fe40486aDeECdf354e1f60",
       this.signers.alice.address,
     );
     input.add4(9n);
