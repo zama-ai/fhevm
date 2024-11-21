f8a19da9900b264a89e3ca7792e502e568e60798
PacificYield <173040337+PacificYield@users.noreply.github.com>
2024-11-21 14:24:25 +0100

    fix: invalid addresses config files

--- config/ZamaFHEVMConfig.sol
+++ config/ZamaFHEVMConfig.sol
@@ -14,10 +14,10 @@ library ZamaFHEVMConfig {
     function getMockConfig() internal pure returns (FHEVMConfig.FHEVMConfigStruct memory) {
         return
             FHEVMConfig.FHEVMConfigStruct({
-                ACLAddress: 0x339EcE85B9E11a3A3AA557582784a15d7F82AAf2,
-                TFHEExecutorAddress: 0x596E6682c72946AF006B27C131793F2b62527A4b,
-                FHEPaymentAddress: 0x6d5A11aC509C707c00bc3A0a113ACcC26c532547,
-                KMSVerifierAddress: 0x208De73316E44722e16f6dDFF40881A3e4F86104
+                ACLAddress: 0xB4d8d77f7F9B465B60c190480c6160b69d695c9D,
+                TFHEExecutorAddress: 0xFdee168C46e1dFD082E78192b3C622cA78B58669,
+                FHEPaymentAddress: 0x2527DD76195fD3BFdd2c76D821e1f5d433d82C25,
+                KMSVerifierAddress: 0x89842EA0b44EF85391Bd1A9f3AC8B382CCF0d3F1
             });
     }
 
