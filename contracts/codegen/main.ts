004d9fd234256a39d4da184126267a2ea1514dd7
PacificYield <173040337+PacificYield@users.noreply.github.com>
2024-11-25 15:32:55 +0100

    refactor: types and codegen

--- contracts/codegen/main.ts
+++ contracts/codegen/main.ts
@@ -6,7 +6,6 @@ import operatorsPrices from './operatorsPrices.json';
 import { generateFHEPayment } from './payments';
 import * as t from './templates';
 import * as testgen from './testgen';
-import { addTFHEExecutorEvents } from './tfheexecutor';
 
 function generateAllFiles() {
   const numSplits = 12;
@@ -18,7 +17,6 @@ function generateAllFiles() {
   writeFileSync('contracts/FHEPayment.sol', generateFHEPayment(operatorsPrices));
   writeFileSync('contracts/InputVerifier.native.sol', generateInputVerifiers(false));
   writeFileSync('contracts/InputVerifier.coprocessor.sol', generateInputVerifiers(true));
-  writeFileSync('contracts/TFHEExecutor.events.sol', addTFHEExecutorEvents('contracts/TFHEExecutor.sol'));
   writeFileSync('payment/Payment.sol', t.paymentSol());
   mkdirSync('contracts/tests', { recursive: true });
   ovShards.forEach((os) => {
