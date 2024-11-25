7115d4fd071f55859dd8aac1c329970b26b8a811
gaspard <gaspard@gmail.com>
2024-11-25 15:56:59 +0100

    fix: display missing key name @avitali-zama

--- front/src/lib/check-env.tsx
+++ front/src/lib/check-env.tsx
@@ -1,5 +1,9 @@
 export function CheckEnv() {
-  return [import.meta.env.VITE_API_URL]
-    .filter((k) => !k)
-    .map((_, i) => <div key={i}>missing env key</div>)
+  return ['VITE_API_URL']
+    .filter((envKey) => import.meta.env[envKey] === undefined)
+    .map((key, i) => (
+      <div key={i}>
+        Missing env key <code>{key}</code>
+      </div>
+    ))
 }
