a5576ace5e467e4118d1f254b9018f8390f53da3
gaspard <gaspard@gmail.com>
2024-11-26 17:59:39 +0100

    feat: add custom fonts

--- apps/front/src/theme.ts
+++ apps/front/src/theme.ts
@@ -5,6 +5,9 @@ import {
   mergeConfigs,
 } from '@chakra-ui/react'
 
+import '@fontsource-variable/archivo/index.css'
+import '@fontsource-variable/inter/index.css'
+
 /**
  * Don't forget to re-generate typegen after modifying this file.
  * This seems to be breaking something in chakra-ui.
@@ -18,7 +21,8 @@ const config = defineConfig({
         secondary: { value: '#F4B458' },
       },
       fonts: {
-        body: { value: 'Skia, system-ui, sans-serif' },
+        body: { value: '"Inter Variable", system-ui, sans-serif' },
+        heading: { value: '"Archivo Variable", system-ui, sans-serif' },
       },
     },
   },
