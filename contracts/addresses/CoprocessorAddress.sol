b90120d733bc895985af923e9a94384c00c0ada5
jatZama <josephandre.turk@zama.ai>
2024-12-02 16:32:57 +0100

    feat: use Sepolia addresses and fix workflow

--- contracts/addresses/CoprocessorAddress.sol
+++ contracts/addresses/CoprocessorAddress.sol
@@ -2,4 +2,4 @@
 
 pragma solidity ^0.8.24;
 
-address constant coprocessorAdd = 0xc9990FEfE0c27D31D0C2aa36196b085c0c4d456c;
+address constant coprocessorAdd = 0xBFb29B4d2cF6D0949F04736AD259Aa4Be4145b4B;
--- contracts/addresses/InputVerifierAddress.sol
+++ contracts/addresses/InputVerifierAddress.sol
@@ -2,4 +2,4 @@
 
 pragma solidity ^0.8.24;
 
-address constant inputVerifierAdd = 0x69dE3158643e738a0724418b21a35FAA20CBb1c5;
+address constant inputVerifierAdd = 0x3a2DA6f1daE9eF988B48d9CF27523FA31a8eBE50;
