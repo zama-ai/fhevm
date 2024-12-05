593326a0da79790961c85c3633ce798faa5f0fa4
Alessandro Vitali <alessandro.vitali@zama.ai>
2024-12-05 15:58:41 +0100

    feat: ininitial setup

---
+++ apps/email/src/app.module.ts
@@ -0,0 +1,42 @@
+import { SNSClient } from '@aws-sdk/client-sns'
+import { SQSClient } from '@aws-sdk/client-sqs'
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { SqsModule } from 'sqs'
+import awsConfig from './config/aws.config'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [awsConfig],
+    }),
+    SqsModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        consumers: [
+          {
+            name: 'email',
+            queueUrl: config.get<string>('aws.queueUrl')!,
+            useQueueUrlAsEndpoint: false,
+            sqs: new SQSClient({
+              endpoint: config.get<string>('aws.queueUrl'),
+              region: config.get<string>('aws.region'),
+            }),
+          },
+        ],
+        producers: [
+          {
+            name: 'console',
+            topicArn: config.get<string>('aws.topicArn')!,
+            sns: new SNSClient({
+              endpoint: config.get<string>('aws.endpoint'),
+              region: config.get<string>('aws.region'),
+            }),
+          },
+        ],
+      }),
+    }),
+  ],
+})
+export class AppModule {}
