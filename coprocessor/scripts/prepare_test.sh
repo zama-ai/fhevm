48bbd3eb8cd9caf8d996bef824ef00c6e3117efa
manoranjith <manoranjith.ponnuraj@zama.ai>
2024-12-05 16:52:29 +0100

    chore: remove unused scripts and asc directory

--- coprocessor/scripts/prepare_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-make run-kms
-sleep 4
-make init-db
-# Deploy ACL, Gateway ..., please wait until the end before testing!!!
-make prepare-e2e-test
-# This test will fail (first event catch is buggy - we are on it)
-make run-async-test
