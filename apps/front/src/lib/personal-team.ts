7c433813f1bc537063676b965f928f2c8a7eee6b
Gaspard Beernaert <gaspard@gmail.com>
2024-12-11 18:51:47 +0100

    fix: spelling ✨

--- apps/front/src/lib/personal-team.ts
+++ apps/front/src/lib/personal-team.ts
@@ -2,7 +2,7 @@ import { Team } from '@/__generated__/graphql'
 
 /**
  * returns the user personal team
- * currently, there is only 1 team per use, so it just returns the first one
+ * currently, there is only 1 team per user, so it just returns the first one
  * @param {Team[]} teams
  * @returns {Team} team
  */
