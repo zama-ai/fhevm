b0122532b356be2680e00f2767944c58eceb75b3
Alessandro Vitali <alessandro.vitali@zama.ai>
2024-12-20 09:39:55 +0100

    chore: move config into service

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -4,12 +4,13 @@ import { ConfigModule } from '@nestjs/config'
 import awsConfig from './config/aws.config'
 import { SqsConsumerModule } from './infra/sqs-consumer/sqs-consumer.module'
 import dbConfig from './config/db.config'
+import jwtConfig from './config/jwt.config'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [awsConfig, dbConfig],
+      load: [awsConfig, dbConfig, jwtConfig],
     }),
     GraphQLModule,
     SqsConsumerModule,
--- apps/back/src/auth/infra/auth.module.ts
+++ apps/back/src/auth/infra/auth.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 import { DatabaseModule } from '@/infra/database/database.module'
-import { jwtConstants } from '@/auth/infra/guards/constants'
 import { AuthResolver } from './auth.resolver'
 import { SignUp } from '@/auth/use-cases/signup.use-case'
 import { LogIn } from '@/auth/use-cases/login.use-case'
 import { GetUserById } from '@/users/use-cases/get-user-by-id.use-case'
 import { JwtStrategy } from './guards/passport-strategy'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1week' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('jwt.secret'),
+        signOptions: { expiresIn: config.get('jwt.expiresIn') ?? '1week' },
+      }),
     }),
     DatabaseModule,
   ],
