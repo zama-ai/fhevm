ff1c8f500c74dcb478ef319ce8f1663d8c77efdf
Alessandro Vitali <alessandro.vitali@zama.ai>
2025-01-03 17:31:07 +0100

    chore: remove uppercase in regex

--- packages/utils/src/validation.ts
+++ packages/utils/src/validation.ts
@@ -2,7 +2,7 @@ export const uuidRegex =
   /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/i
 
 export function validateNanoId(length: number = 21, prefix: string = '') {
-  const re = new RegExp(`^${prefix}[a-zA-Z0-9_-]{${length}}$`, 'i')
+  const re = new RegExp(`^${prefix}[a-z0-9_-]{${length}}$`, 'i')
   return function (value: string) {
     return re.test(value)
   }
