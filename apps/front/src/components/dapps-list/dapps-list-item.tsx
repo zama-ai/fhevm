c43d5e53116d016d47277f57dd28480a3a5f367a
gaspard <gaspard@gmail.com>
2025-01-03 15:58:42 +0100

    fix: getPersonalTeam parse teams

--- apps/front/src/components/dapps-list/dapps-list-item.tsx
+++ apps/front/src/components/dapps-list/dapps-list-item.tsx
@@ -7,19 +7,6 @@ type OwnProps = {
   dapp: Dapp
 }
 export function DappsListItem({ dapp }: OwnProps) {
-  // let lastmodified = `Created ${dapp.createdAt}`
-  // if (dapp.updatedAt !== dapp.createdAt) {
-  //   lastmodified = `Updated ${dapp.updatedAt}`
-  // }
-  // if (dapp.status === 'LIVE') {
-  //   lastmodified = `Deployed ${dapp.updatedAt}`
-  // }
-  // const lastmodified =
-  //   dapp.updatedAt === dapp.createdAt
-  //     ? `Created ${dapp.createdAt}`
-  //     : dapp.status === 'LIVE'
-  //       ? `Deployed ${dapp.updatedAt}`
-  //       : `Updated ${dapp.updatedAt}`
   const lastmodified = 'Created some time ago'
   const link =
     dapp.status === 'DRAFT' ? `/create/2/${dapp.id}` : `/dapp/${dapp.id}`
--- apps/front/src/lib/personal-team.ts
+++ apps/front/src/lib/personal-team.ts
@@ -1,9 +1,11 @@
+import { Team } from '@/__generated__/graphql'
+
 /**
  * returns the user personal team
  * currently, there is only 1 team per user, so it just returns the first one
  * @param {Team[]} teams
  * @returns {Team} team
  */
-export function getPersonalTeam<T>(teams: T[]): T {
+export function getPersonalTeam<T extends Team>(teams: T[]): T {
   return teams[0]
 }
