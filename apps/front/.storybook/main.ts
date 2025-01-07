fed6c15df2deb2b3b2888ba341112c8a5409b7f6
gaspard <gaspard@gmail.com>
2025-01-07 15:13:48 +0100

    feat: storybook DappsListItem + router decorator

--- apps/front/.storybook/main.ts
+++ apps/front/.storybook/main.ts
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
   addons: [
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
