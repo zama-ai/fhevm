505e9249a4d35cfb6c7f240f2bd830ddfb43b35d
gaspard <gaspard@gmail.com>
2025-01-07 15:12:47 +0100

    feat: storybook add light/dark mode button in toolbar

--- apps/front/.storybook/preview.tsx
+++ apps/front/.storybook/preview.tsx
@@ -5,6 +5,20 @@ import type { Preview } from '@storybook/react'
 import { system } from '../src/theme'
 
 const preview: Preview = {
+  globalTypes: {
+    theme: {
+      description: 'Global theme for components',
+      toolbar: {
+        // The label to show for this toolbar item
+        // title: 'Dark / Light mode',
+        icon: 'moon',
+        // Array of plain string values or MenuItem shape (see below)
+        items: ['light', 'dark'],
+        // Change title based on selected value
+        // dynamicTitle: true,
+      },
+    },
+  },
   parameters: {
     controls: {
       matchers: {
