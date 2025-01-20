842cef0bd40a839b6cc6bfb9ec5d40d8ece0ed54
gaspard <gaspard@gmail.com>
2025-01-20 18:51:44 +0100

    chore: abandon password change in this feature

--- apps/front/src/components/account/validations.ts
+++ apps/front/src/components/account/validations.ts
@@ -8,27 +8,3 @@ export const AccountFormSchema = z.object({
 })
 
 export type AccountFormSchemaType = z.infer<typeof AccountFormSchema>
-
-export const PasswordFormSchema = z
-  .object({
-    password: z
-      .string()
-      .min(8, 'Password should be at least 8 characters long')
-      .max(64, 'Password should be at most 64 characters long'),
-    repeatPassword: z.string(),
-  })
-  .refine(data => data.password === data.repeatPassword, {
-    message: "Passwords don't match",
-    path: ['repeatPassword'],
-  })
-
-export type PAsswordFormSchemaType = z.infer<typeof PasswordFormSchema>
-
-export const getPasswordStrengthScore = (password: string): number => {
-  let score = 0
-  if (password.length >= 8) score++
-  if (/[a-z]/.test(password) && /[A-Z]/.test(password)) score++
-  if (/\d/.test(password)) score++
-  if (/[@$!%*?&]/.test(password)) score++
-  return Math.min(score, 4)
-}
