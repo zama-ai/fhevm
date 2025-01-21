d44cfa3bc6d80b6c573edabfc4d97904da0f72b3
Isaac Herrera <isaac.herrera@zama.ai>
2025-01-21 17:24:35 +0100

    chore: configure prettier, solhint, husky and commitlint tools

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
