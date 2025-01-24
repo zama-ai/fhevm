86564f61cc85c3c1b1d12945f90399cedfb5f237
Isaac Herrera <isaac.herrera@zama.ai>
2025-01-24 16:47:20 +0100

    feat: introduce the initial ZKPoKManager interface
    * feat: introduce the ZKPoKManager contract with basic tests
    
    * chore: apply solhint linting
    
    * refactor: split verification process and mock flow business rules
    
    * refactor: introduce IZKPoKManager interface, apply renamings and adjust tests
    
    * refactor: adopt present tense for event naming
    
    * refactor: remove struct definitions and unrequired arguments, improve args types
    
    * refactor: remove struct definitions and unrequired arguments, improve args types
    
    * refactor: reorder arguments and add some event indexes
    
    * refactor: adjust verifyProofResponse arguments
    
    * refactor: adjust verifyProofResponse arguments
    
    * refactor: rename some args and remove unrequired eip712 contract address
    
    * refactor: rename some args and remove unrequired eip712 contract address
    
    * fix: rename mispelled clientAddress arg name

---
+++ contracts/ZKPoKManager.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.28;
+
+import "./interfaces/IZKPoKManager.sol";
+
+/// @title ZKPoKManager
+/// @dev Implementation of the IZKPoKManager for the ZKPoK verifications on the Gateway L2 roll-up.
+contract ZKPoKManager is IZKPoKManager {
+    /// @notice The address of the Payment Manager contract for service fees, burn and distribution
+    address internal immutable _PAYMENT_MANAGER;
+
+    constructor(address paymentManager) {
+        _PAYMENT_MANAGER = paymentManager;
+    }
+
+    function verifyProofRequest(
+        uint256 keychainId,
+        uint256 chainId,
+        address contractAddress,
+        address userAddress,
+        bytes calldata ctProofHandle
+    ) public {
+        if (!_isNetworkRegisteredInKeychain(keychainId, chainId)) {
+            revert NetworkNotRegisteredInKeychain();
+        }
+        _sendServiceFees();
+        uint256 zkProofId = _generateZKProofId();
+        emit VerifyProofRequest(keychainId, zkProofId, chainId, contractAddress, userAddress, ctProofHandle);
+    }
+
+    function verifyProofResponse(uint256 zkProofId, bytes32[] calldata handles, bytes calldata signature) public {
+        if (_isConsensusAgreed()) {
+            _burnAndDistributeFees();
+            bytes[] memory signatures = _getSignatures(zkProofId);
+            emit VerifyProofResponse(zkProofId, handles, signatures);
+        }
+    }
+
+    /// @notice Generates an identifier for a ZKProof verification
+    function _generateZKProofId() internal pure returns (uint256) {
+        // TODO: Implement zkProofId generation
+    }
+
+    /// @notice Checks if the current network (block.chainid) is attached to the keychainId
+    function _isNetworkRegisteredInKeychain(uint256 keychainId, uint256 chainId) internal pure returns (bool) {
+        // TODO: Implement DomainManager contract call to check networks by keychainId
+        return true;
+    }
+
+    /// @notice Checks if the consensus is reached
+    function _isConsensusAgreed() internal pure returns (bool) {
+        // TODO: Implement consensus logic
+        return true;
+    }
+
+    /// @dev Service fees should be refunded if there is no consensus or response after N blocks
+    function _sendServiceFees() internal pure {
+        // TODO: Implement PaymentManager contract call to send service fees
+    }
+
+    /// @notice Burns and distributes the ZKProof verification fees
+    function _burnAndDistributeFees() internal pure {
+        // TODO: Implement PaymentManager contract call to burn and distribute fees
+    }
+
+    /// @notice Aggregates the received ZKProof verification signatures
+    function _getSignatures(uint256 zkProofId) internal pure returns (bytes[] memory) {
+        // TODO: Implement signature aggregation logic
+        return new bytes[](0);
+    }
+}
