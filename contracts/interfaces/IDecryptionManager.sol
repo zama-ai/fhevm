5683966de9f00abb35dfa1e85b2004593f111f21
Roman <56846628+RomanBredehoft@users.noreply.github.com>
2025-01-30 13:29:40 +0100

    chore: update specs in DecryptionManager (#30)
    * chore: update specs in DecryptionManager

--- contracts/interfaces/IDecryptionManager.sol
+++ contracts/interfaces/IDecryptionManager.sol
@@ -3,119 +3,80 @@ pragma solidity ^0.8.28;
 
 /// @title An interface for the decryption manager
 /// @notice The decryption manager is responsible for decrypting ciphertext using a KMS
-/// @notice Both user decryption and oracle decryption are handled
+/// @notice Both user decryption and public decryption are handled
 /// @dev Request functions are callable by any user or the relayer
 /// @dev Response functions are only callable by the KMS Connectors
 interface IDecryptionManager {
-    /// @notice Emitted when an oracle decryption request is made
-    /// @dev This event is meant to be listened by a user or relayer
-    /// @param oracleDecryptionId The oracle decryption request's unique ID
-    event OracleDecryptionId(uint256 indexed oracleDecryptionId);
+    /// @notice A struct that contains the ciphertext handle and its associated contract address
+    /// @notice for which the ciphertext is allowed to be decrypted
+    struct CiphertextContract {
+        uint256 ciphertextHandle;
+        address contractAddress;
+    }
 
-    /// @notice Emitted when an oracle decryption request is made
+    /// @notice A struct that contains the ciphertext handle and the contract address
+    /// @notice Emitted when an public decryption request is made
     /// @dev This event is meant to be listened by the KMS Connectors
-    /// @param keychainId The keychain's unique ID
-    /// @param oracleDecryptionId The oracle decryption request's unique ID
-    /// @param chainId The network's chain ID
-    /// @param kmsVerifier The network's KMS Verifier address to consider
-    /// @param acl The network's ACL address to consider
+    /// @param publicDecryptionId The public decryption request's unique ID
     /// @param ciphertextHandles The handles of the ciphertexts to decrypt
-    event OracleDecryptionRequest(
-        uint256 indexed keychainId,
-        uint256 indexed oracleDecryptionId,
-        uint256 indexed chainId,
-        address kmsVerifier,
-        address acl,
-        uint256[] ciphertextHandles
-    );
+    event PublicDecryptionRequest(uint256 indexed publicDecryptionId, uint256[] ciphertextHandles);
 
-    /// @notice Emitted when an oracle decryption response is made
+    /// @notice Emitted when an public decryption response is made
     /// @dev This event is meant to be listened by a user or relayer
-    /// @param oracleDecryptionId The oracle decryption request's unique ID associated with the response
+    /// @param publicDecryptionId The public decryption request's unique ID associated with the response
     /// @param decryptedResult The decrypted result
     /// @param signatures The signatures of all the KMS Connectors that responded
-    event OracleDecryptionResponse(uint256 indexed oracleDecryptionId, bytes decryptedResult, bytes[] signatures);
-
-    /// @notice Emitted when a user decryption request is made
-    /// @dev This event is meant to be listened by a user or relayer
-    /// @param userDecryptionId The user decryption request's unique ID
-    event UserDecryptionId(uint256 indexed userDecryptionId);
+    event PublicDecryptionResponse(uint256 indexed publicDecryptionId, bytes decryptedResult, bytes[] signatures);
 
     /// @notice Emitted when a user decryption request is made
     /// @dev This event is meant to be listened by the KMS Connectors
-    /// @param keychainId The keychain's unique ID
     /// @param userDecryptionId The user decryption request's unique ID
-    /// @param chainId The network's chain ID
-    /// @param kmsVerifier The network's KMS Verifier address to consider
-    /// @param acl The network's ACL address to consider
+    /// @param ciphertextContracts The ciphertexts and their associated contract addresses to decrypt
     /// @param userAddress The user's address
-    /// @param encryptionKey The encryption key
-    /// @param proof The proof
-    /// @param ciphertextHandle The handles of the ciphertexts to decrypt
     event UserDecryptionRequest(
-        uint256 indexed keychainId,
         uint256 indexed userDecryptionId,
-        uint256 indexed chainId,
-        address kmsVerifier,
-        address acl,
-        address userAddress,
-        bytes encryptionKey,
-        string proof,
-        uint256 ciphertextHandle
+        CiphertextContract[] ciphertextContracts,
+        address userAddress
     );
 
-    /// @notice Emitted when an oracle decryption response is made
+    /// @notice Emitted when an public decryption response is made
     /// @dev This event is meant to be listened by a user or relayer
     /// @param userDecryptionId The user decryption request's unique ID associated with the response
     /// @param decryptedResult The decrypted result
     /// @param signatures The signatures of all the KMS Connectors that responded
     event UserDecryptionResponse(uint256 indexed userDecryptionId, bytes decryptedResult, bytes[] signatures);
 
-    /// @notice Requests an oracle decryption
+    /// @notice Requests an public decryption
     /// @dev This function can be called by a user or relayer
-    /// @param keychainId The keychain's unique ID
-    /// @param chainId The network's chain ID
-    /// @param kmsVerifier The network's KMS Verifier address to consider
-    /// @param acl The network's ACL address to consider
     /// @param ciphertextHandles The handles of the ciphertexts to decrypt
-    function oracleDecryptionRequest(
-        uint256 keychainId,
-        uint256 chainId,
-        address kmsVerifier,
-        address acl,
-        uint256[] calldata ciphertextHandles
-    ) external;
+    function publicDecryptionRequest(uint256[] calldata ciphertextHandles) external;
 
-    /// @notice Responds to an oracle decryption request
+    /// @notice Responds to an public decryption request
     /// @dev This function can only be called by the KMS Connectors
-    /// @param oracleDecryptionId The oracle decryption request's unique ID associated with the response
+    /// @param publicDecryptionId The public decryption request's unique ID associated with the response
     /// @param decryptedResult The decrypted result
     /// @param signature The signature of the KMS Connector that responded
-    function oracleDecryptionResponse(
-        uint256 oracleDecryptionId,
+    function publicDecryptionResponse(
+        uint256 publicDecryptionId,
         bytes calldata decryptedResult,
         bytes calldata signature
     ) external;
 
     /// @notice Requests a user decryption
     /// @dev This function can be called by a user or relayer
-    /// @param keychainId The keychain's unique ID
-    /// @param chainId The network's chain ID
-    /// @param kmsVerifier The network's KMS Verifier address to consider
-    /// @param acl The network's ACL address to consider
+    /// @param ciphertextContracts The ciphertexts and their associated contract addresses to decrypt
     /// @param userAddress The user's address
-    /// @param encryptionKey The encryption key
-    /// @param proof The proof
-    /// @param ciphertextHandle The handles of the ciphertexts to decrypt
+    /// @param publicKey The public key
+    /// @param eip712ChainId The chain ID of the EIP712 signature
+    /// @param eip712Contracts The EIP712 contracts found in the message
+    /// @param eip712Signature The EIP712 signature to verify
     function userDecryptionRequest(
-        uint256 keychainId,
-        uint256 chainId,
-        address kmsVerifier,
-        address acl,
+        CiphertextContract[] calldata ciphertextContracts,
         address userAddress,
-        bytes calldata encryptionKey,
-        string calldata proof,
-        uint256 ciphertextHandle
+        bytes calldata publicKey,
+        uint256 eip712ChainId,
+        address[] calldata eip712Contracts,
+        bytes calldata eip712Signature
     ) external;
 
     /// @notice Responds to a user decryption request
