778c5f425e8f76b335f248f8f0a2c87438654159
Alessandro Vitali <alessandro.vitali@zama.ai>
2025-02-07 12:42:35 +0100

    fix: broken pipeline

--- apps/orchestrator/test/setup.ts
+++ apps/orchestrator/test/setup.ts
@@ -1,4 +1,6 @@
 import { allCustomMatcher } from 'aws-sdk-client-mock-vitest'
 import { expect } from 'vitest'
+import { config } from 'dotenv'
 
+config({ path: './.env.test' })
 expect.extend(allCustomMatcher)
