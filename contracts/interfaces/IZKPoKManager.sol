7c5717bda97707fb98cb0e68c6ae5f54971f7433
Isaac Herrera <isaac.herrera@zama.ai>
2025-02-10 10:14:50 +0100

    feat: implement ZKPoKManager contract business logics
    * feat: implement ZKPoKManager contract business logics
    
    * chore: rename stored struct
    
    * feat: implement proof verification logic and adjust interface definitions
    
    * test: adjust test accordingly to verification logic
    
    * chore: add node-tkms as devDependency
    
    * fix: correct EIP712 hashing and signature validation
    
    * feat: introduce HTTPZ contract calls and versioning states
    
    * chore: simplify verifiedZKProofs mapping by removing diggest index
    
    * docs: include variables and functions descriptions and apply renamings
    
    * test: restore eip712 utils and restructure test cases
    
    * test: specify the EIP712ResponseMessage payload

--- contracts/interfaces/IZKPoKManager.sol
+++ contracts/interfaces/IZKPoKManager.sol
@@ -6,44 +6,50 @@ pragma solidity ^0.8.28;
  * @dev Interface of the ZKPoKManager contract for Zero-Knowledge Proof of Knowledge (ZKPoK)
  * verifications.
  *
- * This interface expose two functions that are meant to process a ZKProof verification asynchronously.
+ * This interface expose two functions that are meant to process a ZK Proof verification asynchronously.
  * The first function is called by the fhEVM Relayer to start the verification process, and the second
  * function is called by the Coprocessors that process the verification.
  */
 interface IZKPoKManager {
-    /// @notice Emitted when a ZKProof verification is started
+    /// @notice Emitted when a ZK Proof verification is started
     /// @dev This event is meant to be listened by the Coprocessor
     event VerifyProofRequest(
         uint256 indexed zkProofId,
-        uint256 indexed chainId,
+        uint256 indexed contractChainId,
         address contractAddress,
         address userAddress,
         bytes ciphertextProof
     );
 
-    /// @notice Emitted once a ZKProof verification is completed
+    /// @notice Emitted once a ZK Proof verification is completed
     /// @dev This event is meant to be listened by the fhEVM Relayer
     event VerifyProofResponse(uint256 indexed zkProofId, bytes32[] handles, bytes[] signatures);
 
     /// @notice Error indicating that a given chain ID is not registered
     error NetworkNotRegistered();
 
-    /// @notice Starts the ZKProof verification
+    /// @notice Error indicating that the signer is not a valid Coprocessor
+    error InvalidCoprocessorSigner(address signer);
+
+    /// @notice Error indicating that the Coprocessor has already signed the ZK Proof
+    error CoprocessorHasAlreadySigned(uint256 zkProofId, address signer);
+
+    /// @notice Requests the verification of a ZK Proof
     /// @dev This function is called by the fhEVM Relayer
-    /// @param chainId The network's chain ID
+    /// @param contractChainId The chainId of the blockchain the contract belongs to
     /// @param contractAddress The address of the dapp the input is used for
     /// @param userAddress The address of the user providing the input
     /// @param ciphertextProof The ciphertext and proof to be verified
     function verifyProofRequest(
-        uint256 chainId,
+        uint256 contractChainId,
         address contractAddress,
         address userAddress,
         bytes calldata ciphertextProof
     ) external;
 
-    /// @notice Processes ZKProof verification responses and validates the verification completion
+    /// @notice Responds to a ZK Proof verification request
     /// @dev This function is called by the Coprocessor
-    /// @param zkProofId The ID of the requested ZKProof verification
+    /// @param zkProofId The ID of the requested ZK Proof
     /// @param handles The Coprocessor's computed handles
     /// @param signature The Coprocessor's signature
     function verifyProofResponse(uint256 zkProofId, bytes32[] calldata handles, bytes calldata signature) external;
