3a573e51a12068f1e3a0c2a337029a3ffc751884
Alessandro Vitali <alessandro.vitali@zama.ai>
2025-02-11 12:01:24 +0100

    feat: retrieve port from config service

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { Logger } from 'nestjs-pino'
 import { AppModule } from './app.module.js'
-
-const PORT = process.env.PORT ?? 3000
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
   app.useLogger(app.get(Logger))
-  await app.listen(PORT)
+  const port = app.get(ConfigService).get<number>('common.port') ?? 3000
+  await app.listen(port)
 }
 bootstrap()
--- apps/web3/src/main.ts
+++ apps/web3/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module.js'
 import { Logger } from 'nestjs-pino'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useLogger(app.get(Logger))
-  await app.listen(process.env.PORT ?? 3000)
+  const port = app.get(ConfigService).get<number>('common.port') ?? 3000
+  await app.listen(port)
 }
 bootstrap()
