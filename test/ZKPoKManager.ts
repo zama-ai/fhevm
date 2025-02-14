64416d228a8dfeea2f442d3edf6ee3aff5ff967d
Isaac Herrera <isaac.herrera@zama.ai>
2025-02-14 11:41:48 +0100

    feat: enable the HTTPZ contract calls
    * feat: enable the HTTPZ calls
    
    * test: introduce httpz deployment and new tests
    
    * test: introduce httpz deployment and new tests
    
    * chore: rename ciphertextProof argument and adjust docs

--- test/ZKPoKManager.ts
+++ test/ZKPoKManager.ts
@@ -4,97 +4,164 @@ import { expect } from "chai";
 import hre from "hardhat";
 
 import { ZKPoKManager } from "../typechain-types";
-import { createEIP712ResponseZKPoK, getSignaturesZKPoK } from "./utils/eip712";
+import { createEIP712ResponseZKPoK, deployInitKmsHTTPZFixture, getSignaturesZKPoK } from "./utils";
 
 describe("ZKPoKManager", function () {
   async function deployZKPoKManagerFixture() {
+    const { httpz, coprocessorSigners, admin, signers } = await deployInitKmsHTTPZFixture();
     const ZKPoKManagerContract = await hre.ethers.getContractFactory("ZKPoKManager");
-    // TODO: Implement the HTTPZ deployment and replace the first address
-    const zkpokManager = await ZKPoKManagerContract.deploy(
-      "0xDA9FeD390f02F559E62240a112aBd2FAe06DCdB5",
-      "0xDA9FeD390f02F559E62240a112aBd2FAe06DCdB5",
-    );
-    return { zkpokManager };
+    const zkpokManager = await ZKPoKManagerContract.deploy(httpz, "0xDA9FeD390f02F559E62240a112aBd2FAe06DCdB5");
+    return { httpz, zkpokManager, coprocessorSigners, admin, signers };
   }
 
   describe("Verify proof request", async function () {
     const zkProofId = "0";
-    let zkpokManagerInstance: ZKPoKManager;
+    const contractChainId = "123";
+    let zkpokManager: ZKPoKManager;
     before(async function () {
-      const { zkpokManager } = await loadFixture(deployZKPoKManagerFixture);
-      zkpokManagerInstance = zkpokManager;
+      const fixture = await loadFixture(deployZKPoKManagerFixture);
+      await fixture.httpz.connect(fixture.admin).addNetwork({
+        chainId: contractChainId,
+        httpzLibrary: hre.ethers.Wallet.createRandom().address,
+        acl: hre.ethers.Wallet.createRandom().address,
+        name: "Network name",
+        website: "network-website.com",
+      });
+      zkpokManager = fixture.zkpokManager;
     });
 
     it("Should success", async function () {
       // Given
-      const contractChainId = "789";
       const contractAddress = "0xa83114A443dA1CecEFC50368531cACE9F37fCCcb";
       const userAddress = "0x388C818CA8B9251b393131C08a736A67ccB19297";
       const ctProofHandle = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
       // When
-      const result = zkpokManagerInstance.verifyProofRequest(
-        contractChainId,
-        contractAddress,
-        userAddress,
-        ctProofHandle,
-      );
+      const txResponse = zkpokManager.verifyProofRequest(contractChainId, contractAddress, userAddress, ctProofHandle);
 
       // Then
-      await expect(result)
-        .to.emit(zkpokManagerInstance, "VerifyProofRequest")
+      await expect(txResponse)
+        .to.emit(zkpokManager, "VerifyProofRequest")
         .withArgs(zkProofId, contractChainId, contractAddress, userAddress, ctProofHandle);
+    });
+
+    it("Should revert with NetworkNotRegistered", async function () {
+      // Given
+      const fakeChainId = "456";
+      const contractAddress = "0xa83114A443dA1CecEFC50368531cACE9F37fCCcb";
+      const userAddress = "0x388C818CA8B9251b393131C08a736A67ccB19297";
+      const ctProofHandle = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+      // When
+      const txResponse = zkpokManager.verifyProofRequest(fakeChainId, contractAddress, userAddress, ctProofHandle);
+
+      // Then
+      await expect(txResponse).revertedWithCustomError(zkpokManager, "NetworkNotRegistered").withArgs(fakeChainId);
     });
   });
 
   describe("Verify proof response", async function () {
     const zkProofId = "0";
-    let signers: HardhatEthersSigner[];
-    let zkpokManagerInstance: ZKPoKManager;
+    const contractChainId = "123";
+    const contractAddress = "0xa83114A443dA1CecEFC50368531cACE9F37fCCcb";
+    const userAddress = "0x388C818CA8B9251b393131C08a736A67ccB19297";
+    let zkpokManager: ZKPoKManager;
+    let coprocessorSigners: HardhatEthersSigner[];
+    let fakeSigner: HardhatEthersSigner;
 
     beforeEach(async function () {
-      const { zkpokManager } = await loadFixture(deployZKPoKManagerFixture);
-      zkpokManagerInstance = zkpokManager;
-      signers = await hre.ethers.getSigners();
-
-      const contractChainId = "789";
-      const contractAddress = "0xa83114A443dA1CecEFC50368531cACE9F37fCCcb";
-      const userAddress = "0x388C818CA8B9251b393131C08a736A67ccB19297";
-      const ctProofHandle = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-      await zkpokManagerInstance.verifyProofRequest(contractChainId, contractAddress, userAddress, ctProofHandle);
+      const fixture = await loadFixture(deployZKPoKManagerFixture);
+      await fixture.httpz.connect(fixture.admin).addNetwork({
+        chainId: contractChainId,
+        httpzLibrary: hre.ethers.Wallet.createRandom().address,
+        acl: hre.ethers.Wallet.createRandom().address,
+        name: "Network name",
+        website: "network-website.com",
+      });
+      zkpokManager = fixture.zkpokManager;
+      coprocessorSigners = fixture.coprocessorSigners;
+      fakeSigner = fixture.signers[0];
+      const ctProofHandle = hre.ethers.randomBytes(32);
+      await zkpokManager.verifyProofRequest(contractChainId, contractAddress, userAddress, ctProofHandle);
     });
 
     it("Should success", async function () {
       // Given
-      const handles = [hre.ethers.encodeBytes32String("123456789"), hre.ethers.encodeBytes32String("987654321")];
-      const zkpokManagerAddress = await zkpokManagerInstance.getAddress();
-      const eip712Message = createEIP712ResponseZKPoK(hre.network.config.chainId!, zkpokManagerAddress);
-      const [signature1, signature2, signature3] = await getSignaturesZKPoK(eip712Message, signers.slice(0, 3));
+      const zkpokManagerAddress = await zkpokManager.getAddress();
+      const handles = [hre.ethers.randomBytes(32), hre.ethers.randomBytes(32)];
+      const eip712Message = createEIP712ResponseZKPoK(
+        hre.network.config.chainId!,
+        zkpokManagerAddress,
+        handles,
+        userAddress,
+        contractAddress,
+        contractChainId,
+      );
+      const [signature1, signature2, signature3, signature4] = await getSignaturesZKPoK(
+        eip712Message,
+        coprocessorSigners,
+      );
 
       // When
-      await zkpokManagerInstance.verifyProofResponse(zkProofId, handles, signature1);
-      await zkpokManagerInstance.verifyProofResponse(zkProofId, handles, signature2);
-      let result = zkpokManagerInstance.verifyProofResponse(zkProofId, handles, signature3);
+      await zkpokManager.connect(coprocessorSigners[0]).verifyProofResponse(zkProofId, handles, signature1);
+      await zkpokManager.connect(coprocessorSigners[1]).verifyProofResponse(zkProofId, handles, signature2);
+      let txResponse = zkpokManager.connect(coprocessorSigners[2]).verifyProofResponse(zkProofId, handles, signature3);
+      let lateTxResponse = zkpokManager
+        .connect(coprocessorSigners[2])
+        .verifyProofResponse(zkProofId, handles, signature4);
 
       // Then
-      await expect(result)
-        .to.emit(zkpokManagerInstance, "VerifyProofResponse")
+      await expect(txResponse)
+        .to.emit(zkpokManager, "VerifyProofResponse")
         .withArgs(zkProofId, handles, [signature1, signature2, signature3]);
+      await expect(lateTxResponse).to.not.emit(zkpokManager, "VerifyProofResponse");
+    });
+
+    it("Should revert with InvalidCoprocessorSigner", async function () {
+      // Given
+      const handles = [hre.ethers.randomBytes(32), hre.ethers.randomBytes(32)];
+      const zkpokManagerAddress = await zkpokManager.getAddress();
+      const eip712Message = createEIP712ResponseZKPoK(
+        hre.network.config.chainId!,
+        zkpokManagerAddress,
+        handles,
+        userAddress,
+        contractAddress,
+        contractChainId,
+      );
+      const [signature1] = await getSignaturesZKPoK(eip712Message, [fakeSigner]);
+
+      // When
+      let txResponse = zkpokManager.verifyProofResponse(zkProofId, handles, signature1);
+
+      // Then
+      await expect(txResponse)
+        .revertedWithCustomError(zkpokManager, "InvalidCoprocessorSigner")
+        .withArgs(fakeSigner.address);
     });
 
     it("Should revert with CoprocessorHasAlreadySigned", async function () {
       // Given
-      const handles = [hre.ethers.encodeBytes32String("123456789")];
-      const zkpokManagerAddress = await zkpokManagerInstance.getAddress();
-      const eip712Message = createEIP712ResponseZKPoK(hre.network.config.chainId!, zkpokManagerAddress);
-      const [signature1] = await getSignaturesZKPoK(eip712Message, signers.slice(0, 1));
+      const handles = [hre.ethers.randomBytes(32), hre.ethers.randomBytes(32)];
+      const zkpokManagerAddress = await zkpokManager.getAddress();
+      const eip712Message = createEIP712ResponseZKPoK(
+        hre.network.config.chainId!,
+        zkpokManagerAddress,
+        handles,
+        userAddress,
+        contractAddress,
+        contractChainId,
+      );
+      const [signature1] = await getSignaturesZKPoK(eip712Message, coprocessorSigners);
 
       // When
-      await zkpokManagerInstance.verifyProofResponse(zkProofId, handles, signature1);
-      let result = zkpokManagerInstance.verifyProofResponse(zkProofId, handles, signature1);
+      await zkpokManager.verifyProofResponse(zkProofId, handles, signature1);
+      let txResponse = zkpokManager.verifyProofResponse(zkProofId, handles, signature1);
 
       // Then
-      await expect(result).revertedWithCustomError(zkpokManagerInstance, "CoprocessorHasAlreadySigned");
+      await expect(txResponse)
+        .revertedWithCustomError(zkpokManager, "CoprocessorHasAlreadySigned")
+        .withArgs(zkProofId, coprocessorSigners[0].address);
     });
   });
 });
--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,1 +1,2 @@
+export * from "./deploys";
 export * from "./eip712";
