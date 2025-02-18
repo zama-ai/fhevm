45c0b2e901dac46699db3c593e1a183c31f65f94
Alessandro Vitali <alessandro.vitali@zama.ai>
2025-02-18 09:55:47 +0100

    chore: refactor app deployment events
    Refactor the app deployment events with back and web3 namespace

--- packages/messages/src/back.events.ts
+++ packages/messages/src/back.events.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod'
 import { chainId, meta, Meta, web3Address } from './shared.js'
 
-type EventTypes = 'dapp:stats-requested' | 'dapp:stats-available'
+type EventTypes =
+  | 'dapp:created'
+  | 'dapp:confirmed'
+  | 'dapp:failed'
+  | 'dapp:stats-requested'
+  | 'dapp:stats-available'
 
 function genSchema<Key extends EventTypes, Payload extends z.ZodRawShape>(
   key: Key,
@@ -18,26 +23,26 @@ function genSchema<Key extends EventTypes, Payload extends z.ZodRawShape>(
   })
 }
 
-const eventMap = {
-  'dapp:stats-requested': genSchema('dapp:stats-requested', {}),
-  'dapp:stats-available': genSchema('dapp:stats-available', {
+const schemas = [
+  genSchema('dapp:created', { dAppId: z.string() }),
+  genSchema('dapp:confirmed', { dAppId: z.string() }),
+  genSchema('dapp:failed', {
+    dAppId: z.string(),
+    reason: z.string(),
+  }),
+  genSchema('dapp:stats-requested', {}),
+  genSchema('dapp:stats-available', {
     name: z.string(),
     timestamp: z.string().datetime(),
     externalRef: z.string(),
   }),
-} as const
-type EventMap = typeof eventMap
+] as const
 
-export const schema = z
-  .discriminatedUnion('type', [
-    eventMap['dapp:stats-requested'],
-    eventMap['dapp:stats-available'],
-  ])
-  .and(
-    z.object({
-      meta: meta,
-    }),
-  )
+export const schema = z.discriminatedUnion('type', [...schemas]).and(
+  z.object({
+    meta: meta,
+  }),
+)
 export type BackEvent = z.infer<typeof schema>
 
 /**
@@ -46,16 +51,25 @@ export type BackEvent = z.infer<typeof schema>
  * @param type The type of the Event to generate
  * @returns the factory function for the selected event
  */
-function factory<K extends keyof EventMap>(type: K) {
-  return function (payload: z.infer<EventMap[K]>['payload'], meta: Meta) {
+function factory<
+  K extends EventTypes,
+  Event extends { type: `back:${K}`; payload: object; meta: Meta } = Extract<
+    BackEvent,
+    { type: `back:${K}` }
+  >,
+>(type: K) {
+  return function (payload: Event['payload'], meta: Meta) {
     return {
       type: `back:${type}`,
       payload,
       meta,
-    } as BackEvent
+    } as Event
   }
 }
 
+export const dappCreated = factory('dapp:created')
+export const dappConfirmed = factory('dapp:confirmed')
+export const dappFailed = factory('dapp:failed')
 export const dappStatsRequested = factory('dapp:stats-requested')
 export const dappStatsAvailable = factory('dapp:stats-available')
 
