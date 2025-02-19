f86b084e7cf279adafe2613447ce57b39d2cf500
manoranjith <manoranjith.ponnuraj@zama.ai>
2025-02-19 15:09:16 +0100

    feat: add generic once handler

--- apps/relayer/fhevm-relayer/src/utils.rs
+++ apps/relayer/fhevm-relayer/src/utils.rs
@@ -1,3 +1,7 @@
+use crate::orchestrator::traits::{Event, EventHandler};
+use async_trait::async_trait;
+use std::sync::Mutex;
+use tokio::sync::oneshot;
 use uuid::Uuid;
 
 pub fn colorize_event_type(event_type: &str) -> String {
@@ -6,4 +10,33 @@ pub fn colorize_event_type(event_type: &str) -> String {
 
 pub fn colorize_request_id(request_id: &Uuid) -> String {
     format!("\x1b[33m{}\x1b[0m", request_id) // Yellow for request ID
+}
+
+pub struct OnceHandler<T> {
+    tx: Mutex<Option<oneshot::Sender<T>>>,
+}
+
+impl<T> OnceHandler<T> {
+    pub fn new() -> (Self, oneshot::Receiver<T>) {
+        let (tx, rx) = oneshot::channel();
+        (
+            Self {
+                tx: Mutex::new(Some(tx)),
+            },
+            rx,
+        )
+    }
+}
+
+#[async_trait]
+impl<E> EventHandler<E> for OnceHandler<E>
+where
+    E: Event + Send + Sync + 'static,
+{
+    async fn handle_event(&self, event: E) {
+        let mut lock = self.tx.lock().unwrap();
+        if let Some(tx) = lock.take() {
+            let _ = tx.send(event);
+        }
+    }
 }
