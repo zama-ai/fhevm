43b31deef29e2c39bd83a4766de522e46adb468b
Georgi <georgi.goranov@zama.ai>
2025-02-25 15:52:02 +0200

    fix: configure and start gw-listener thread

--- fhevm-engine/gw-listener/src/bin/gw_listener.rs
+++ fhevm-engine/gw-listener/src/bin/gw_listener.rs
@@ -1,5 +1,11 @@
+use alloy::network::EthereumWallet;
+use alloy::providers::{ProviderBuilder, WsConnect};
+use alloy::signers::local::PrivateKeySigner;
 use alloy::{primitives::Address, transports::http::reqwest::Url};
 use clap::Parser;
+use gw_listener::gw_listener::GatewayListener;
+use gw_listener::ConfigSettings;
+use tokio_util::sync::CancellationToken;
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
@@ -24,8 +30,44 @@ struct Conf {
 
     #[arg(long, default_value = "10")]
     error_sleep_max_secs: u16,
+
+    #[arg(long)]
+    ws_endpoint_url: String,
 }
 
-fn main() {
-    let _conf = Conf::parse();
+#[tokio::main]
+async fn main() {
+    let conf = Conf::parse();
+
+    let database_url = conf
+        .database_url
+        .clone()
+        .unwrap_or_else(|| std::env::var("DATABASE_URL").expect("DATABASE_URL is undefined"));
+
+    let signer: PrivateKeySigner = PrivateKeySigner::random(); // TODO:
+    let wallet: EthereumWallet = signer.clone().into();
+
+    let provider = ProviderBuilder::new()
+        .wallet(wallet)
+        .on_ws(WsConnect::new(conf.ws_endpoint_url))
+        .await
+        .expect("should have valid provider");
+
+    let cancel_token = CancellationToken::new();
+    let gw_listener = GatewayListener::new(
+        conf.zkpok_manager_address,
+        ConfigSettings {
+            database_url,
+            database_pool_size: conf.database_pool_size,
+            verify_proof_req_db_channel: conf.verify_proof_req_database_channel,
+            gw_url: conf.gw_url,
+            error_sleep_initial_secs: conf.error_sleep_initial_secs,
+            error_sleep_max_secs: conf.error_sleep_max_secs,
+        },
+        cancel_token.clone(),
+        provider.clone(),
+    );
+
+    // Run gw_listener thread
+    let _ = gw_listener.run().await;
 }
