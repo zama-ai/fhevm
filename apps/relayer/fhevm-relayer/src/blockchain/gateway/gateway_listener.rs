12dee343a731eb06e7a66e06a0252954badfa857
manoranjith <manoranjith.ponnuraj@zama.ai>
2025-03-12 15:09:46 +0100

    chore: forward all events from listener
    - The filtering for specific topic, after receiving the event is done by
      the handlers concerning the topic.
    
    - The filtering for specific topics to be sent from subscription
      provider is set while making the subscription.

--- apps/relayer/fhevm-relayer/src/blockchain/gateway/gateway_listener.rs
+++ apps/relayer/fhevm-relayer/src/blockchain/gateway/gateway_listener.rs
@@ -1,28 +1,13 @@
-use alloy_sol_types::SolEvent;
 use tracing::{error, info};
 
-use crate::blockchain::ethereum::bindings::{DecyptionManager, ZKPoKManager};
 use crate::core::event::{
     ApiCategory, ApiVersion, GenericEventData, RelayerEvent, RelayerEventData,
 };
 use crate::orchestrator::traits::{EventDispatcher, HandlerRegistry};
 use crate::orchestrator::Orchestrator;
-use alloy::hex;
-use alloy::primitives::FixedBytes;
 use alloy::rpc::types::Log;
 use futures_util::StreamExt;
 use std::sync::Arc;
-
-// Define event topics as constants
-
-const PROOF_VERIFICATION_RESPONSE_TOPIC: alloy::primitives::FixedBytes<32> =
-    ZKPoKManager::VerifyProofResponse::SIGNATURE_HASH;
-
-const PUBLIC_DECRYPTION_RESPONSE_TOPIC: alloy::primitives::FixedBytes<32> =
-    DecyptionManager::PublicDecryptionResponse::SIGNATURE_HASH;
-
-const USER_DECRYPTION_RESPONSE_TOPIC: alloy::primitives::FixedBytes<32> =
-    DecyptionManager::UserDecryptionResponse::SIGNATURE_HASH;
 
 pub async fn event_listener_gateway(
     mut subscription: alloy::pubsub::SubscriptionStream<Log>,
@@ -37,56 +22,15 @@ pub async fn event_listener_gateway(
         tokio::select! {
             event = subscription.next() => match event {
                 Some(event_log) => {
-                    info!("rollup listener catches one event with topic {:?}", event_log.topic0());
-
-                    let id = orchestrator.new_request_id();
-
-                    // Determine event type based on topic
-                    let event_data = if let Some(topic0) = event_log.topic0() {
-                        // Convert B256 to FixedBytes<32>
-                        let topic_bytes = FixedBytes::<32>::from_slice(topic0.as_slice());
-
-                        match topic_bytes {
-                        PROOF_VERIFICATION_RESPONSE_TOPIC => {
-                            let sleep_time = 500;
-                            info!("Received Proof Verification response event");
-                            info!("Artificial sleep in anvil dev mode {}ms", sleep_time);
-                            tokio::time::sleep(tokio::time::Duration::from_millis(sleep_time)).await;
-                            RelayerEventData::Generic(GenericEventData::EventLogFromGw   {
-                                log: event_log
-                            })
-                            },
-                            PUBLIC_DECRYPTION_RESPONSE_TOPIC => {
-                                info!("Received Decryption response event");
-                                RelayerEventData::Generic(GenericEventData::EventLogFromGw   {
-                                            log: event_log
-                                        })
-
-                                },
-                                USER_DECRYPTION_RESPONSE_TOPIC => {
-                                    info!("Received Decryption response event");
-                                    RelayerEventData::Generic(GenericEventData::EventLogFromGw   {
-                                                log: event_log
-                                            })
-
-                                    },
-                            _ => {
-                                info!("Unknown event topic: 0x{}", hex::encode(topic0));
-                                continue; // Skip unknown events
-                            }
-                        }
-                    } else {
-                        error!("Event log missing topic0");
-                        continue;
-                    };
-
                     let event = RelayerEvent::new(
-                        id,
+                        orchestrator.new_request_id(),
                         ApiVersion {
                             category: ApiCategory::PRODUCTION,
                             number: 1,
                         },
-                        event_data,
+                        RelayerEventData::Generic(GenericEventData::EventLogFromGw   {
+                                                log: event_log
+                                            }),
                     );
 
                     orchestrator.dispatch_event(event).await.unwrap_or_else(|e| {
