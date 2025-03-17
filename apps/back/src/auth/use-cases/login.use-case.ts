cfec337e7d49fe50cbf323afc35c6cf1509bf698
Alessandro Vitali <alessandro.vitali@zama.ai>
2025-03-17 12:32:26 +0100

    feat: soft delete user
    Enable soft delete for users.
    Note: currently there is no GraphQL endpoint to delete a user

--- apps/back/src/auth/use-cases/login.use-case.ts
+++ apps/back/src/auth/use-cases/login.use-case.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import type { AppError, UseCase } from 'utils'
-import { Task } from 'utils'
+import { isNotFoundError, Task, unauthorizedError } from 'utils'
 import { UserRepository } from '#users/domain/repositories/user.repository.js'
 import { type UserProps } from '#users/domain/entities/user.js'
 import { JwtPayload } from '../interfaces/jwt-payload.js'
@@ -23,15 +23,21 @@ export class LogIn
     email: string
     password: string
   }): Task<{ user: UserProps; token: string }, AppError> {
-    return this.userRepository
-      .findByEmail(input.email)
-      .chain(user => user.checkPassword(input.password).async())
-      .map(user => ({
-        token: this.jwtService.sign({
-          sub: user.id.value,
-          email: user.email,
-        } satisfies JwtPayload),
-        user: user.toJSON(),
-      }))
+    return (
+      this.userRepository
+        .findByEmail(input.email)
+        .chain(user => user.checkPassword(input.password).async())
+        .map(user => ({
+          token: this.jwtService.sign({
+            sub: user.id.value,
+            email: user.email,
+          } satisfies JwtPayload),
+          user: user.toJSON(),
+        }))
+        // Note: We remap our error to prevent exposing if the user exists or not
+        .mapError(error => {
+          return isNotFoundError(error) ? unauthorizedError() : error
+        })
+    )
   }
 }
