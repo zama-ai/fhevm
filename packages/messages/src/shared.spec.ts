b1f58c2668019841dc4bc610727d86d677e00bfc
Alessandro Vitali <alessandro.vitali@zama.ai>
2025-03-19 15:31:24 +0100

    feat: improve chain id

--- packages/messages/src/shared.spec.ts
+++ packages/messages/src/shared.spec.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, test } from 'vitest'
-import { meta, metaFactory, requestId } from './shared.js'
+import { chainId, meta, metaFactory, requestId } from './shared.js'
 import { z } from 'zod'
 import { faker } from '@faker-js/faker'
 
@@ -58,5 +58,44 @@ describe('metaFactory', () => {
     test('that define the right meta field', () => {
       expect(event.meta).toEqual({ correlationId })
     })
+  })
+})
+
+describe('chainId', () => {
+  test('should accept a number as a string', () => {
+    const value = faker.string.numeric(5)
+    const result = chainId.safeParse(value)
+    if (!result.success) {
+      console.log(`failed to parse "${value}": ${JSON.stringify(result)}`)
+    }
+    expect(result.success).toBe(true)
+  })
+
+  test('should accept a hex-decimal string', () => {
+    const value = faker.string.hexadecimal({ length: 5 })
+    const result = chainId.safeParse(value)
+    if (!result.success) {
+      console.log(`failed to parse "${value}": ${JSON.stringify(result)}`)
+    }
+    expect(result.success).toBe(true)
+  })
+
+  test('should accept a positive integer', () => {
+    const value = faker.number.int({ min: 1 })
+    const result = chainId.safeParse(value)
+    if (!result.success) {
+      console.log(`failed to parse ${value}: ${JSON.stringify(result)}`)
+    }
+    expect(result.success).toBe(true)
+  })
+
+  test('should reject a generic string', () => {
+    expect(chainId.safeParse(faker.string.alphanumeric()).success).toBe(false)
+  })
+
+  test('should reject a negative integer', () => {
+    expect(
+      chainId.safeParse(faker.number.int({ min: -Infinity, max: -1 })).success,
+    ).toBe(false)
   })
 })
