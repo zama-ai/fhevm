a551f5cc5cbb5d0083dac2f5c64689f456f0301e
Roman <56846628+RomanBredehoft@users.noreply.github.com>
2025-03-31 18:20:13 +0200

    chore: add logs for addNetworks (#246)

--- add-httpz-networks.sh
+++ add-httpz-networks.sh
@@ -5,4 +5,7 @@ npx hardhat clean
 npx hardhat compile
 
 # Add L1 networks to HTTPZ contract
+echo "Register networks to HTTPZ contract"
 npx hardhat task:addNetworksToHttpz
+
+echo "Networks registration done !"
