0645fc59a32e32254c2d21b55438c5e92006caff
Roman <56846628+RomanBredehoft@users.noreply.github.com>
2025-04-01 10:20:22 +0200

    chore: make addNetwork task consider HTTPZ_ADDRESS as env var (#247)
    * chore: make addNetwork task consider HTTPZ_ADDRESS as env var

--- tasks/addNetworks.ts
+++ tasks/addNetworks.ts
@@ -1,36 +1,45 @@
 import dotenv from "dotenv";
 import fs from "fs";
-import { task } from "hardhat/config";
+import { task, types } from "hardhat/config";
 
 import { getRequiredEnvVar } from "./utils/loadVariables";
 
 // Add L1 networks metadata to the HTTPZ contract
-task("task:addNetworksToHttpz").setAction(async function (_, { ethers }) {
-  const deployerPrivateKey = getRequiredEnvVar("DEPLOYER_PRIVATE_KEY");
-  const numNetworks = parseInt(getRequiredEnvVar("NUM_NETWORKS"));
-  const deployer = new ethers.Wallet(deployerPrivateKey).connect(ethers.provider);
+// Note: Internal HTTPZ address is defined in the `addresses/` directory. It should be used
+// for local testing. By default, we use the HTTPZ_ADDRESS env var, as done in deployment
+task("task:addNetworksToHttpz")
+  .addParam("useInternalHttpzAddress", "If internal HTTPZ address should be used", false, types.boolean)
+  .setAction(async function (taskArgs, { ethers }) {
+    const deployerPrivateKey = getRequiredEnvVar("DEPLOYER_PRIVATE_KEY");
+    const numNetworks = parseInt(getRequiredEnvVar("NUM_NETWORKS"));
+    const deployer = new ethers.Wallet(deployerPrivateKey).connect(ethers.provider);
 
-  // Parse the L1 network
-  const layer1Networks = [];
-  for (let idx = 0; idx < numNetworks; idx++) {
-    layer1Networks.push({
-      chainId: getRequiredEnvVar(`NETWORK_CHAIN_ID_${idx}`),
-      httpzExecutor: getRequiredEnvVar(`NETWORK_HTTPZ_EXECUTOR_${idx}`),
-      aclAddress: getRequiredEnvVar(`NETWORK_ACL_ADDRESS_${idx}`),
-      name: getRequiredEnvVar(`NETWORK_NAME_${idx}`),
-      website: getRequiredEnvVar(`NETWORK_WEBSITE_${idx}`),
-    });
-  }
+    // Parse the L1 network
+    const layer1Networks = [];
+    for (let idx = 0; idx < numNetworks; idx++) {
+      layer1Networks.push({
+        chainId: getRequiredEnvVar(`NETWORK_CHAIN_ID_${idx}`),
+        httpzExecutor: getRequiredEnvVar(`NETWORK_HTTPZ_EXECUTOR_${idx}`),
+        aclAddress: getRequiredEnvVar(`NETWORK_ACL_ADDRESS_${idx}`),
+        name: getRequiredEnvVar(`NETWORK_NAME_${idx}`),
+        website: getRequiredEnvVar(`NETWORK_WEBSITE_${idx}`),
+      });
+    }
 
-  const parsedEnvHttpz = dotenv.parse(fs.readFileSync("addresses/.env.httpz"));
-  const proxyAddress = parsedEnvHttpz.HTTPZ_ADDRESS;
+    let proxyAddress: string;
+    if (taskArgs.useInternalHttpzAddress) {
+      const parsedEnvHttpz = dotenv.parse(fs.readFileSync("addresses/.env.httpz"));
+      proxyAddress = parsedEnvHttpz.HTTPZ_ADDRESS;
+    } else {
+      proxyAddress = getRequiredEnvVar("HTTPZ_ADDRESS");
+    }
 
-  // Add L1 networks
-  const httpz = await ethers.getContractAt("HTTPZ", proxyAddress, deployer);
-  for (const network of layer1Networks) {
-    await httpz.addNetwork(network);
-  }
+    // Add L1 networks
+    const httpz = await ethers.getContractAt("HTTPZ", proxyAddress, deployer);
+    for (const network of layer1Networks) {
+      await httpz.addNetwork(network);
+    }
 
-  console.log("In HTTPZ contract:", proxyAddress, "\n");
-  console.log("Added L1 networks:", layer1Networks, "\n");
-});
+    console.log("In HTTPZ contract:", proxyAddress, "\n");
+    console.log("Added L1 networks:", layer1Networks, "\n");
+  });
