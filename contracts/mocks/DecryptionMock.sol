af33fec89a425a8b9cb1e8143b77bbc2a1f9e09c
Isaac Herrera <isaac.herrera@zama.ai>
2025-04-23 16:58:45 +0200

    feat: implement script to generate mock contracts
    * feat: implement script for contracts mocking
    
    * feat: implement script to generate contract mocks
    
    * refactor: adjust comments and simplify some code lines
    
    * refactor: add some exception raising and adjust a few comments
    
    * build: add makefile target for mock contracts generation
    
    * refactor: change the default arguments logic and improve naming
    
    * refactor: align with bindings script and introduce CLI feature with OOP
    
    * ci: add action for mock contracts check and include check in pre-commit hook
    
    * feat: redefine function for memory located arguments
    
    * refactor: implement mock contracts script based on antlr parser
    
    * build: modify make targets to use js script
    
    * chore: remove python script
    
    * chore: move mocks into contracts directory
    
    * chore: adjust some comments and code lines
    
    * fix: rollback tmp directory
    
    * chore: remove obsolete named mock contracts
    
    * refactor: remove function inputs propagation and use defaults
    
    * fix: remove obsolete httpz_gateway_rust_bindings
    
    * refactor: include changes for future-proof suggestions
    
    * refactor: improve getParameterType by avoiding extra arguments for arrays

---
+++ contracts/mocks/DecryptionMock.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.24;
+import "../shared/Structs.sol";
+
+contract DecryptionMock {
+    struct RequestValidity {
+        uint256 startTimestamp;
+        uint256 durationDays;
+    }
+
+    event PublicDecryptionRequest(uint256 indexed publicDecryptionId, SnsCiphertextMaterial[] snsCtMaterials);
+
+    event PublicDecryptionResponse(uint256 indexed publicDecryptionId, bytes decryptedResult, bytes[] signatures);
+
+    event UserDecryptionRequest(
+        uint256 indexed userDecryptionId,
+        SnsCiphertextMaterial[] snsCtMaterials,
+        address userAddress,
+        bytes publicKey
+    );
+
+    event UserDecryptionResponse(uint256 indexed userDecryptionId, bytes[] reencryptedShares, bytes[] signatures);
+
+    function publicDecryptionRequest(bytes32[] calldata ctHandles) public {
+        uint256 publicDecryptionId;
+        SnsCiphertextMaterial[] memory snsCtMaterials = new SnsCiphertextMaterial[](1);
+        emit PublicDecryptionRequest(publicDecryptionId, snsCtMaterials);
+    }
+
+    function publicDecryptionResponse(
+        uint256 publicDecryptionId,
+        bytes calldata decryptedResult,
+        bytes calldata signature
+    ) public {
+        uint256 publicDecryptionId;
+        bytes memory decryptedResult;
+        bytes[] memory signatures = new bytes[](1);
+        emit PublicDecryptionResponse(publicDecryptionId, decryptedResult, signatures);
+    }
+
+    function userDecryptionRequest(
+        CtHandleContractPair[] calldata ctHandleContractPairs,
+        RequestValidity calldata requestValidity,
+        uint256 contractsChainId,
+        address[] calldata contractAddresses,
+        address userAddress,
+        bytes calldata publicKey,
+        bytes calldata signature
+    ) external {
+        uint256 userDecryptionId;
+        SnsCiphertextMaterial[] memory snsCtMaterials = new SnsCiphertextMaterial[](1);
+        address userAddress;
+        bytes memory publicKey;
+        emit UserDecryptionRequest(userDecryptionId, snsCtMaterials, userAddress, publicKey);
+    }
+
+    function delegatedUserDecryptionRequest(
+        CtHandleContractPair[] calldata ctHandleContractPairs,
+        RequestValidity calldata requestValidity,
+        DelegationAccounts calldata delegationAccounts,
+        uint256 contractsChainId,
+        address[] calldata contractAddresses,
+        bytes calldata publicKey,
+        bytes calldata signature
+    ) external {
+        uint256 userDecryptionId;
+        SnsCiphertextMaterial[] memory snsCtMaterials = new SnsCiphertextMaterial[](1);
+        address userAddress;
+        bytes memory publicKey;
+        emit UserDecryptionRequest(userDecryptionId, snsCtMaterials, userAddress, publicKey);
+    }
+
+    function userDecryptionResponse(
+        uint256 userDecryptionId,
+        bytes calldata reencryptedShare,
+        bytes calldata signature
+    ) external {
+        uint256 userDecryptionId;
+        bytes[] memory reencryptedShares = new bytes[](1);
+        bytes[] memory signatures = new bytes[](1);
+        emit UserDecryptionResponse(userDecryptionId, reencryptedShares, signatures);
+    }
+}
