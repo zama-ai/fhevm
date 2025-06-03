3559f4d2d34777d50667fca9e5a3e71872cc4ffa
Isaac Herrera <isaac.herrera@zama.ai>
2025-06-03 20:21:18 +0200

    test(gateway-contracts): include upgrade test for enhanced storage layout case

---
+++ gateway-contracts/contracts/examples/GatewayConfigV3Example.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.24;
+
+import { Ownable2StepUpgradeable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import "../shared/Pausable.sol";
+
+contract GatewayConfigV3Example is Ownable2StepUpgradeable, UUPSUpgradeable, Pausable {
+    /// @notice Name of the contract
+    string private constant CONTRACT_NAME = "GatewayConfig";
+
+    /// @notice Version of the contract
+    uint256 private constant MAJOR_VERSION = 0;
+    uint256 private constant MINOR_VERSION = 3;
+    uint256 private constant PATCH_VERSION = 0;
+
+    struct ProtocolMetadataV2 {
+        string name;
+        string website;
+        string newField;
+    }
+
+    /// @custom:storage-location erc7201:fhevm_gateway.storage.GatewayConfig
+    struct GatewayConfigStorage {
+        address pauser;
+        mapping(address kmsTxSenderAddress => bool isKmsTxSender) _isKmsTxSender;
+        mapping(address kmsSignerAddress => bool isKmsSigner) _isKmsSigner;
+        mapping(address coprocessorTxSenderAddress => bool isCoprocessorTxSender) _isCoprocessorTxSender;
+        mapping(address coprocessorSignerAddress => bool isCoprocessorSigner) _isCoprocessorSigner;
+        mapping(uint256 chainId => bool isRegistered) _isHostChainRegistered;
+        ProtocolMetadata protocolMetadata;
+        mapping(address kmsTxSenderAddress => KmsNode kmsNode) kmsNodes;
+        address[] kmsTxSenderAddresses;
+        address[] kmsSignerAddresses;
+        uint256 mpcThreshold;
+        uint256 publicDecryptionThreshold;
+        uint256 userDecryptionThreshold;
+        mapping(address coprocessorTxSenderAddress => Coprocessor coprocessor) coprocessors;
+        address[] coprocessorTxSenderAddresses;
+        address[] coprocessorSignerAddresses;
+        HostChain[] hostChains;
+        // New state variables added in the upgraded version
+        ProtocolMetadataV2 protocolMetadataV2;
+    }
+
+    bytes32 private constant GATEWAY_CONFIG_STORAGE_LOCATION =
+        0x86d3070a8993f6b209bee6185186d38a07fce8bbd97c750d934451b72f35b400;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * @notice  Initializes the contract.
+     * @dev This function needs to be public in order to be called by the UUPS proxy.
+     */
+    function initialize(string calldata newField) public virtual reinitializer(3) {
+        __Ownable_init(owner());
+        __Pausable_init();
+        _migrate(newField);
+    }
+
+    function getProtocolMetadata() external view virtual returns (ProtocolMetadataV2 memory) {
+        GatewayConfigStorage storage $ = _getGatewayConfigStorage();
+        return $.protocolMetadataV2;
+    }
+
+    function getCoprocessor(address coprocessorTxSenderAddress) external view virtual returns (Coprocessor memory) {
+        GatewayConfigStorage storage $ = _getGatewayConfigStorage();
+        return $.coprocessors[coprocessorTxSenderAddress];
+    }
+
+    /// @notice Getter for the name and version of the contract
+    /// @return string representing the name and the version of the contract
+    function getVersion() external pure virtual returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    CONTRACT_NAME,
+                    " v",
+                    Strings.toString(MAJOR_VERSION),
+                    ".",
+                    Strings.toString(MINOR_VERSION),
+                    ".",
+                    Strings.toString(PATCH_VERSION)
+                )
+            );
+    }
+
+    function _migrate(string calldata newField) private {
+        GatewayConfigStorage storage $ = _getGatewayConfigStorage();
+        $.protocolMetadataV2 = ProtocolMetadataV2({
+            name: $.protocolMetadata.name,
+            website: $.protocolMetadata.website,
+            newField: newField
+        });
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    function _authorizeUpgrade(address _newImplementation) internal virtual override {}
+
+    function _getGatewayConfigStorage() internal pure returns (GatewayConfigStorage storage $) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            $.slot := GATEWAY_CONFIG_STORAGE_LOCATION
+        }
+    }
+}
--- gateway-contracts/test/upgrades/upgrades.ts
+++ gateway-contracts/test/upgrades/upgrades.ts
@@ -1,135 +1,166 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-import dotenv from "dotenv";
 import { Wallet } from "ethers";
-import fs from "fs";
 import { ethers, upgrades } from "hardhat";
 
-import { createAndFundRandomWallet } from "../utils";
+import {
+  CiphertextCommitsUpgradedExample__factory,
+  CiphertextCommits__factory,
+  DecryptionUpgradedExample__factory,
+  Decryption__factory,
+  GatewayConfigUpgradedExample__factory,
+  GatewayConfigV3Example__factory,
+  GatewayConfig__factory,
+  InputVerificationUpgradedExample__factory,
+  InputVerification__factory,
+  KmsManagementUpgradedExample__factory,
+  KmsManagement__factory,
+  MultichainAclUpgradedExample__factory,
+  MultichainAcl__factory,
+} from "../../typechain-types";
+import { createAndFundRandomWallet, loadTestVariablesFixture, toValues } from "../utils";
 
 describe("Upgrades", function () {
+  let owner: Wallet;
+  let emptyUUPSFactory: any;
+  let ciphertextCommitsFactoryV1: CiphertextCommits__factory;
+  let ciphertextCommitsFactoryV2: CiphertextCommitsUpgradedExample__factory;
+  let decryptionFactoryV1: Decryption__factory;
+  let decryptionFactoryV2: DecryptionUpgradedExample__factory;
+  let gatewayConfigFactoryV1: GatewayConfig__factory;
+  let gatewayConfigFactoryV2: GatewayConfigUpgradedExample__factory;
+  let gatewayConfigFactoryV3: GatewayConfigV3Example__factory;
+  let inputVerificationFactoryV1: InputVerification__factory;
+  let inputVerificationFactoryV2: InputVerificationUpgradedExample__factory;
+  let kmsManagementFactoryV1: KmsManagement__factory;
+  let kmsManagementFactoryV2: KmsManagementUpgradedExample__factory;
+  let multichainAclFactoryV1: MultichainAcl__factory;
+  let multichainAclFactoryV2: MultichainAclUpgradedExample__factory;
+
   before(async function () {
-    this.owner = new Wallet(process.env.DEPLOYER_PRIVATE_KEY!).connect(ethers.provider);
-    this.emptyUUPSFactory = await ethers.getContractFactory("EmptyUUPSProxy", this.owner);
-    this.multichainAclFactory = await ethers.getContractFactory("MultichainAcl", this.owner);
-    this.multichainAclFactoryUpgraded = await ethers.getContractFactory("MultichainAclUpgradedExample", this.owner);
-    this.ciphertextCommitsFactory = await ethers.getContractFactory("CiphertextCommits", this.owner);
-    this.ciphertextCommitsFactoryUpgraded = await ethers.getContractFactory(
-      "CiphertextCommitsUpgradedExample",
-      this.owner,
-    );
-    this.decryptionFactory = await ethers.getContractFactory("Decryption", this.owner);
-    this.decryptionFactoryUpgraded = await ethers.getContractFactory("DecryptionUpgradedExample", this.owner);
-    this.gatewayConfigFactory = await ethers.getContractFactory("GatewayConfig", this.owner);
-    this.gatewayConfigFactoryUpgraded = await ethers.getContractFactory("GatewayConfigUpgradedExample", this.owner);
-    this.kmsManagementFactory = await ethers.getContractFactory("KmsManagement", this.owner);
-    this.kmsManagementFactoryUpgraded = await ethers.getContractFactory("KmsManagementUpgradedExample", this.owner);
-    this.inputVerificationFactory = await ethers.getContractFactory("InputVerification", this.owner);
-    this.inputVerificationFactoryUpgraded = await ethers.getContractFactory(
-      "InputVerificationUpgradedExample",
-      this.owner,
-    );
+    owner = new Wallet(process.env.DEPLOYER_PRIVATE_KEY!).connect(ethers.provider);
+    emptyUUPSFactory = await ethers.getContractFactory("EmptyUUPSProxy", owner);
+
+    ciphertextCommitsFactoryV1 = await ethers.getContractFactory("CiphertextCommits", owner);
+    ciphertextCommitsFactoryV2 = await ethers.getContractFactory("CiphertextCommitsUpgradedExample", owner);
+
+    decryptionFactoryV1 = await ethers.getContractFactory("Decryption", owner);
+    decryptionFactoryV2 = await ethers.getContractFactory("DecryptionUpgradedExample", owner);
+
+    gatewayConfigFactoryV1 = await ethers.getContractFactory("GatewayConfig", owner);
+    gatewayConfigFactoryV2 = await ethers.getContractFactory("GatewayConfigUpgradedExample", owner);
+    gatewayConfigFactoryV3 = await ethers.getContractFactory("GatewayConfigV3Example", owner);
+
+    inputVerificationFactoryV1 = await ethers.getContractFactory("InputVerification", owner);
+    inputVerificationFactoryV2 = await ethers.getContractFactory("InputVerificationUpgradedExample", owner);
+
+    kmsManagementFactoryV1 = await ethers.getContractFactory("KmsManagement", owner);
+    kmsManagementFactoryV2 = await ethers.getContractFactory("KmsManagementUpgradedExample", owner);
+
+    multichainAclFactoryV1 = await ethers.getContractFactory("MultichainAcl", owner);
+    multichainAclFactoryV2 = await ethers.getContractFactory("MultichainAclUpgradedExample", owner);
   });
 
-  it("deploy upgradable MultichainAcl", async function () {
-    const nonceBef = await ethers.provider.getTransactionCount(this.owner);
-    const emptyUUPS = await upgrades.deployProxy(this.emptyUUPSFactory, [this.owner.address], {
+  it("Should deploy upgradable MultichainAcl", async function () {
+    const nonceBef = await ethers.provider.getTransactionCount(owner);
+    const emptyUUPS = await upgrades.deployProxy(emptyUUPSFactory, [owner.address], {
       initializer: "initialize",
       kind: "uups",
     });
-    const multichainAcl = await upgrades.upgradeProxy(emptyUUPS, this.multichainAclFactory);
+    const multichainAcl = await upgrades.upgradeProxy(emptyUUPS, multichainAclFactoryV1);
     await multichainAcl.waitForDeployment();
     const ownerBef = await multichainAcl.owner();
     expect(await multichainAcl.getVersion()).to.equal("MultichainAcl v0.1.0");
-    const multichainAcl2 = await upgrades.upgradeProxy(multichainAcl, this.multichainAclFactoryUpgraded);
+    const multichainAcl2 = await upgrades.upgradeProxy(multichainAcl, multichainAclFactoryV2);
     await multichainAcl2.waitForDeployment();
     const ownerAft = await multichainAcl2.owner();
     expect(ownerBef).to.equal(ownerAft);
     expect(await multichainAcl2.getVersion()).to.equal("MultichainAcl v0.2.0");
     const multichainAclAddress = ethers.getCreateAddress({
-      from: this.owner.address,
+      from: owner.address,
       nonce: nonceBef, // using nonce of nonceBef instead of nonceBef+1 here, since the original implementation has already been deployer during the setup phase, and hardhat-upgrades plugin is able to detect this and not redeploy twice same contract
     });
     expect(multichainAclAddress).to.equal(await multichainAcl2.getAddress());
   });
 
-  it("deploy upgradable CiphertextCommits", async function () {
-    const emptyUUPS = await upgrades.deployProxy(this.emptyUUPSFactory, [this.owner.address], {
+  it("Should deploy upgradable CiphertextCommits", async function () {
+    const emptyUUPS = await upgrades.deployProxy(emptyUUPSFactory, [owner.address], {
       initializer: "initialize",
       kind: "uups",
     });
-    const ciphertextCommits = await upgrades.upgradeProxy(emptyUUPS, this.ciphertextCommitsFactory);
+    const ciphertextCommits = await upgrades.upgradeProxy(emptyUUPS, ciphertextCommitsFactoryV1);
     await ciphertextCommits.waitForDeployment();
     expect(await ciphertextCommits.getVersion()).to.equal("CiphertextCommits v0.1.0");
-    const ciphertextCommits2 = await upgrades.upgradeProxy(ciphertextCommits, this.ciphertextCommitsFactoryUpgraded);
+    const ciphertextCommits2 = await upgrades.upgradeProxy(ciphertextCommits, ciphertextCommitsFactoryV2);
     await ciphertextCommits2.waitForDeployment();
     expect(await ciphertextCommits2.getVersion()).to.equal("CiphertextCommits v0.2.0");
   });
 
-  it("deploy upgradable Decryption", async function () {
-    const emptyUUPS = await upgrades.deployProxy(this.emptyUUPSFactory, [this.owner.address], {
+  it("Should deploy upgradable Decryption", async function () {
+    const emptyUUPS = await upgrades.deployProxy(emptyUUPSFactory, [owner.address], {
       initializer: "initialize",
       kind: "uups",
     });
-    const decryption = await upgrades.upgradeProxy(emptyUUPS, this.decryptionFactory);
+    const decryption = await upgrades.upgradeProxy(emptyUUPS, decryptionFactoryV1);
     await decryption.waitForDeployment();
     expect(await decryption.getVersion()).to.equal("Decryption v0.1.0");
-    const decryption2 = await upgrades.upgradeProxy(decryption, this.decryptionFactoryUpgraded);
+    const decryption2 = await upgrades.upgradeProxy(decryption, decryptionFactoryV2);
     await decryption2.waitForDeployment();
     expect(await decryption2.getVersion()).to.equal("Decryption v0.2.0");
   });
 
-  it("deploy upgradable GatewayConfig", async function () {
-    const emptyUUPS = await upgrades.deployProxy(this.emptyUUPSFactory, [this.owner.address], {
+  it("Should deploy upgradable GatewayConfig", async function () {
+    const emptyUUPS = await upgrades.deployProxy(emptyUUPSFactory, [owner.address], {
       initializer: "initialize",
       kind: "uups",
     });
-    const gatewayConfig = await upgrades.upgradeProxy(emptyUUPS, this.gatewayConfigFactory);
+    const gatewayConfig = await upgrades.upgradeProxy(emptyUUPS, gatewayConfigFactoryV1);
     await gatewayConfig.waitForDeployment();
     expect(await gatewayConfig.getVersion()).to.equal("GatewayConfig v0.1.0");
-    const gatewayConfig2 = await upgrades.upgradeProxy(gatewayConfig, this.gatewayConfigFactoryUpgraded);
+    const gatewayConfig2 = await upgrades.upgradeProxy(gatewayConfig, gatewayConfigFactoryV2);
     await gatewayConfig2.waitForDeployment();
     expect(await gatewayConfig2.getVersion()).to.equal("GatewayConfig v0.2.0");
   });
 
-  it("deploy upgradable KmsManagement", async function () {
-    const emptyUUPS = await upgrades.deployProxy(this.emptyUUPSFactory, [this.owner.address], {
+  it("Should deploy upgradable KmsManagement", async function () {
+    const emptyUUPS = await upgrades.deployProxy(emptyUUPSFactory, [owner.address], {
       initializer: "initialize",
       kind: "uups",
     });
-    const kmsManagement = await upgrades.upgradeProxy(emptyUUPS, this.kmsManagementFactory);
+    const kmsManagement = await upgrades.upgradeProxy(emptyUUPS, kmsManagementFactoryV1);
     await kmsManagement.waitForDeployment();
     expect(await kmsManagement.getVersion()).to.equal("KmsManagement v0.1.0");
-    const kmsManagement2 = await upgrades.upgradeProxy(kmsManagement, this.kmsManagementFactoryUpgraded);
+    const kmsManagement2 = await upgrades.upgradeProxy(kmsManagement, kmsManagementFactoryV2);
     await kmsManagement2.waitForDeployment();
     expect(await kmsManagement2.getVersion()).to.equal("KmsManagement v0.2.0");
   });
 
-  it("deploy upgradable InputVerification", async function () {
-    const emptyUUPS = await upgrades.deployProxy(this.emptyUUPSFactory, [this.owner.address], {
+  it("Should deploy upgradable InputVerification", async function () {
+    const emptyUUPS = await upgrades.deployProxy(emptyUUPSFactory, [owner.address], {
       initializer: "initialize",
       kind: "uups",
     });
-    const inputVerification = await upgrades.upgradeProxy(emptyUUPS, this.inputVerificationFactory);
+    const inputVerification = await upgrades.upgradeProxy(emptyUUPS, inputVerificationFactoryV1);
     await inputVerification.waitForDeployment();
     expect(await inputVerification.getVersion()).to.equal("InputVerification v0.1.0");
-    const inputVerification2 = await upgrades.upgradeProxy(inputVerification, this.inputVerificationFactoryUpgraded);
+    const inputVerification2 = await upgrades.upgradeProxy(inputVerification, inputVerificationFactoryV2);
     await inputVerification2.waitForDeployment();
     expect(await inputVerification2.getVersion()).to.equal("InputVerification v0.2.0");
   });
 
-  it("original owner upgrades the original GatewayConfig and transfer ownership", async function () {
+  it("Should allow original owner to upgrade the original GatewayConfig and transfer ownership", async function () {
     // Create a new gateway contract in order to avoid upgrading the original one and thus break
-    // some tests if it nos re-compiled in the mean time
-    const emptyUUPS = await upgrades.deployProxy(this.emptyUUPSFactory, [this.owner.address], {
+    // some tests if it's not re-compiled in the mean time
+    const emptyUUPS = await upgrades.deployProxy(emptyUUPSFactory, [owner.address], {
       initializer: "initialize",
       kind: "uups",
     });
-    const originalGatewayConfig = await upgrades.upgradeProxy(emptyUUPS, this.gatewayConfigFactory);
+    const originalGatewayConfig = await upgrades.upgradeProxy(emptyUUPS, gatewayConfigFactoryV1);
     await originalGatewayConfig.waitForDeployment();
     expect(await originalGatewayConfig.getVersion()).to.equal("GatewayConfig v0.1.0");
 
     const originalGatewayConfigAddress = await originalGatewayConfig.getAddress();
-    const deployer = this.owner;
+    const deployer = owner;
 
     const newGatewayConfigFactoryUpgraded = await ethers.getContractFactory("GatewayConfigUpgradedExample", deployer);
     const gatewayConfig2 = await upgrades.upgradeProxy(originalGatewayConfig, newGatewayConfigFactoryUpgraded);
@@ -152,5 +183,32 @@ describe("Upgrades", function () {
 
     await gatewayConfig3.waitForDeployment();
     expect(await gatewayConfig3.getVersion()).to.equal("GatewayConfig v0.3.0");
+  });
+
+  it("Should maintain state consistency after upgrade", async function () {
+    const fixtureData = await loadFixture(loadTestVariablesFixture);
+    const { gatewayConfig } = fixtureData;
+
+    // Check that GatewayConfig is at version 0.1.0
+    expect(await gatewayConfig.getVersion()).to.equal("GatewayConfig v0.1.0");
+
+    // Check that the protocol metadata is correct
+    const metadata = await gatewayConfig.getProtocolMetadata();
+    const protocolMetadata = { name: "Protocol", website: "https://protocol.com" };
+    expect(metadata).to.deep.equal(toValues(protocolMetadata));
+
+    // Upgrade the GatewayConfig contract to version 0.3.0
+    const protocolNewField = "Protocol new field";
+    const gatewayConfigV2 = await upgrades.upgradeProxy(gatewayConfig, gatewayConfigFactoryV3, {
+      call: { fn: "initialize", args: [protocolNewField] },
+    });
+    await gatewayConfigV2.waitForDeployment();
+    expect(await gatewayConfigV2.getVersion()).to.equal("GatewayConfig v0.3.0");
+    expect(await gatewayConfig.getAddress()).to.equal(await gatewayConfigV2.getAddress());
+
+    // Check that the protocol metadata is still correct after the upgrade
+    const protocolMetadataV2 = { name: "Protocol", website: "https://protocol.com", newField: protocolNewField };
+    const metadataAfterUpgrade = await gatewayConfigV2.getProtocolMetadata();
+    expect(metadataAfterUpgrade).to.deep.equal(toValues(protocolMetadataV2));
   });
 });
