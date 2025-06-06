e9675fad2323ee9a12b738f4cc00eff68890568d
Simon Eudeline <simon.eudeline@zama.ai>
2025-06-06 14:53:27 +0200

    chore(kms-connector): add timestamp to decryption response tables

--- kms-connector/connector-db/migrations/20250604141515_decryption_response.sql
+++ kms-connector/connector-db/migrations/20250604141515_decryption_response.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS public_decryption_responses (
     decryption_id BYTEA NOT NULL,
     decrypted_result BYTEA NOT NULL,
     signatures BYTEA NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (decryption_id)
 );
 
@@ -9,5 +10,6 @@ CREATE TABLE IF NOT EXISTS user_decryption_responses (
     decryption_id BYTEA NOT NULL,
     user_decrypted_shares BYTEA NOT NULL,
     signatures BYTEA NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (decryption_id)
 );
