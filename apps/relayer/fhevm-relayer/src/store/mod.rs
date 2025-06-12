042c7964647c4f7b2abc611f372b437baa011ec1
manoranjith <manoranjith.ponnuraj@zama.ai>
2025-06-12 19:53:53 +0200

    feat: add block number store

--- apps/relayer/fhevm-relayer/src/store/mod.rs
+++ apps/relayer/fhevm-relayer/src/store/mod.rs
@@ -5,10 +5,12 @@
 //! 2. A data translation layer for storing different kinds of data . Eg: EventStore.
 
 // Export the store components and traits
+mod block_number;
 mod event;
 pub mod key_value_db;
 mod pub_decrypt_cache;
 
-// Re-export EventStore interface easier access
+// Re-export for easier access
+pub use block_number::BlockNumberStore;
 pub use event::EventStore;
 pub use pub_decrypt_cache::PublicDecryptCacheStore;
