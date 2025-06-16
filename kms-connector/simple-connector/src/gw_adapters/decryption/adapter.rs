5dcd43f6ec7304bbbf3302401fcd54c98011ec41
Simon Eudeline <simon.eudeline@zama.ai>
2025-06-16 09:49:55 +0200

    fix(kms-connector): infinite gas limit + add tx retry

--- kms-connector/simple-connector/src/gw_adapters/decryption/adapter.rs
+++ kms-connector/simple-connector/src/gw_adapters/decryption/adapter.rs
@@ -1,11 +1,19 @@
 use crate::error::{Error, Result};
 use alloy::{
+    network::Ethereum,
     primitives::{Address, Bytes, U256},
-    providers::Provider,
+    providers::{PendingTransactionBuilder, Provider},
+    rpc::types::TransactionRequest,
 };
 use fhevm_gateway_rust_bindings::decryption::Decryption;
-use std::sync::Arc;
-use tracing::{debug, info};
+use std::{sync::Arc, time::Duration};
+use tracing::{debug, info, warn};
+
+/// The max value for the `gas_limit` of a transaction.
+const INFINITE_GAS_LIMIT: u64 = u64::MAX;
+
+/// The time to wait between two transactions attempt.
+const TX_INTERVAL: Duration = Duration::from_secs(3);
 
 /// Adapter for decryption operations
 #[derive(Clone)]
@@ -57,28 +65,12 @@ impl<P: Provider + Clone> DecryptionAdapter<P> {
 
         let contract = Decryption::new(self.decryption_address, self.provider.clone());
 
-        let call_builder = contract.publicDecryptionResponse(id, result, signature.into());
+        let mut call = contract
+            .publicDecryptionResponse(id, result, signature.into())
+            .into_transaction_request();
+        call.gas = Some(INFINITE_GAS_LIMIT);
+        let tx = self.send_tx_with_retry(call).await?;
 
-        // Estimate gas and add an 80% buffer
-        let estimated_gas = call_builder
-            .estimate_gas()
-            .await
-            .map_err(|e| Error::Contract(e.to_string()))?;
-        let gas_limit = estimated_gas * 18 / 10; // 80% buffer
-        info!(
-            ?estimated_gas,
-            ?gas_limit,
-            "Gas estimated for public decryption response"
-        );
-
-        let mut call = call_builder.into_transaction_request();
-        call.gas = Some(gas_limit);
-
-        let tx = self
-            .provider()
-            .send_transaction(call)
-            .await
-            .map_err(|e| Error::Contract(e.to_string()))?;
         // TODO: optimize for low latency
         let receipt = tx
             .get_receipt()
@@ -118,28 +110,11 @@ impl<P: Provider + Clone> DecryptionAdapter<P> {
         let contract = Decryption::new(self.decryption_address, self.provider.clone());
 
         // Create and send transaction
-        let call_builder = contract.userDecryptionResponse(id, result, signature.into());
-
-        // Estimate gas and add an 80% buffer
-        let estimated_gas = call_builder
-            .estimate_gas()
-            .await
-            .map_err(|e| Error::Contract(e.to_string()))?;
-        let gas_limit = estimated_gas * 18 / 10; // 80% buffer
-        info!(
-            ?estimated_gas,
-            ?gas_limit,
-            "Gas estimated for user decryption response"
-        );
-
-        let mut call = call_builder.into_transaction_request();
-        call.gas = Some(gas_limit);
-
-        let tx = self
-            .provider()
-            .send_transaction(call)
-            .await
-            .map_err(|e| Error::Contract(e.to_string()))?;
+        let mut call = contract
+            .userDecryptionResponse(id, result, signature.into())
+            .into_transaction_request();
+        call.gas = Some(INFINITE_GAS_LIMIT);
+        let tx = self.send_tx_with_retry(call).await?;
 
         // TODO: optimize for low latency
         let receipt = tx
@@ -148,5 +123,29 @@ impl<P: Provider + Clone> DecryptionAdapter<P> {
             .map_err(|e| Error::Contract(e.to_string()))?;
         info!(decryption_id = ?id, "🎯 User Decryption response sent with tx receipt: {:?}", receipt);
         Ok(())
+    }
+
+    /// Sends the requested transactions with one retry.
+    async fn send_tx_with_retry(
+        &self,
+        call: TransactionRequest,
+    ) -> Result<PendingTransactionBuilder<Ethereum>> {
+        match self.provider.send_transaction(call.clone()).await {
+            Ok(tx) => Ok(tx),
+            Err(e) => {
+                warn!(
+                    "Retrying to send transaction in {}s after failure: {}",
+                    TX_INTERVAL.as_secs(),
+                    e
+                );
+
+                tokio::time::sleep(TX_INTERVAL).await;
+
+                self.provider
+                    .send_transaction(call)
+                    .await
+                    .map_err(|e| Error::Contract(e.to_string()))
+            }
+        }
     }
 }
