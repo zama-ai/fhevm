a1d875be735c4386d2973a23e1075757a664d603
Simon Eudeline <simon.eudeline@zama.ai>
2025-06-19 20:03:00 +0200

    chore(kms-connector): add tx-sender integration tests

--- kms-connector/tests/src/setup/instance.rs
+++ kms-connector/tests/src/setup/instance.rs
@@ -1,12 +1,14 @@
 use crate::setup::{
+    CHAIN_ID, DEPLOYER_PRIVATE_KEY,
     db::setup_test_db_instance,
     gw::{GatewayInstance, setup_anvil_gateway},
 };
 use alloy::{
     node_bindings::AnvilInstance,
+    primitives::ChainId,
     providers::{ProviderBuilder, WsConnect},
 };
-use connector_utils::conn::GatewayProvider;
+use connector_utils::{config::KmsWallet, conn::WalletGatewayProvider};
 use fhevm_gateway_rust_bindings::{
     decryption::Decryption::DecryptionInstance,
     gatewayconfig::GatewayConfig::GatewayConfigInstance,
@@ -23,7 +25,13 @@ pub async fn test_instance_with_db_only() -> anyhow::Result<TestInstance> {
 
 pub async fn test_instance_with_db_and_gw() -> anyhow::Result<TestInstance> {
     let anvil = setup_anvil_gateway().await?;
+    let wallet = KmsWallet::from_private_key_str(
+        DEPLOYER_PRIVATE_KEY,
+        Some(ChainId::from(*CHAIN_ID as u64)),
+    )?;
+
     let provider = ProviderBuilder::new()
+        .wallet(wallet)
         .on_ws(WsConnect::new(anvil.ws_endpoint_url()))
         .await?;
     let gw_instance = GatewayInstance::new(anvil, provider);
@@ -32,7 +40,11 @@ pub async fn test_instance_with_db_and_gw() -> anyhow::Result<TestInstance> {
     Ok(test_instance.with_gateway(gw_instance))
 }
 
+/// The integration test environment.
 pub struct TestInstance {
+    /// Use to enable tracing during tests.
+    pub _tracing_default_guard: tracing::subscriber::DefaultGuard,
+    /// Use to keep the database container running during the tests.
     pub _db_container: ContainerAsync<GenericImage>,
     pub db: Pool<Postgres>,
     pub gateway_instance: Option<GatewayInstance>,
@@ -41,13 +53,21 @@ pub struct TestInstance {
 impl TestInstance {
     /// `TestInstance` with database only.
     pub fn new(db_container: ContainerAsync<GenericImage>, db: Pool<Postgres>) -> Self {
+        // Initialize tracing for this test
+        let subscriber = tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::INFO)
+            .with_test_writer()
+            .finish();
+
         Self {
+            _tracing_default_guard: tracing::subscriber::set_default(subscriber),
             _db_container: db_container,
             db,
             gateway_instance: None,
         }
     }
 
+    /// Adds a `GatewayInstance` to the test environment.
     pub fn with_gateway(mut self, gateway_instance: GatewayInstance) -> Self {
         self.gateway_instance = Some(gateway_instance);
         self
@@ -57,19 +77,19 @@ impl TestInstance {
         &self.gateway().anvil
     }
 
-    pub fn provider(&self) -> &GatewayProvider {
+    pub fn provider(&self) -> &WalletGatewayProvider {
         &self.gateway().provider
     }
 
-    pub fn decryption_contract(&self) -> &DecryptionInstance<(), GatewayProvider> {
+    pub fn decryption_contract(&self) -> &DecryptionInstance<(), WalletGatewayProvider> {
         &self.gateway().decryption_contract
     }
 
-    pub fn gateway_config_contract(&self) -> &GatewayConfigInstance<(), GatewayProvider> {
+    pub fn gateway_config_contract(&self) -> &GatewayConfigInstance<(), WalletGatewayProvider> {
         &self.gateway().gateway_config_contract
     }
 
-    pub fn kms_management_contract(&self) -> &KmsManagementInstance<(), GatewayProvider> {
+    pub fn kms_management_contract(&self) -> &KmsManagementInstance<(), WalletGatewayProvider> {
         &self.gateway().kms_management_contract
     }
 
