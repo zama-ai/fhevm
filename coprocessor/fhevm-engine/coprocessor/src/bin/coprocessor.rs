9922727bf477fead39783148da332b6922f6bc73
Antoniu Pop <antoniu.pop@zama.ai>
2025-07-03 13:07:58 +0100

    fix(coprocessor): remove restriction on batching size

--- coprocessor/fhevm-engine/coprocessor/src/bin/coprocessor.rs
+++ coprocessor/fhevm-engine/coprocessor/src/bin/coprocessor.rs
@@ -1,10 +1,5 @@
 fn main() {
     let args = coprocessor::daemon_cli::parse_args();
-    assert!(
-        args.work_items_batch_size < args.tenant_key_cache_size,
-        "Work items batch size must be less than tenant key cache size"
-    );
-
     if args.generate_fhe_keys {
         coprocessor::generate_dump_fhe_keys();
     } else {
