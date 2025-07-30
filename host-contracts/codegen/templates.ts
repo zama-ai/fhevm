163275f75fc7bab8ac6659098131a5b17586e903
jatZama <josephandre.turk@zama.ai>
2025-07-30 14:41:18 +0200

    revert(host-contracts): revert extraData for decryption

--- host-contracts/codegen/templates.ts
+++ host-contracts/codegen/templates.ts
@@ -406,8 +406,7 @@ function generateKMSVerifierInterface(): string {
     function verifyDecryptionEIP712KMSSignatures(
         bytes32[] memory handlesList,
         bytes memory decryptedResult,
-        bytes[] memory signatures,
-        bytes memory extraData
+        bytes[] memory signatures
     ) external returns (bool);
   }
   `;
@@ -1105,9 +1104,9 @@ function generateSolidityDecryptionOracleMethods(fheTypes: AdjustedFheType[]): s
      * @dev     otherwise fake decryption results could be submitted.
      * @notice  Warning: MUST be called directly in the callback function called by the relayer.
      */
-    function checkSignatures(uint256 requestID, bytes[] memory signatures, bytes memory extraData) internal {
+    function checkSignatures(uint256 requestID, bytes[] memory signatures) internal {
         bytes32[] memory handlesList = loadRequestedHandles(requestID);
-        bool isVerified = verifySignatures(handlesList, signatures, extraData);
+        bool isVerified = verifySignatures(handlesList, signatures);
         if (!isVerified) {
             revert InvalidKMSSignatures();
         }
@@ -1129,7 +1128,7 @@ function generateSolidityDecryptionOracleMethods(fheTypes: AdjustedFheType[]): s
      * @dev Private low-level function used to extract the decryptedResult bytes array and verify the KMS signatures.
      * @notice  Warning: MUST be called directly in the callback function called by the relayer.
      */
-    function verifySignatures(bytes32[] memory handlesList, bytes[] memory signatures, bytes memory extraData) private returns (bool) {
+    function verifySignatures(bytes32[] memory handlesList, bytes[] memory signatures) private returns (bool) {
       uint256 start = 4 + 32; // start position after skipping the selector (4 bytes) and the first argument (index, 32 bytes)
       uint256 length = getSignedDataLength(handlesList);
       bytes memory decryptedResult = new bytes(length);
@@ -1141,8 +1140,7 @@ function generateSolidityDecryptionOracleMethods(fheTypes: AdjustedFheType[]): s
           IKMSVerifier($.KMSVerifierAddress).verifyDecryptionEIP712KMSSignatures(
               handlesList,
               decryptedResult,
-              signatures,
-              extraData
+              signatures
           );
     }
 
@@ -1160,6 +1158,7 @@ function generateSolidityDecryptionOracleMethods(fheTypes: AdjustedFheType[]): s
                 revert UnsupportedHandleType();
             }
         }
+        signedDataLength += 32; // add offset of signatures
         return signedDataLength;
     }
   `,
