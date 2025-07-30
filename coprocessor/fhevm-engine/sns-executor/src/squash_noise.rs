de6371be44d06c7c8ac9daed8660820a0f176b0c
goshawk-3 <76947196+goshawk-3@users.noreply.github.com>
2025-07-30 07:45:30 +0100

    feat(coprocessor):  Enable compression for big ciphertexts (#593)
    * feat(coprocessor): bump tfhe version to 1.3
    
    * fix(coprocessor): impl From trait for CryptoParametersRecord
    
    * feat(coprocessor): support compression for large ciphertexts
    
    Adds optional compression to `squash_noise_and_serialize`, enabled via the '--enable-compression'
    CLI flag.
    
    - Introduces a new column in the 'ciphertexts_digest' table to indicate whether a ciphertext is
      stored in compressed or raw format.
    
    - Adds a metadata field in the S3 object to specify the actual format of the uploaded ciphertext.
    
    * feat(coprocessor): exclude noise squashing compression key from the Evaluation key
    
    * feat(coprocessor): enable noise_squashing_compression in generate-keys
    
    * fix(coprocessor): filter out empty cts
    
    * fix(coprocessor): use a test db instance in the host-listener unit-tests
    
    * fix(coprocessor): update setup_test_db inputs in host-listener
    
    * fix(test-suite): update e2e setup to be aligned with tfhers v1.3 changes
    
    * fix(test-suite): bump relayer-sdk version to support tfhe-js v1.3
    
    * feat(kms-connector): support compressed ciphertext (#618)
    
    * feat(kms-connector): compressed ct support
    
    * chore(kms-connector): test & improvements
    
    * chore(test-suite): update versions
    
    * fix(test-suite): typo
    
    * chore(test-suite): update versions
    
    * chore(test-suite): update sdk import path
    
    * chore(test-suite): update versions
    
    ---------
    
    Co-authored-by: eudelins-zama <simon.eudeline@zama.ai>

--- coprocessor/fhevm-engine/sns-executor/src/squash_noise.rs
+++ coprocessor/fhevm-engine/sns-executor/src/squash_noise.rs
@@ -4,29 +4,50 @@ use serde::Serialize;
 
 use tfhe::named::Named;
 use tfhe::prelude::SquashNoise;
-
+use tfhe::CompressedSquashedNoiseCiphertextListBuilder;
 use tfhe::SquashedNoiseFheUint;
 use tfhe::Versionize;
 
 use fhevm_engine_common::types::SupportedFheCiphertexts;
 
 macro_rules! squash_and_serialize_with_error {
-    ($value:expr, $target_ty:ty) => {{
+    ($value:expr, $target_ty:ty, $enable_compression:expr) => {{
         let squashed: $target_ty = $value
             .squash_noise()
             .map_err(ExecutionError::SquashedNoiseError)?;
-        Ok(safe_serialize(&squashed)?)
+
+        if !$enable_compression {
+            return safe_serialize(&squashed);
+        }
+
+        let mut builder = CompressedSquashedNoiseCiphertextListBuilder::new();
+        builder.push(squashed);
+        let list = builder.build()?;
+
+        Ok(safe_serialize(&list)?)
     }};
 }
 
 pub(crate) trait SquashNoiseCiphertext {
     /// Squashes the noise of the ciphertext and serializes it.
-    /// Returns the serialized ciphertext as a byte vector.
-    fn squash_noise_and_serialize(&self) -> Result<Vec<u8>, ExecutionError>;
+    /// Returns the compressed big ciphertext serialized if `enable_compression` is true,
+    /// otherwise returns the squashed ciphertext serialized.
+    fn squash_noise_and_serialize(
+        &self,
+        enable_compression: bool,
+    ) -> Result<Vec<u8>, ExecutionError>;
 
-    /// Decrypts the squashed noise ciphertext and returns the decrypted value.
+    /// Tries to decrypt a squashed noise ciphertext and returns the cleartext.
     #[cfg(feature = "test_decrypt_128")]
     fn decrypt_squash_noise(
+        &self,
+        key: &tfhe::ClientKey,
+        data: &[u8],
+    ) -> Result<u128, ExecutionError>;
+
+    /// Tries to decrypt a compressed squashed noise ciphertext and returns the cleartext.
+    #[cfg(feature = "test_decrypt_128")]
+    fn decrypt_squash_noise_compressed(
         &self,
         key: &tfhe::ClientKey,
         data: &[u8],
@@ -34,44 +55,47 @@ pub(crate) trait SquashNoiseCiphertext {
 }
 
 impl SquashNoiseCiphertext for SupportedFheCiphertexts {
-    fn squash_noise_and_serialize(&self) -> Result<Vec<u8>, ExecutionError> {
+    fn squash_noise_and_serialize(
+        &self,
+        enable_compression: bool,
+    ) -> Result<Vec<u8>, ExecutionError> {
         match self {
             SupportedFheCiphertexts::FheBool(v) => {
-                squash_and_serialize_with_error!(v, tfhe::SquashedNoiseFheBool)
+                squash_and_serialize_with_error!(v, tfhe::SquashedNoiseFheBool, enable_compression)
             }
             SupportedFheCiphertexts::FheUint4(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
 
             SupportedFheCiphertexts::FheUint8(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheUint16(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheUint32(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheUint64(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheUint128(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheUint160(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheUint256(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheBytes64(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheBytes128(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::FheBytes256(v) => {
-                squash_and_serialize_with_error!(v, SquashedNoiseFheUint)
+                squash_and_serialize_with_error!(v, SquashedNoiseFheUint, enable_compression)
             }
             SupportedFheCiphertexts::Scalar(_) => {
                 panic!("we should never need to serialize scalar")
@@ -93,7 +117,36 @@ impl SquashNoiseCiphertext for SupportedFheCiphertexts {
                 clear as u128
             }
             _ => {
-                let v: SquashedNoiseFheUint = safe_deserialize(data).unwrap();
+                let v: SquashedNoiseFheUint = safe_deserialize(data)?;
+                let clear: u128 = v.decrypt(key);
+                clear
+            }
+        };
+        Ok(res)
+    }
+
+    #[cfg(feature = "test_decrypt_128")]
+    fn decrypt_squash_noise_compressed(
+        &self,
+        key: &tfhe::ClientKey,
+        list: &[u8],
+    ) -> Result<u128, ExecutionError> {
+        use tfhe::CompressedSquashedNoiseCiphertextList;
+        use tfhe::{prelude::FheDecrypt, SquashedNoiseFheUint};
+        let list: CompressedSquashedNoiseCiphertextList = safe_deserialize(list)?;
+
+        let res = match self {
+            SupportedFheCiphertexts::FheBool(_) => {
+                let v: tfhe::SquashedNoiseFheBool = list.get(0)?.ok_or_else(|| {
+                    anyhow::anyhow!("Failed to get the first element from the list")
+                })?;
+                let clear: bool = v.decrypt(key);
+                clear as u128
+            }
+            _ => {
+                let v: SquashedNoiseFheUint = list.get(0)?.ok_or_else(|| {
+                    anyhow::anyhow!("Failed to get the first element from the list")
+                })?;
                 let clear: u128 = v.decrypt(key);
                 clear
             }
