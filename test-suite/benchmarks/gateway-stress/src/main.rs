62266ad7a4f7bfc80367dbed4642a9aafd63d1ae
Simon Eudeline <simon.eudeline@zama.ai>
2025-08-07 10:40:46 +0200

    chore(test-suite): public decryption stress tests

--- test-suite/benchmarks/gateway-stress/src/main.rs
+++ test-suite/benchmarks/gateway-stress/src/main.rs
@@ -4,14 +4,13 @@ mod cli;
 mod config;
 mod decryption;
 
-use std::process::ExitCode;
-
 use crate::{
     app::App,
     cli::{Cli, Subcommands},
     config::Config,
 };
 use clap::Parser;
+use std::process::ExitCode;
 use tracing::{error, info};
 use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -32,7 +31,7 @@ async fn run() -> anyhow::Result<()> {
 
     let app = App::connect(config.clone()).await?;
     match cli.subcommand {
-        Subcommands::Public => app.public().await?,
+        Subcommands::Public => app.public_decryption_stress_test().await?,
         Subcommands::User => app.user().await?,
         _ => todo!(),
     }
@@ -43,9 +42,6 @@ async fn run() -> anyhow::Result<()> {
 fn init_tracing() {
     tracing_subscriber::registry()
         .with(fmt::layer())
-        .with(
-            EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| EnvFilter::new("info,gateway-sdk=debug")),
-        )
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
         .init();
 }
