6b068201f5f3560440e687728c94bf7250b9b23b
eudelins-zama <simon.eudeline@zama.ai>
2025-08-14 15:47:21 +0200

    chore(kms-connector): upgrade components version in tests (#729)

--- kms-connector/crates/utils/src/tests/setup/gw.rs
+++ kms-connector/crates/utils/src/tests/setup/gw.rs
@@ -2,7 +2,7 @@ use crate::{
     config::KmsWallet,
     conn::WalletGatewayProvider,
     provider::{FillersWithoutNonceManagement, NonceManagedProvider},
-    tests::setup::pick_free_port,
+    tests::setup::{ROOT_CARGO_TOML, pick_free_port},
 };
 use alloy::{
     primitives::{Address, ChainId, FixedBytes},
@@ -148,8 +148,9 @@ pub async fn setup_anvil_gateway(
     let anvil_internal_ip = endpoint_settings.ip_address.clone().unwrap();
 
     info!("Deploying Gateway mock contracts...");
+    let version = ROOT_CARGO_TOML.get_gateway_bindings_version();
     let _deploy_mock_container =
-        GenericImage::new("ghcr.io/zama-ai/fhevm/gateway-contracts", "v0.7.6")
+        GenericImage::new("ghcr.io/zama-ai/fhevm/gateway-contracts", &version)
             .with_wait_for(WaitFor::message_on_stdout("Mock contract deployment done!"))
             .with_env_var("HARDHAT_NETWORK", "staging")
             .with_env_var(
