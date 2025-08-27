3fe4e9992e9b360d087721471d137ff5dd027fd4
Roman Bredehoft <roman.bredehoft@zama.ai>
2025-08-27 11:02:40 +0200

    chore(gateway-contracts): silence reinitializer warnings

--- gateway-contracts/contracts/CiphertextCommits.sol
+++ gateway-contracts/contracts/CiphertextCommits.sol
@@ -93,6 +93,8 @@ contract CiphertextCommits is
     /**
      * @notice Re-initializes the contract from V1.
      */
+    /// @custom:oz-upgrades-unsafe-allow missing-initializer-call
+    /// @custom:oz-upgrades-validate-as-initializer
     function reinitializeV2() public virtual reinitializer(REINITIALIZER_VERSION) {}
 
     /// @notice See {ICiphertextCommits-addCiphertextMaterial}.
