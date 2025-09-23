9771463d14b3a377007afcfc975daff48f993a6c
rudy <rudy.sicard@gmail.com>
2025-09-23 15:42:27 +0200

    feat(coprocessor): insert with up-to-date is_allowed

--- coprocessor/fhevm-engine/host-listener/src/cmd/mod.rs
+++ coprocessor/fhevm-engine/host-listener/src/cmd/mod.rs
@@ -8,7 +8,7 @@ use anyhow::{anyhow, Result};
 use futures_util::stream::StreamExt;
 use sqlx::types::Uuid;
 
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
@@ -22,11 +22,13 @@ use rustls;
 use tokio_util::sync::CancellationToken;
 
 use fhevm_engine_common::healthz_server::HttpServer as HealthHttpServer;
-use fhevm_engine_common::types::BlockchainProvider;
+use fhevm_engine_common::types::{BlockchainProvider, Handle};
 use fhevm_engine_common::utils::HeartBeat;
 
 use crate::contracts::{AclContract, TfheContract};
-use crate::database::tfhe_event_propagate::{ChainId, Database, LogTfhe};
+use crate::database::tfhe_event_propagate::{
+    acl_result_handles, tfhe_result_handle, ChainId, Database, LogTfhe,
+};
 use crate::health_check::HealthCheck;
 
 pub mod block_history;
@@ -819,66 +821,61 @@ async fn db_insert_block_no_retry(
     tfhe_contract_address: &Option<Address>,
 ) -> std::result::Result<(), sqlx::Error> {
     let mut tx = db.new_transaction().await?;
-    // Proces all tfhe events first in case an allow event is out of
-    // order and occurs before the operation producing its handle
-    for log in &block_logs.logs {
-        let current_address = Some(log.inner.address);
-        let is_tfhe_address = &current_address == tfhe_contract_address;
-        if tfhe_contract_address.is_none() || is_tfhe_address {
-            info!(
-                block = ?log.block_number,
-                tx = ?log.transaction_hash,
-                log_index = ?log.log_index,
-                "Log",
-            );
-            if let Ok(event) =
-                TfheContract::TfheContractEvents::decode_log(&log.inner)
-            {
-                info!(tfhe_event = ?event, "TFHE event");
-                let log = LogTfhe {
-                    event,
-                    transaction_hash: log.transaction_hash,
-                };
-                db.insert_tfhe_event(&mut tx, &log).await?;
-                continue;
-            }
-        }
-        if is_tfhe_address {
-            error!(
-                event_address = ?log.inner.address,
-                acl_contract_address = ?acl_contract_address,
-                tfhe_contract_address = ?tfhe_contract_address,
-                "Cannot decode event",
-            );
-        }
-    }
-    // Process ACL events
+    let mut is_allowed = HashSet::<Handle>::new();
+    let mut tfhe_event_log = vec![];
     for log in &block_logs.logs {
         let current_address = Some(log.inner.address);
         let is_acl_address = &current_address == acl_contract_address;
         if acl_contract_address.is_none() || is_acl_address {
-            info!(
-                block = ?log.block_number,
-                tx = ?log.transaction_hash,
-                log_index = ?log.log_index,
-                "Log",
-            );
             if let Ok(event) =
                 AclContract::AclContractEvents::decode_log(&log.inner)
             {
                 info!(acl_event = ?event, "ACL event");
+                let handles = acl_result_handles(&event);
+                for handle in handles {
+                    is_allowed.insert(handle.to_vec());
+                }
                 db.handle_acl_event(&mut tx, &event).await?;
                 continue;
             }
         }
-        if is_acl_address {
+        let is_tfhe_address = &current_address == tfhe_contract_address;
+        if tfhe_contract_address.is_none() || is_tfhe_address {
+            if let Ok(event) =
+                TfheContract::TfheContractEvents::decode_log(&log.inner)
+            {
+                let log = LogTfhe {
+                    event,
+                    transaction_hash: log.transaction_hash,
+                    is_allowed: false, // updated in the next loop
+                };
+                tfhe_event_log.push(log);
+                continue;
+            }
+        }
+        if is_acl_address || is_tfhe_address {
             error!(
                 event_address = ?log.inner.address,
                 acl_contract_address = ?acl_contract_address,
                 tfhe_contract_address = ?tfhe_contract_address,
+                log = ?log,
                 "Cannot decode event",
             );
         }
+    }
+    for tfhe_log in tfhe_event_log {
+        info!(tfhe_log = ?tfhe_log, "TFHE event");
+        let is_allowed =
+            if let Some(result_handle) = tfhe_result_handle(&tfhe_log.event) {
+                is_allowed.contains(&result_handle.to_vec())
+            } else {
+                false
+            };
+        let tfhe_log = LogTfhe {
+            is_allowed,
+            ..tfhe_log
+        };
+        db.insert_tfhe_event(&mut tx, &tfhe_log).await?;
     }
     db.mark_block_as_valid(&mut tx, &block_logs.summary).await?;
     tx.commit().await
