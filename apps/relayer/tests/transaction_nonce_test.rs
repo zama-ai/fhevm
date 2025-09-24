fa70369c517376fecd3bcdc93214a72dc53782cf
Nicolas Boisde <nicolas.boisde@zama.ai>
2025-09-24 22:20:00 +0200

    fix: removing useless nonce for integration tests

--- apps/relayer/tests/transaction_nonce_test.rs
+++ apps/relayer/tests/transaction_nonce_test.rs
@@ -1,15 +1,8 @@
 mod common;
 
-use alloy::consensus::Transaction;
-use alloy::primitives::address;
 use alloy::primitives::Address;
-use alloy::primitives::U256;
-use alloy::providers::fillers::NonceFiller;
 use alloy::providers::fillers::NonceManager;
-use alloy::providers::Provider;
 use alloy::providers::ProviderBuilder;
-use alloy::providers::WalletProvider;
-use alloy::rpc::types::TransactionRequest;
 use fhevm_relayer::transaction::nonce::CachedNonceManagerWithRefresh;
 use reqwest::Url;
 
@@ -17,48 +10,6 @@ use reqwest::Url;
 fn test() {
     let val = true;
     assert!(val);
-}
-
-// TODO: Remove usage of `connect_anvil_with_wallet` and `connect_anvil` that relies on
-// `anvil` being available on the test machine.
-#[tokio::test]
-async fn increments_nonce() {
-    let _setup = common::utils::TestSetup::new()
-        .await
-        .expect("Failed to create test setup");
-    let cnm1 = CachedNonceManagerWithRefresh::default();
-    let provider = ProviderBuilder::new()
-        .disable_recommended_fillers()
-        .filler(NonceFiller::new(cnm1))
-        .connect_anvil_with_wallet();
-
-    let from = provider.default_signer_address();
-    let tx = TransactionRequest {
-        from: Some(from),
-        value: Some(U256::from(100)),
-        to: Some(address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into()),
-        gas_price: Some(20e9 as u128),
-        gas: Some(21000),
-        ..Default::default()
-    };
-
-    let pending = provider.send_transaction(tx.clone()).await.unwrap();
-    let tx_hash = pending.watch().await.unwrap();
-    let mined_tx = provider
-        .get_transaction_by_hash(tx_hash)
-        .await
-        .expect("failed to fetch tx")
-        .expect("tx not included");
-    assert_eq!(mined_tx.nonce(), 0);
-
-    let pending = provider.send_transaction(tx).await.unwrap();
-    let tx_hash = pending.watch().await.unwrap();
-    let mined_tx = provider
-        .get_transaction_by_hash(tx_hash)
-        .await
-        .expect("fail to fetch tx")
-        .expect("tx didn't finalize");
-    assert_eq!(mined_tx.nonce(), 1);
 }
 
 #[tokio::test]
