e58ea8e69da83cceb477bd45904305ef400e6bd9
Alessandro Vitali <alessandro.vitali@zama.ai>
2025-09-26 15:55:25 +0200

    fix: remove reference (#655)

--- apps/back/app.js
+++ apps/back/app.js
@@ -114,12 +114,6 @@ app.get("/plans", jsonParser, async (req, res) => {
 
 app.get(
   "/subscriptions",
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  }),
   authMiddleware,
   jsonParser,
   async (req, res) => {
