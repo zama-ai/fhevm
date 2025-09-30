5d06e66c6eb83be07e3aa356263008d4ebc86b85
0xalexbel <alexandre.belhoste@zama.ai>
2025-09-30 09:38:08 +0200

    chore(coprocessor): rename MultichainAcl to MultichainACL

--- coprocessor/fhevm-engine/transaction-sender/contracts/MultichainAcl.sol
+++ coprocessor/fhevm-engine/transaction-sender/contracts/MultichainAcl.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.24;
 
-/// @title MultichainAcl smart contract
-/// @dev source: github.com/zama-ai/fhevm-gateway/blob/main/contracts/MultichainAcl.sol
-/// @notice This contract is a mock of the MultichainAcl contract from L2.
-contract MultichainAcl {
-    error CoprocessorAlreadyAllowedAccount(bytes32 ctHandle, address account, address coprocessor);
+/// @title MultichainACL smart contract
+/// @dev sources: 
+///      - github.com/zama-ai/fhevm/blob/main/gateway-contracts/contracts/MultichainACL.sol
+///      - github.com/zama-ai/fhevm/blob/main/gateway-contracts/contracts/interfaces/IMultichainACL.sol
+/// @notice This contract is a mock of the MultichainACL contract from L2.
+contract MultichainACL {
+    error CoprocessorAlreadyAllowedAccount(bytes32 ctHandle, address account, address txSender);
     error CoprocessorAlreadyAllowedPublicDecrypt(bytes32 ctHandle, address txSender);
 
     event AllowAccount(bytes32 indexed ctHandle, address accountAddress);
