97eae3e25845d32e8a2529c01e7a4079667cd737
Roman Bredehoft <roman.bredehoft@zama.ai>
2025-10-08 12:34:05 +0200

    chore(gateway-contracts): add docker jobs for getters

--- gateway-contracts/tasks/getters.ts
+++ gateway-contracts/tasks/getters.ts
@@ -1,70 +1,121 @@
 import dotenv from "dotenv";
-import fs from "fs";
-import { task } from "hardhat/config";
-import type { HardhatEthersHelpers, TaskArguments } from "hardhat/types";
+import { task, types } from "hardhat/config";
+import type { HardhatEthersHelpers } from "hardhat/types";
+import path from "path";
 
 import { CoprocessorContexts, GatewayConfig } from "../typechain-types";
+import { ADDRESSES_DIR } from "../hardhat.config";
+import { getRequiredEnvVar } from "./utils/loadVariables";
 
-async function loadGatewayConfigContract(
-  customGatewayConfigAddress: string | undefined,
-  ethers: HardhatEthersHelpers,
-): Promise<GatewayConfig> {
-  const gatewayConfigFactory = await ethers.getContractFactory("./contracts/GatewayConfig.sol:GatewayConfig");
-  const gatewayConfigAddress = customGatewayConfigAddress
-    ? customGatewayConfigAddress
-    : dotenv.parse(fs.readFileSync("addresses/.env.gateway")).GATEWAY_CONFIG_ADDRESS;
-  return gatewayConfigFactory.attach(gatewayConfigAddress).connect(ethers.provider) as GatewayConfig;
+async function loadGatewayConfigContract(ethers: HardhatEthersHelpers): Promise<GatewayConfig> {
+  const proxyAddress = getRequiredEnvVar("GATEWAY_CONFIG_ADDRESS");
+
+  console.log("In GatewayConfig contract:", proxyAddress, "\n");
+
+  const contract = await ethers.getContractAt("GatewayConfig", proxyAddress);
+  return contract as GatewayConfig;
 }
 
-async function loadCoprocessorContextsContract(
-  customCoprocessorContextsAddress: string | undefined,
-  ethers: HardhatEthersHelpers,
-): Promise<CoprocessorContexts> {
-  const coprocessorContextsFactory = await ethers.getContractFactory(
-    "./contracts/CoprocessorContexts.sol:CoprocessorContexts",
-  );
-  const coprocessorContextsAddress = customCoprocessorContextsAddress
-    ? customCoprocessorContextsAddress
-    : dotenv.parse(fs.readFileSync("addresses/.env.gateway")).COPROCESSOR_CONTEXTS_ADDRESS;
-  return coprocessorContextsFactory.attach(coprocessorContextsAddress).connect(ethers.provider) as CoprocessorContexts;
+async function loadCoprocessorContextsContract(ethers: HardhatEthersHelpers): Promise<CoprocessorContexts> {
+  const proxyAddress = getRequiredEnvVar("COPROCESSOR_CONTEXTS_ADDRESS");
+
+  console.log("In CoprocessorContexts contract:", proxyAddress, "\n");
+
+  const contract = await ethers.getContractAt("CoprocessorContexts", proxyAddress);
+  return contract as CoprocessorContexts;
 }
 
 task("task:getKmsSigners")
   .addOptionalParam(
-    "customGatewayConfigAddress",
-    "Use a custom address for the GatewayConfig contract instead of the default one - ie stored inside .env.gateway",
+    "useInternalProxyAddress",
+    "If proxy address from the /addresses directory should be used",
+    false,
+    types.boolean,
   )
-  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const gatewayConfig = await loadGatewayConfigContract(taskArguments.customGatewayConfigAddress, ethers);
+  .setAction(async function ({ useInternalProxyAddress }, hre) {
+    await hre.run("compile:specific", { contract: "contracts" });
+    console.log("Get registered KMS signers");
+
+    if (useInternalProxyAddress) {
+      dotenv.config({ path: path.join(ADDRESSES_DIR, ".env.gateway"), override: true });
+    }
+    const gatewayConfig = await loadGatewayConfigContract(hre.ethers);
     const listCurrentKMSSigners = await gatewayConfig.getKmsSigners();
-    console.log("The list of current KMS Signers stored inside GatewayConfig contract is: ", listCurrentKMSSigners);
+
+    console.log("Registered KMS signers: ", listCurrentKMSSigners);
   });
 
 task("task:getCoprocessorSigners")
   .addOptionalParam(
-    "customCoprocessorContextsAddress",
-    "Use a custom address for the CoprocessorContexts contract instead of the default one - ie stored inside .env.gateway",
+    "useInternalProxyAddress",
+    "If proxy address from the /addresses directory should be used",
+    false,
+    types.boolean,
   )
-  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const coprocessorContexts = await loadCoprocessorContextsContract(
-      taskArguments.customCoprocessorContextsAddress,
-      ethers,
-    );
-    const activeContextId = await coprocessorContexts.getActiveCoprocessorContextId();
-    const listCurrentCoprocessorSigners = await coprocessorContexts.getCoprocessorSigners(activeContextId);
+  .addOptionalParam(
+    "coprocessorContextId",
+    "The ID of the coprocessor context to get the signers from. If not provided, the active context will be used.",
+    undefined,
+    types.int,
+  )
+  .setAction(async function ({ useInternalProxyAddress, coprocessorContextId }, hre) {
+    await hre.run("compile:specific", { contract: "contracts" });
+    if (coprocessorContextId) {
+      console.log("Getting registered coprocessor signers from coprocessor context: ", coprocessorContextId);
+    } else {
+      console.log("Getting registered coprocessor signers from active coprocessor context.");
+    }
+
+    if (useInternalProxyAddress) {
+      dotenv.config({ path: path.join(ADDRESSES_DIR, ".env.gateway"), override: true });
+    }
+    const coprocessorContexts = await loadCoprocessorContextsContract(hre.ethers);
+    const contextId = coprocessorContextId || (await coprocessorContexts.getActiveCoprocessorContextId());
+    const listCurrentCoprocessorSigners = await coprocessorContexts.getCoprocessorSigners(contextId);
+
     console.log(
-      "The list of current Coprocessor Signers stored inside GatewayConfig contract is: ",
+      "Registered coprocessor signers for context ID ",
+      contextId,
+      ": ",
       listCurrentCoprocessorSigners,
     );
   });
 
 task("task:getHostChains")
   .addOptionalParam(
-    "customGatewayConfigAddress",
-    "Use a custom address for the GatewayConfig contract instead of the default one - ie stored inside .env.gateway",
+    "useInternalProxyAddress",
+    "If proxy address from the /addresses directory should be used",
+    false,
+    types.boolean,
   )
-  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const gatewayConfig = await loadGatewayConfigContract(taskArguments.customGatewayConfigAddress, ethers);
+  .setAction(async function ({ useInternalProxyAddress }, hre) {
+    await hre.run("compile:specific", { contract: "contracts" });
+    console.log("Get registered host chains");
+
+    if (useInternalProxyAddress) {
+      dotenv.config({ path: path.join(ADDRESSES_DIR, ".env.gateway"), override: true });
+    }
+    const gatewayConfig = await loadGatewayConfigContract(hre.ethers);
     const listCurrentHostChains = await gatewayConfig.getHostChains();
-    console.log("The list of current host chains stored inside GatewayConfig contract is: ", listCurrentHostChains);
+
+    console.log("Registered host chains: ", listCurrentHostChains);
+  });
+
+task("task:getAllRegisteredEntities")
+  .addOptionalParam(
+    "useInternalProxyAddress",
+    "If proxy address from the /addresses directory should be used",
+    false,
+    types.boolean,
+  )
+  .addOptionalParam(
+    "coprocessorContextId",
+    "The ID of the coprocessor context to get the signers from. If not provided, the active context will be used.",
+    undefined,
+    types.int,
+  )
+  .setAction(async function ({ useInternalProxyAddress, coprocessorContextId }, hre) {
+    await hre.run("task:getKmsSigners", { useInternalProxyAddress });
+    await hre.run("task:getCoprocessorSigners", { useInternalProxyAddress, coprocessorContextId });
+    await hre.run("task:getHostChains", { useInternalProxyAddress });
   });
