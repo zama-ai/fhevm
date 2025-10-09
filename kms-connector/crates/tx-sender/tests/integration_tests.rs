ef5ff63b67f55b38bdf82748feffa817dfa7e801
Simon Eudeline <simon.eudeline@zama.ai>
2025-10-09 12:00:37 +0200

    fix(kms-connector): fix tests

--- kms-connector/crates/tx-sender/tests/integration_tests.rs
+++ kms-connector/crates/tx-sender/tests/integration_tests.rs
@@ -340,7 +340,7 @@ async fn start_test_tx_sender(
         DbKmsResponsePicker::connect(test_instance.db().clone(), &Config::default().await).await?;
     let response_remover = DbKmsResponseRemover::new(test_instance.db().clone());
     let provider = connect_to_gateway_with_wallet(
-        &test_instance.anvil_ws_endpoint(),
+        test_instance.anvil_http_endpoint().as_str(),
         *CHAIN_ID as u64,
         KmsWallet::from_private_key_str(DEPLOYER_PRIVATE_KEY, Some(*CHAIN_ID as u64))?,
     )
