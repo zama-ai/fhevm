a578f9014900338f6a3b45dc482ace005e87d4b2
jat <153528475+jatZama@users.noreply.github.com>
2025-10-09 12:06:41 +0200

    feat(host-contracts): cleanup ACL (#1036)
    * feat(host-contracts): cleanup ACL
    
    * chore(host-contracts): updated tests and selectors
    
    * chore(coprocessor): removed deprecated events
    
    chore(coprocessor): fix typo
    
    * chore(host-contracts): update readme

--- coprocessor/fhevm-engine/host-listener/src/database/tfhe_event_propagate.rs
+++ coprocessor/fhevm-engine/host-listener/src/database/tfhe_event_propagate.rs
@@ -551,12 +551,6 @@ impl Database {
             AclContractEvents::Initialized(initialized) => {
                 warn!(event = ?initialized, "unhandled Acl::Initialized event");
             }
-            AclContractEvents::NewDelegation(new_delegation) => {
-                warn!(
-                    event = ?new_delegation,
-                    "unhandled Acl::NewDelegation event"
-                );
-            }
             AclContractEvents::OwnershipTransferStarted(
                 ownership_transfer_started,
             ) => {
@@ -569,12 +563,6 @@ impl Database {
                 warn!(
                     event = ?ownership_transferred,
                     "unhandled Acl::OwnershipTransferred event"
-                );
-            }
-            AclContractEvents::RevokedDelegation(revoked_delegation) => {
-                warn!(
-                    event = ?revoked_delegation,
-                    "unhandled Acl::RevokedDelegation event"
                 );
             }
             AclContractEvents::Upgraded(upgraded) => {
@@ -593,12 +581,6 @@ impl Database {
                 warn!(
                     event = ?unpaused,
                     "unhandled Acl::Unpaused event"
-                );
-            }
-            AclContractEvents::UpdatePauser(update_pauser) => {
-                warn!(
-                    event = ?update_pauser,
-                    "unhandled Acl::UpdatePauser event"
                 );
             }
         }
@@ -785,13 +767,10 @@ pub fn acl_result_handles(event: &Log<AclContractEvents>) -> Vec<Handle> {
             allowed_for_decryption.handlesList.clone()
         }
         AclContractEvents::Initialized(_)
-        | AclContractEvents::NewDelegation(_)
         | AclContractEvents::OwnershipTransferStarted(_)
         | AclContractEvents::OwnershipTransferred(_)
-        | AclContractEvents::RevokedDelegation(_)
         | AclContractEvents::Upgraded(_)
         | AclContractEvents::Paused(_)
-        | AclContractEvents::Unpaused(_)
-        | AclContractEvents::UpdatePauser(_) => vec![],
+        | AclContractEvents::Unpaused(_) => vec![],
     }
 }
