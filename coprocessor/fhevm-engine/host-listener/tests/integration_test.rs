520228c8d3512be2aa1caf1974629d7078a975d1
rudy <rudy.sicard@zama.ai>
2025-10-17 13:59:05 +0200

    feat(coprocessor): host-listener + transaction-sender, delegations

--- coprocessor/fhevm-engine/host-listener/tests/integration_test.rs
+++ coprocessor/fhevm-engine/host-listener/tests/integration_test.rs
@@ -17,12 +17,13 @@ use alloy::sol;
 use futures_util::future::try_join_all;
 use serial_test::serial;
 use sqlx::postgres::PgPoolOptions;
+use std::collections::HashSet;
 use std::process::Command;
-use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering;
+use std::sync::atomic::{AtomicU32, AtomicU64};
 use test_harness::health_check;
 use test_harness::instance::ImportMode;
-use tracing::{warn, Level};
+use tracing::{info, warn, Level};
 
 use host_listener::cmd::main;
 use host_listener::cmd::Args;
@@ -398,6 +399,108 @@ async fn test_health() -> Result<(), anyhow::Error> {
     tokio::time::sleep(tokio::time::Duration::from_secs(5)).await; // time to recover
     assert!(health_check::wait_healthy(&setup.health_check_url, 10, 1).await);
     warn!("Test is killing the listener");
+    listener_handle.abort();
+    Ok(())
+}
+
+const NB_DELEGATION_PER_WALLET: usize = 15;
+
+async fn emit_delegations<P, N>(
+    wallets: &[EthereumWallet],
+    url: &str,
+    acl_contract: ACLTestInstance<P, N>,
+) where
+    P: Clone + alloy::providers::Provider<N> + 'static,
+    N: Clone
+        + alloy::providers::Network<TransactionRequest = TransactionRequest>
+        + 'static,
+{
+    static UNIQUE_INT: AtomicU64 = AtomicU64::new(1); // to counter avoid idempotency
+    let mut threads = vec![];
+    let delegate = *acl_contract.address();
+    let contract_address = *acl_contract.address();
+    for (i_wallet, wallet) in wallets.iter().enumerate() {
+        let expiration_date = 3600_u64 + i_wallet as u64;
+        let wallet = wallet.clone();
+        let acl_contract = acl_contract.clone();
+        let url = url.to_string();
+        let thread = tokio::spawn(async move {
+            let delegation_counter = UNIQUE_INT.fetch_add(1, Ordering::SeqCst);
+            for _ in 1..=NB_DELEGATION_PER_WALLET {
+                let provider = ProviderBuilder::new()
+                    .wallet(wallet.clone())
+                    .connect_ws(WsConnect::new(url.to_string()))
+                    .await
+                    .unwrap();
+                let acl_txn_req = acl_contract
+                    .delegateForUserDecryption(
+                        delegate,
+                        contract_address,
+                        delegation_counter,
+                        0,
+                        expiration_date,
+                    )
+                    .into_transaction_request();
+                let pending_txn = provider
+                    .send_transaction(acl_txn_req.clone())
+                    .await
+                    .unwrap();
+                let receipt = pending_txn.get_receipt().await.unwrap();
+                assert!(receipt.status());
+            }
+        });
+        threads.push(thread);
+    }
+    if let Err(err) = try_join_all(threads).await {
+        eprintln!("{err}");
+        panic!("One event emission failed: {err}");
+    }
+}
+
+#[tokio::test]
+#[serial(db)]
+async fn test_listener_delegations() -> Result<(), anyhow::Error> {
+    let setup = setup(None).await?;
+    let args = setup.args.clone();
+
+    // Start listener in background task
+    let listener_handle = tokio::spawn(main(args.clone()));
+    assert!(health_check::wait_healthy(&setup.health_check_url, 60, 1).await);
+
+    // Emit first batch of events
+    let wallets_clone = setup.wallets.clone();
+    let url_clone = setup.args.url.clone();
+    let acl_contract_clone = setup.acl_contract.clone();
+    let event_source = tokio::spawn(async move {
+        emit_delegations(&wallets_clone, &url_clone, acl_contract_clone).await;
+    });
+
+    let mut delegation_set = HashSet::new();
+    for _ in 1..30 {
+        let _ = tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        let delegations = sqlx::query!(
+            "SELECT block_number, new_expiration_date FROM delegate_user_decrypt"
+        )
+        .fetch_all(&setup.db_pool)
+        .await?;
+        for delegation in delegations {
+            delegation_set.insert((
+                delegation.block_number,
+                delegation.new_expiration_date,
+            ));
+        }
+        if delegation_set.len()
+            >= setup.wallets.len() * NB_DELEGATION_PER_WALLET
+        {
+            info!("Delegations in database");
+            break;
+        }
+    }
+    event_source.await?;
+    assert_eq!(
+        delegation_set.len(),
+        setup.wallets.len() * NB_DELEGATION_PER_WALLET
+    );
     listener_handle.abort();
     Ok(())
 }
--- coprocessor/fhevm-engine/transaction-sender/src/nonce_managed_provider.rs
+++ coprocessor/fhevm-engine/transaction-sender/src/nonce_managed_provider.rs
@@ -10,9 +10,10 @@ use alloy::{
         PendingTransactionBuilder,
     },
     rpc::types::TransactionRequest,
-    transports::TransportResult,
+    transports::{RpcError, TransportResult},
 };
 use futures_util::lock::Mutex;
+use tracing::{error, warn};
 
 pub type FillersWithoutNonceManagement =
     JoinFill<GasFiller, JoinFill<BlobGasFiller, ChainIdFiller>>;
@@ -28,6 +29,17 @@ where
     provider: P,
     nonce_manager: Arc<Mutex<CachedNonceManager>>,
     signer_address: Option<Address>,
+    retry_immediately_on_nonce_issue: u64,
+}
+
+pub fn is_nonce_error(err: &RpcError<impl std::fmt::Debug>) -> bool {
+    if let RpcError::ErrorResp(err) = err {
+        // server returned an error response: error code -32003: Nonce too high err
+        if err.code == -32003 && (err.message.contains("Nonce") || err.message.contains("nonce")) {
+            return true;
+        }
+    }
+    false
 }
 
 impl<P: alloy::providers::Provider<Ethereum> + Clone + 'static> NonceManagedProvider<P> {
@@ -36,6 +48,20 @@ impl<P: alloy::providers::Provider<Ethereum> + Clone + 'static> NonceManagedProv
             provider,
             nonce_manager: Default::default(),
             signer_address,
+            retry_immediately_on_nonce_issue: 0,
+        }
+    }
+
+    pub fn new_with_nonce_retry(
+        provider: P,
+        signer_address: Option<Address>,
+        retry_immediately_on_nonce_issue: u64,
+    ) -> Self {
+        Self {
+            provider,
+            nonce_manager: Default::default(),
+            signer_address,
+            retry_immediately_on_nonce_issue,
         }
     }
 
@@ -43,19 +69,32 @@ impl<P: alloy::providers::Provider<Ethereum> + Clone + 'static> NonceManagedProv
         &self,
         tx: impl Into<TransactionRequest>,
     ) -> TransportResult<PendingTransactionBuilder<Ethereum>> {
-        let mut tx = tx.into();
-        if let Some(signer_address) = self.signer_address {
-            let nonce_manager = self.nonce_manager.lock().await;
-            let nonce = nonce_manager
-                .get_next_nonce(&self.provider, signer_address)
-                .await?;
-            tx.nonce = Some(nonce);
+        let mut res = Err(RpcError::UnsupportedFeature("Not reachable"));
+        // res cannot be returned non initialized
+        let tx_req = tx.into();
+        for _ in 0..=self.retry_immediately_on_nonce_issue {
+            let mut tx: TransactionRequest = tx_req.clone();
+            if let Some(signer_address) = self.signer_address {
+                let nonce_manager = self.nonce_manager.lock().await;
+                let nonce = nonce_manager
+                    .get_next_nonce(&self.provider, signer_address)
+                    .await?;
+                tx.nonce = Some(nonce);
+            }
+            res = self.provider.send_transaction(tx).await;
+            if let Err(err) = &res {
+                // Reset the nonce manager if the transaction sending failed.
+                *self.nonce_manager.lock().await = Default::default();
+                if is_nonce_error(err) {
+                    let msg = err.to_string();
+                    warn!(msg, "Transaction failed due to nonce, resetting nonce manager and retrying immediately");
+                    continue;
+                }
+                warn!("Transaction failed, resetting nonce manager");
+            }
+            return res;
         }
-        let res = self.provider.send_transaction(tx).await;
-        if res.is_err() {
-            // Reset the nonce manager if the transaction sending failed.
-            *self.nonce_manager.lock().await = Default::default();
-        }
+        error!("Transaction failed multiple time due to nonce, aborting");
         res
     }
 
