8fba307add1851540c1940d9d71ea2243e0adb77
Arr00 <13561405+arr00@users.noreply.github.com>
2025-10-19 00:15:46 -0400

    lint fix

--- protocol-contracts/staking/contracts/mocks/ProtocolStakingSlashingMock.sol
+++ protocol-contracts/staking/contracts/mocks/ProtocolStakingSlashingMock.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.27;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Checkpoints} from "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
 import {Time} from "@openzeppelin/contracts/utils/types/Time.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ProtocolStaking} from "../ProtocolStaking.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ProtocolStaking} from "./../ProtocolStaking.sol";
 
 contract ProtocolStakingSlashingMock is ProtocolStaking {
     using Checkpoints for Checkpoints.Trace208;
