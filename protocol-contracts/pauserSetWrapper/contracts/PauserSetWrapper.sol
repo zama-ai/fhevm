dbac0293abb4df2e92945cca3b239d73cdf3284b
jatZama <josephandre.turk@zama.ai>
2025-10-22 21:00:22 +0200

    feat(protocol-contracts): update PauserSetWrapper implementation

--- protocol-contracts/pauserSetWrapper/contracts/PauserSetWrapper.sol
+++ protocol-contracts/pauserSetWrapper/contracts/PauserSetWrapper.sol
@@ -1,23 +1,25 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.24;
 
-import {IPauserSet} from "@fhevm/host-contracts/contracts/interfaces/IPauserSet.sol";
+import { IPauserSet } from "@fhevm/host-contracts/contracts/interfaces/IPauserSet.sol";
+
+interface IPausableToken {
+    function pauseMinting() external;
+}
 
 contract PauserSetWrapper {
-  IPauserSet immutable public PAUSER_SET;
+    IPausableToken public immutable PAUSABLE_TOKEN;
+    IPauserSet public immutable PAUSER_SET;
 
-  error ExecutionFailed(bytes errorData);
-  error SenderNotPauser();
+    error SenderNotPauser();
 
-  constructor(address _pauserSet) {
-    PAUSER_SET = IPauserSet(_pauserSet);
-  }
+    constructor(address _pausableToken, address _pauserSet) {
+        PAUSABLE_TOKEN = IPausableToken(_pausableToken);
+        PAUSER_SET = IPauserSet(_pauserSet);
+    }
 
-  function execute(address callee, bytes memory data) external payable {
-    if(!PAUSER_SET.isPauser(msg.sender)) revert SenderNotPauser();
-    (bool success, bytes memory err) = callee.call{ value: msg.value }(data);
-        if (!success) {
-            revert ExecutionFailed(err);
-        }
-  }
+    function pauseMinting() external payable {
+        if (!PAUSER_SET.isPauser(msg.sender)) revert SenderNotPauser();
+        PAUSABLE_TOKEN.pauseMinting();
+    }
 }
