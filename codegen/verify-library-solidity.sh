ced9c5ff5f9923baec78a12a6b0348d911302298
0xalexbel <alexandre.belhoste@zama.ai>
2025-10-22 22:03:42 +0200

    feat(library-solidity): new centralized codegen

---
+++ codegen/verify-library-solidity.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+FHEVM_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+OUT_BASE_DIR=$(jq -r '.directories.baseDir' ./codegen.library-solidity.config.json)
+SOL_REL_DIR=$(jq -r '.solidity.outDir' ./codegen.library-solidity.config.json)
+TS_REL_DIR=$(jq -r '.typescript.outDir' ./codegen.library-solidity.config.json)
+
+npm run build && ./codegen.mjs lib --overloads ../library-solidity/codegen/overloads.json --config ./codegen.library-solidity.config.json --debug 
+
+SOL_DIR="${FHEVM_DIR}/library-solidity/examples/tests"
+TS_DIR="${FHEVM_DIR}/library-solidity/test/fhevmOperations"
+
+OUT_SOL_DIR="${OUT_BASE_DIR}/${SOL_REL_DIR}"
+OUT_TS_DIR="${OUT_BASE_DIR}/${TS_REL_DIR}"
+
+for i in {1..7}; do
+    FILE_A=${OUT_SOL_DIR}/FHEVMTestSuite${i}.sol
+    FILE_B=${SOL_DIR}/FHEVMTestSuite${i}.sol
+    diff "${FILE_A}" "${FILE_B}" 
+    if [ $? -eq 0 ]; then
+        echo "✅ Files are identical: '${FILE_A}' and '${FILE_B}'"
+    fi
+done
+
+for i in {1..13}; do
+    FILE_A=${OUT_TS_DIR}/fhevmOperations${i}.ts
+    FILE_B=${TS_DIR}/fhevmOperations${i}.ts
+    diff "${FILE_A}" "${FILE_B}" 
+    if [ $? -eq 0 ]; then
+        echo "✅ Files are identical: '${FILE_A}' and '${FILE_B}'"
+    fi
+done
