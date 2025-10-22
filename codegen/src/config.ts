cb4a7151e8dddc6b9a8989d38999c7fe1a59b749
0xalexbel <alexandre.belhoste@zama.ai>
2025-10-22 22:03:42 +0200

    feat(library-solidity): new centralized codegen

---
+++ codegen/src/config.ts
@@ -0,0 +1,346 @@
+import type { Command, OptionValues } from 'commander';
+import { copyFileSync, existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+import * as path from 'path';
+import prettier from 'prettier';
+
+import { assertAbsolute, assertRelative } from './utils/paths.js';
+
+export type UserSolidityTestGroup = {
+  contractNameTemplate?: string;
+  outDir: string;
+  parentContract?: ContractConfig;
+  imports?: (string | [string, string])[];
+};
+
+export type UserTypescriptTestGroup = {
+  contractNameTemplate?: string;
+  outDir: string;
+  parentContract?: ContractConfig;
+  imports?: (string | [string, string])[];
+};
+
+export type SolidityTestGroup = {
+  outDir: string;
+  contractNameTemplate?: string;
+  parentContract?: ContractConfig;
+  imports?: (string | [string, string])[];
+};
+
+export type TypescriptTestGroup = {
+  outDir: string;
+  imports?: (string | [string, string])[];
+};
+
+export type TestGroup = {
+  outDir: string;
+  contractNameTemplate?: string;
+  parentContract?: ContractConfig;
+  imports?: (string | [string, string])[];
+};
+
+export type UserConfig = {
+  generateHCULimit?: boolean;
+  shuffle?: boolean;
+  shuffleWithPseuseRand?: boolean;
+  publicDecrypt?: boolean;
+  numberOfTestSplits?: number;
+  noLib?: boolean;
+  directories?: DirectoriesUserConfig;
+  solidity?: UserSolidityTestGroup;
+  typescript?: UserTypescriptTestGroup;
+};
+
+export type ResolvedConfig = {
+  generateHCULimit: boolean;
+  shuffle: boolean;
+  shuffleWithPseuseRand: boolean;
+  publicDecrypt: boolean;
+  numberOfTestSplits: number;
+  noLib: boolean;
+  directories: DirectoriesConfig;
+  solidity?: SolidityTestGroup;
+  typescript?: TypescriptTestGroup;
+};
+
+export type DirectoriesUserConfig = {
+  baseDir?: string;
+  typesDir?: string;
+  libDir?: string;
+  overloadsDir?: string;
+  contractsDir?: string;
+};
+
+export type ContractConfig = {
+  name: string;
+  solidityFile: string;
+};
+
+export type DirectoriesConfig = {
+  baseDir: string;
+  typesDir: string;
+  libDir: string;
+  overloadsDir: string;
+  contractsDir: string;
+};
+
+export function getProgram(): Command {
+  return (globalThis as any).program! as Command;
+}
+
+export function getOptions(): OptionValues {
+  return getProgram().opts();
+}
+
+export function getUserConfig(): UserConfig | undefined {
+  const p = getProgram().opts().config;
+  if (p) {
+    const jsonFile = toAbsoluteJsonFile(p, process.cwd());
+    if (!existsSync(jsonFile)) {
+      throw new Error(`Codegen config file at ${jsonFile} does not exist.`);
+    }
+    const json = readFileSync(jsonFile, 'utf8');
+    debugLog(`Load config file: ${jsonFile}`);
+    try {
+      const o = JSON.parse(json);
+      return o;
+    } catch (e) {
+      console.error(`Invalid json file ${p}.\${e}`);
+    }
+  }
+  return undefined;
+}
+
+export function isDebug(): boolean {
+  return getProgram().opts().debug === true;
+}
+
+export function isDryRun(): boolean {
+  return getProgram().opts().dryRun === true;
+}
+
+export function debugLog(s: string) {
+  if (isDebug()) {
+    console.log(s);
+  }
+}
+
+export function debugLogDirectoriesUserConfig(config: DirectoriesUserConfig) {
+  debugLog(JSON.stringify(config, null, 2));
+}
+
+export function getUserOverloadsFile(opts: any): string | undefined {
+  return opts.overloads;
+}
+
+function assertDirectoriesConfig(resolved: DirectoriesConfig) {
+  assertAbsolute(resolved.baseDir);
+  assertRelative(resolved.typesDir);
+  assertRelative(resolved.libDir);
+  assertRelative(resolved.overloadsDir);
+  assertRelative(resolved.contractsDir);
+}
+
+function toAbsoluteDirectory(dir: string, baseDir: string): string {
+  if (!path.isAbsolute(baseDir)) {
+    throw new Error(`Path ${baseDir} is not absolute.`);
+  }
+  return path.join(baseDir, dir);
+}
+
+export function toAbsoluteJsonFile(jsonFile: string, baseDir: string): string {
+  const f = path.parse(jsonFile);
+  if (f.ext !== '.json') {
+    throw new Error(`Invalid json file name: ${jsonFile}. Missing ".json" extension.`);
+  }
+  if (path.isAbsolute(jsonFile)) {
+    return jsonFile;
+  }
+  return path.join(baseDir, jsonFile);
+}
+
+function toAbsoluteSolidityFile(solidityFile: string, baseDir: string): string {
+  const f = path.parse(solidityFile);
+  if (f.ext !== '.sol') {
+    throw new Error(`Invalid solidity file name: ${solidityFile}. Missing ".sol" extension.`);
+  }
+  if (path.isAbsolute(solidityFile)) {
+    return solidityFile;
+  }
+  return path.join(baseDir, solidityFile);
+}
+
+export function resolveUserConfig(userConfig: UserConfig | undefined): ResolvedConfig {
+  return {
+    generateHCULimit: userConfig?.generateHCULimit === true,
+    shuffle: userConfig?.shuffle === true,
+    shuffleWithPseuseRand: userConfig?.shuffleWithPseuseRand === true,
+    publicDecrypt: userConfig?.publicDecrypt === true,
+    noLib: userConfig?.noLib === true,
+    numberOfTestSplits: userConfig?.numberOfTestSplits ?? 12,
+    directories: resolveDirectoriesConfig(userConfig?.directories),
+    ...(userConfig?.solidity ? { solidity: resolveTestGroup(userConfig.solidity) } : {}),
+    ...(userConfig?.typescript ? { typescript: resolveTestGroup(userConfig.typescript) } : {}),
+  };
+}
+
+function resolveDirectoriesConfig(userDirs: DirectoriesUserConfig | undefined): DirectoriesConfig {
+  let baseDir = userDirs?.baseDir ?? process.cwd();
+  if (!path.isAbsolute(baseDir)) {
+    baseDir = path.normalize(path.join(process.cwd(), baseDir));
+  }
+  const p: DirectoriesConfig = {
+    baseDir,
+    typesDir: userDirs?.typesDir ?? './lib',
+    libDir: userDirs?.libDir ?? './lib',
+    overloadsDir: userDirs?.overloadsDir ?? './overloads',
+    contractsDir: userDirs?.contractsDir ?? './contracts',
+  };
+  assertDirectoriesConfig(p);
+  return p;
+}
+
+function resolveTestGroup(userTestGroup: UserSolidityTestGroup): TestGroup {
+  return {
+    outDir: userTestGroup.outDir,
+    ...(userTestGroup.parentContract ? { parentContract: userTestGroup.parentContract } : {}),
+    ...(userTestGroup.contractNameTemplate ? { contractNameTemplate: userTestGroup.contractNameTemplate } : {}),
+    ...(userTestGroup.imports ? { imports: userTestGroup.imports } : {}),
+  };
+}
+
+export function toAbsulteConfig(resolved: ResolvedConfig): ResolvedConfig {
+  const directories = toAbsultePaths(resolved.directories);
+  return {
+    generateHCULimit: resolved.generateHCULimit,
+    shuffle: resolved.shuffle,
+    shuffleWithPseuseRand: resolved.shuffleWithPseuseRand,
+    publicDecrypt: resolved.publicDecrypt,
+    noLib: resolved.noLib,
+    numberOfTestSplits: resolved.numberOfTestSplits,
+    directories,
+    ...(resolved.solidity ? { solidity: toAbsoluteTestGroup(resolved.solidity, directories) } : {}),
+    ...(resolved.typescript ? { typescript: toAbsoluteTestGroup(resolved.typescript, directories) } : {}),
+  };
+}
+
+function toAbsultePaths(resolved: DirectoriesConfig): DirectoriesConfig {
+  return {
+    baseDir: resolved.baseDir,
+    typesDir: toAbsoluteDirectory(resolved.typesDir, resolved.baseDir),
+    libDir: toAbsoluteDirectory(resolved.libDir, resolved.baseDir),
+    overloadsDir: toAbsoluteDirectory(resolved.overloadsDir, resolved.baseDir),
+    contractsDir: toAbsoluteDirectory(resolved.contractsDir, resolved.baseDir),
+  };
+}
+
+function toAbsoluteTestGroup(testGroup: TestGroup, directories: DirectoriesConfig): TestGroup {
+  return {
+    ...testGroup,
+    outDir: toAbsoluteDirectory(testGroup.outDir, directories.baseDir),
+    ...(testGroup.parentContract
+      ? { parentContract: toAbsoluteContratConfig(testGroup.parentContract, directories) }
+      : {}),
+  };
+}
+
+function toAbsoluteContratConfig(contractConfig: ContractConfig, directories: DirectoriesConfig): ContractConfig {
+  let baseDir = directories.baseDir;
+  assertAbsolute(baseDir);
+  return {
+    name: contractConfig.name,
+    solidityFile: toAbsoluteSolidityFile(contractConfig.solidityFile, directories.baseDir),
+  };
+}
+
+export function mkDir(dir: string) {
+  if (!path.isAbsolute(dir)) {
+    throw new Error(`Path ${dir} is not absolute.`);
+  }
+  if (!existsSync(dir)) {
+    debugLog(`mkdir -p ${dir}`);
+    if (!isDryRun()) {
+      mkdirSync(dir, { recursive: true });
+    }
+  }
+}
+
+function prettierConfigExists(baseDir: string): boolean {
+  return existsSync(path.join(baseDir, '.prettierrc.yml')) || existsSync(path.join(baseDir, '.prettierrc.json'));
+}
+
+export function generatePrettierConfig(baseDir: string) {
+  if (!isDryRun()) {
+    if (!prettierConfigExists(baseDir)) {
+      const prettierConfigFile = path.resolve('./.prettierrc.json');
+
+      if (!existsSync(prettierConfigFile)) {
+        throw new Error(`Prettier config file ${prettierConfigFile} does not exist`);
+      }
+
+      if (!existsSync(baseDir)) {
+        mkDir(baseDir);
+      }
+
+      const newPrettierConfigFile = path.join(baseDir, path.basename(prettierConfigFile));
+      copyFileSync(prettierConfigFile, newPrettierConfigFile);
+
+      if (!prettierConfigExists(baseDir)) {
+        throw new Error(`No prettier config file in ${baseDir}`);
+      }
+    }
+  }
+}
+
+export async function writeFile(file: string, content: string) {
+  if (!path.isAbsolute(file)) {
+    throw new Error(`Path ${file} is not absolute.`);
+  }
+  debugLog(`generate file ${file}`);
+  const res = await formatFileUsingPrettier(file, content);
+  if (!isDryRun()) {
+    debugLog(`write file ${file}`);
+    const dir = path.dirname(file);
+    if (!existsSync(dir)) {
+      mkDir(dir);
+    }
+    writeFileSync(file, res.fromattedCode);
+  }
+}
+
+export function toImportsCode(imports: (string | [string, string])[]) {
+  let importsCode: string[] = [];
+  if (imports) {
+    importsCode = imports.map((i) => {
+      if (typeof i === 'string') {
+        return `import "${i}"`;
+      }
+      return `import {${i[0]}} from "${i[1]}"`;
+    });
+
+    importsCode.forEach(debugLog);
+  } else {
+    debugLog(`No imports in solidity FHEVMTestSuiteXX.sol files.`);
+  }
+  return importsCode;
+}
+
+async function formatFileUsingPrettier(
+  filePath: string,
+  content: string,
+): Promise<{ result: 'ignored' | 'unchanged' | 'formatted'; fromattedCode: string }> {
+  const info = await prettier.getFileInfo(filePath);
+  if (info.ignored || info.inferredParser == null) {
+    return { result: 'ignored', fromattedCode: content };
+  }
+
+  // const input = readFileSync(filePath, "utf8");
+  const prettierConfig = await prettier.resolveConfig(filePath);
+
+  const output = await prettier.format(content, { ...prettierConfig, filepath: filePath });
+
+  if (output === content) {
+    return { result: 'unchanged', fromattedCode: output };
+  }
+
+  return { result: 'formatted', fromattedCode: output };
+}
---
+++ codegen/verify-host-contracts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+FHEVM_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+OUT_BASE_DIR=$(jq -r '.directories.baseDir' ./codegen.host-contracts.config.json)
+SOL_REL_DIR=$(jq -r '.solidity.outDir' ./codegen.host-contracts.config.json)
+TS_REL_DIR=$(jq -r '.typescript.outDir' ./codegen.host-contracts.config.json)
+
+npm run build && ./codegen.mjs lib --overloads ../host-contracts/codegen/overloads.json --config ./codegen.host-contracts.config.json --debug 
+
+SOL_DIR="${FHEVM_DIR}/host-contracts/examples/tests"
+TS_DIR="${FHEVM_DIR}/host-contracts/test/fhevmOperations"
+
+OUT_SOL_DIR="${OUT_BASE_DIR}/${SOL_REL_DIR}"
+OUT_TS_DIR="${OUT_BASE_DIR}/${TS_REL_DIR}"
+
+for i in {1..7}; do
+    FILE_A=${OUT_SOL_DIR}/FHEVMTestSuite${i}.sol
+    FILE_B=${SOL_DIR}/FHEVMTestSuite${i}.sol
+    diff "${FILE_A}" "${FILE_B}" 
+    if [ $? -eq 0 ]; then
+        echo "✅ Files are identical: '${FILE_A}' and '${FILE_B}'"
+    fi
+done
+
+for i in {1..13}; do
+    FILE_A=${OUT_TS_DIR}/fhevmOperations${i}.ts
+    FILE_B=${TS_DIR}/fhevmOperations${i}.ts
+    diff "${FILE_A}" "${FILE_B}" 
+    if [ $? -eq 0 ]; then
+        echo "✅ Files are identical: '${FILE_A}' and '${FILE_B}'"
+    fi
+done
+
+# Only host-contracts
+# HCULimit.sol
+FILE_A="${FHEVM_DIR}/host-contracts/contracts/HCULimit.sol"
+FILE_B="${OUT_BASE_DIR}/contracts/HCULimit.sol"
+diff "${FILE_A}" "${FILE_B}" 
+if [ $? -eq 0 ]; then
+    echo "✅ Files are identical: '${FILE_A}' and '${FILE_B}'"
+fi
