3b65821004837438039d635c6a33b040e66aca12
0xalexbel <alexandre.belhoste@zama.ai>
2025-10-23 08:30:21 +0200

    chore(library-solidity): new codegen archive overloads

--- codegen/verify-host-contracts.sh
+++ codegen/verify-host-contracts.sh
@@ -5,7 +5,7 @@ OUT_BASE_DIR=$(jq -r '.directories.baseDir' ./codegen.host-contracts.config.json
 SOL_REL_DIR=$(jq -r '.solidity.outDir' ./codegen.host-contracts.config.json)
 TS_REL_DIR=$(jq -r '.typescript.outDir' ./codegen.host-contracts.config.json)
 
-npm run build && ./codegen.mjs lib --overloads ../host-contracts/codegen/overloads.json --config ./codegen.host-contracts.config.json --debug 
+npm run build && ./codegen.mjs lib --overloads ./overloads/host-contracts.json --config ./codegen.host-contracts.config.json --debug 
 
 SOL_DIR="${FHEVM_DIR}/host-contracts/examples/tests"
 TS_DIR="${FHEVM_DIR}/host-contracts/test/fhevmOperations"
