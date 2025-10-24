6d49d04aceb7f161f9e768a2c7927d73b956ef79
malatrax <71888134+zmalatrax@users.noreply.github.com>
2025-10-24 18:28:21 +0200

    chore: fix typo

--- protocol-contracts/token/tasks/validateEVMAddress.ts
+++ protocol-contracts/token/tasks/validateEVMAddress.ts
@@ -9,7 +9,7 @@ task('evm:validate:address', 'Sends OFT tokens cross‐chain from EVM chains')
     .addParam('address', 'Address to validate', undefined, types.string)
     .setAction(async (args: MasterArgs) => {
         if (!ethers.utils.isAddress(args.address)) {
-            throw new Error(`The provided adress is not a valid EVM address: ${args.address}`)
+            throw new Error(`The provided address is not a valid EVM address: ${args.address}`)
         }
 
         console.log(`${args.address} is a valid EVM address.`)
