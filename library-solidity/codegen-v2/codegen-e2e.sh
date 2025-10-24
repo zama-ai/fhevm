b2207e855846987d64c1d1b3b0a0ede163b9f0aa
0xalexbel <alexandre.belhoste@zama.ai>
2025-10-24 08:14:44 +0200

    fix(library-solidity): fix scripts + improve readme

--- library-solidity/codegen-v2/codegen-e2e.sh
+++ library-solidity/codegen-v2/codegen-e2e.sh
@@ -6,4 +6,4 @@ if [ ! -d "${FHEVM_DIR}/.github" ]; then
   exit 1
 fi
 
-npm run build && ./codegen.mjs --overloads ./overloads/e2e.json --config ./codegen.e2e.config.json --verbose 
+npm run build && ./codegen.mjs lib --overloads ./overloads/e2e.json --config ./codegen.e2e.config.json --verbose 
