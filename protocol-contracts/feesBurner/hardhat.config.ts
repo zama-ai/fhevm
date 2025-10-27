c89963ab534a0191f7b6aa82b69ed37a9f74d759
jat <153528475+jatZama@users.noreply.github.com>
2025-10-27 09:29:45 +0100

    feat(protocol-contracts): first version of feesBurner (#1137)
    * feat(protocol-contracts): first version of feesBurner
    
    * chore(protocol-contracts): remove duplicated obsolete contracts
    
    * feat(protocol-contracts): deploy script
    
    * chore(protocol-contracts): update README
    
    * chore(protocol-contracts): fix feesSenderToBurner
    
    * chore(protocol-contracts): verify task for blockscout
    
    * feat(protocol-contracts): make burner work end to end
    
    * chore(protocol-contracts): fix decimals conversion rate

---
+++ protocol-contracts/feesBurner/hardhat.config.ts
@@ -0,0 +1,62 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import "hardhat-deploy";
+import type { HardhatUserConfig } from "hardhat/config";
+
+import "./tasks/blockExplorerVerify";
+
+// Set your preferred authentication method
+//
+// If you prefer using a mnemonic, set a MNEMONIC environment variable
+// to a valid mnemonic
+const MNEMONIC = process.env.MNEMONIC;
+
+// If you prefer to be authenticated using a private key, set a PRIVATE_KEY environment variable
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const accounts = MNEMONIC ? { mnemonic: MNEMONIC } : PRIVATE_KEY ? [PRIVATE_KEY] : undefined;
+
+if (accounts == null) {
+  console.warn(
+    "Could not find MNEMONIC or PRIVATE_KEY environment variables. It will not be possible to execute transactions in your example.",
+  );
+}
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    "ethereum-testnet": {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts,
+    },
+    "gateway-testnet": {
+      url: process.env.RPC_URL_ZAMA_GATEWAY_TESTNET,
+      accounts,
+    },
+    hardhat: {
+      chainId: 11155111, // to make hardhat tests pass because of security check in FeesSenderToBurner constructor
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      "gateway-testnet": "empty",
+    },
+    customChains: [
+      {
+        network: "gateway-testnet",
+        chainId: 10901,
+        urls: {
+          apiURL: "https://explorer-zama-testnet-0.t.conduit.xyz/api",
+          browserURL: "https://explorer-zama-testnet-0.t.conduit.xyz/",
+        },
+      },
+    ],
+  },
+};
+
+export default config;
