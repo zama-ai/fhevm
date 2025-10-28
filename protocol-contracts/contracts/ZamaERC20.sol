8d41d062c4c5dad23ee74a34b8a376109fc58a18
Roman Bredehoft <roman.bredehoft@zama.ai>
2025-10-28 16:22:47 +0100

    chore(common): update license mentions to BSD-3-Clause-Clear

--- protocol-contracts/contracts/ZamaERC20.sol
+++ protocol-contracts/contracts/ZamaERC20.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.20;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
--- protocol-contracts/test/mocks/OFTComposerMock.sol
+++ protocol-contracts/test/mocks/OFTComposerMock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.0;
 
 import { IOAppComposer } from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppComposer.sol";
