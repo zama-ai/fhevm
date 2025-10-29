0138c9fe3f664aacf8ab183a097e752a33ad9de9
Roman <56846628+RomanBredehoft@users.noreply.github.com>
2025-10-29 12:02:49 +0100

    chore(common): update license mentions to BSD-3-Clause-Clear (#1177)

--- host-contracts/test/acl/acl.t.sol
+++ host-contracts/test/acl/acl.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 pragma solidity ^0.8.24;
 
 import {UnsafeUpgrades} from "@openzeppelin/foundry-upgrades/src/Upgrades.sol";
