a60aa9564280b2a04f04a8c8250737d195afa7de
Roman <56846628+RomanBredehoft@users.noreply.github.com>
2025-10-31 19:16:00 +0100

    chore(contracts): add acceptOwnership task for EOA new owner (#1212)

--- gateway-contracts/tasks/ownership.ts
+++ gateway-contracts/tasks/ownership.ts
@@ -35,3 +35,32 @@ task(
        The new owner needs to send an acceptOwnership transaction to validate the transfer`,
     );
   });
+
+task(
+  "task:acceptGatewayOwnership",
+  "Accepts ownership of the gateway contracts. This can only be used if the account is an EOA.",
+).setAction(async function ({}, { ethers }) {
+  // Get the new owner wallet.
+  const newOwner = new Wallet(getRequiredEnvVar("NEW_OWNER_PRIVATE_KEY")).connect(ethers.provider);
+
+  // Get the GatewayConfig contract: its owner is the owner of all gateway contracts
+  const gatewayConfigSnakeCase = pascalCaseToSnakeCase("GatewayConfig");
+  const gatewayConfigAddressEnvVarName = `${gatewayConfigSnakeCase.toUpperCase()}_ADDRESS`;
+  const gatewayConfigContractAddress = getRequiredEnvVar(gatewayConfigAddressEnvVarName);
+  const gatewayConfigContract = await ethers.getContractAt("GatewayConfig", gatewayConfigContractAddress);
+
+  if ((await gatewayConfigContract.pendingOwner()) !== newOwner.address) {
+    throw new Error(
+      `The new owner account ${newOwner.address} is not the pending owner of the GatewayConfig contract ${gatewayConfigContractAddress}`,
+    );
+  }
+
+  // Transfer ownership of the GatewayConfig contract to the destination address.
+  const tx = await gatewayConfigContract.connect(newOwner).acceptOwnership();
+
+  await tx.wait();
+
+  console.log(
+    `Ownership of GatewayConfig contract ${gatewayConfigContractAddress} has been accepted by account ${newOwner.address}`,
+  );
+});
--- gateway-contracts/test/tasks/ownership.ts
+++ gateway-contracts/test/tasks/ownership.ts
@@ -3,12 +3,13 @@ import { expect } from "chai";
 import { Wallet } from "ethers";
 import hre from "hardhat";
 
+import { getRequiredEnvVar } from "../../tasks/utils/loadVariables";
 import { GatewayConfig } from "../../typechain-types";
 import { loadTestVariablesFixture } from "../utils";
 
 describe("Ownership tasks", function () {
-  // Define the private key of the new owner (Account 2)
-  const newOwnerPrivateKey = "0x7ae52cf0d3011ef7fecbe22d9537aeda1a9e42a0596e8def5d49970eb59e7a40";
+  // Get the private key of the new owner
+  const newOwnerPrivateKey = getRequiredEnvVar("NEW_OWNER_PRIVATE_KEY");
   const newOwner = new Wallet(newOwnerPrivateKey).connect(hre.ethers.provider);
 
   let gatewayConfig: GatewayConfig;
@@ -23,10 +24,7 @@ describe("Ownership tasks", function () {
   it("Should ask transfer ownership of the GatewayConfig contract", async function () {
     expect(await gatewayConfig.owner()).to.eq(owner.address);
 
-    await hre.run("task:transferGatewayOwnership", {
-      currentOwnerPrivateKey: owner.privateKey,
-      newOwnerAddress: newOwner.address,
-    });
+    await hre.run("task:transferGatewayOwnership", { newOwnerAddress: newOwner.address });
 
     // Check that the ownership has not been transferred as the transfer is only pending since the
     // new owner has not accepted it yet.
@@ -34,5 +32,12 @@ describe("Ownership tasks", function () {
 
     // Check that the pending owner is the new owner.
     expect(await gatewayConfig.pendingOwner()).to.eq(newOwner.address);
+  });
+
+  it("Should accept ownership of the GatewayConfig contract", async function () {
+    await hre.run("task:acceptGatewayOwnership");
+
+    // Check that the ownership has been transferred to the new owner.
+    expect(await gatewayConfig.owner()).to.eq(newOwner.address);
   });
 });
