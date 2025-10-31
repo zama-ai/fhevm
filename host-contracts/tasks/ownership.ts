ba24eadfd30877c71a811451b4cf98f78d6e722b
Roman <56846628+RomanBredehoft@users.noreply.github.com>
2025-10-31 19:16:00 +0100

    chore(contracts): add acceptOwnership task for EOA new owner (#1212)

--- host-contracts/tasks/ownership.ts
+++ host-contracts/tasks/ownership.ts
@@ -30,3 +30,29 @@ task('task:transferHostOwnership', "Transfers the deployer's ownership of the ho
        The new owner needs to send an acceptOwnership transaction to validate the transfer`,
     );
   });
+
+task(
+  'task:acceptHostOwnership',
+  'Accepts ownership of the host contracts. This can only be used if the account is an EOA.',
+).setAction(async function ({}, { ethers }) {
+  // Get the new owner wallet.
+  const newOwner = new Wallet(getRequiredEnvVar('NEW_OWNER_PRIVATE_KEY')).connect(ethers.provider);
+
+  // Get the ACL contract: its owner is the owner of all host contracts
+  const contractName = 'ACL';
+  const aclContractAddress = getRequiredEnvVar(`${contractName}_CONTRACT_ADDRESS`);
+  const aclContract = await ethers.getContractAt(contractName, aclContractAddress);
+
+  if ((await aclContract.pendingOwner()) !== newOwner.address) {
+    throw new Error(
+      `The new owner account ${newOwner.address} is not the pending owner of the ACL contract ${aclContractAddress}`,
+    );
+  }
+
+  // Accept ownership of the ACL contract.
+  const tx = await aclContract.connect(newOwner).acceptOwnership();
+
+  await tx.wait();
+
+  console.log(`Ownership of ACL contract ${aclContractAddress} has been accepted by account ${newOwner.address}`);
+});
