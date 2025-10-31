25e37838758ebe3f7b6b1a194ffe10113bde3f27
jatZama <josephandre.turk@zama.ai>
2025-10-31 18:27:20 +0100

    feat(protocol-contracts): add missing tasks for the safe

--- protocol-contracts/safe/README.md
+++ protocol-contracts/safe/README.md
@@ -1,1 +1,46 @@
 # AdminModule for the Safe MultiSig wallet to be used on Gateway
+
+## Deploy Multisig Safe Wallet with only the deployer as owner
+
+Be sure to start with an `.env` - or copy paste the `.env.example.deploy` and fill its values (not to be confused with `.env.example.test` to be used to run tests in hardhat only!) - with just those filled variables: 
+```
+PRIVATE_KEY=
+RPC_URL_ZAMA_GATEWAY_TESTNET=
+BLOCKSCOUT_API=https://xxxxxxxxxxxx.xyz/api # don't forget the /api suffix at the end
+``````
+
+Then run: 
+```
+npx hardhat task:deploySafe --network gateway-testnet
+```
+
+This will deploy `SafeL2` singleton contract, as well as `SafeProxyFactory` and `SafeL2Proxy`. The `SafeL2Proxy` is actually the multisig wallet which will become later the owner of `GatewayConfig`.
+
+Then run: 
+```
+npx hardhat task:verifySafe --network gateway-testnet
+```
+
+This will verify on Blockscout the 3 previously deployed contracts.
+
+## Deploy, verify and enable the AdminModule
+
+Add in your `.env` a value for the `ADMIN_ADDRESS`, which should be the address of an already deployed `GovernanceOAppReceiver` contract.
+
+Deploy then the AdminModule with: 
+
+```
+npx hardhat task:deployAdminModule --network gateway-testnet
+```
+
+And verify it with: 
+
+```
+npx hardhat task:verifyAdminModule --network gateway-testnet
+```
+
+Finally, enable the safe module with: 
+
+```
+npx hardhat task:enableAdminModule --network gateway-testnet
+```
--- protocol-contracts/safe/tasks/verify.ts
+++ protocol-contracts/safe/tasks/verify.ts
@@ -1,43 +1,52 @@
 import { task } from "hardhat/config";
 
-import { getRequiredEnvVar } from "./utils/loadVariables";
+import { getRequiredEnvVar, getDeployedAddress } from "./utils/loadVariables";
 
 // Verify the SafeL2 contract
 // Example usage:
-// npx hardhat task:verifySafeL2 \
-// --safeL2Address \
-// "0x1234567890123456789012345678901234567890"
-task("task:verifySafeL2")
-  .addParam(
-    "safeL2Address",
-    "address of the already deployed SafeL2 contract that should be verified",
-  )
-  .setAction(async function ({ safeL2Address }, { run }) {
-    await run("verify:verify", {
-      address: safeL2Address,
-      constructorArguments: [],
-    });
+// npx hardhat task:verifySafe --network gateway-testnet
+task("task:verifySafe").setAction(async function (_, { run, network }) {
+  const safeL2Address = await getDeployedAddress(network.name, "SafeL2");
+  await run("verify:verify", {
+    address: safeL2Address,
+    constructorArguments: [],
   });
+  const safeProxyFactoryAddress = await getDeployedAddress(
+    network.name,
+    "SafeProxyFactory",
+  );
+  await run("verify:verify", {
+    address: safeProxyFactoryAddress,
+    constructorArguments: [],
+  });
+  const safeL2ProxyAddress = await getDeployedAddress(
+    network.name,
+    "SafeL2Proxy",
+  );
+  await run("verify:verify", {
+    address: safeL2ProxyAddress,
+    constructorArguments: [safeL2Address],
+  });
+});
 
 // Verify the AdminModule contract
 // Example usage:
-// npx hardhat task:verifyAdminModule \
-// --adminModuleAddress \
-// "0x1234567890123456789012345678901234567890"
-task("task:verifyAdminModule")
-  .addParam(
-    "adminModuleAddress",
-    "address of the already deployed AdminModule contract that should be verified",
-  )
-  .setAction(async function ({ adminModuleAddress }, { run }) {
-    const adminAddress = getRequiredEnvVar("ADMIN_ADDRESS");
-    const safeProxyAddress = getRequiredEnvVar("SAFE_ADDRESS");
-
-    await run("verify:verify", {
-      address: adminModuleAddress,
-      constructorArguments: [adminAddress, safeProxyAddress],
-    });
+// npx hardhat task:verifyAdminModule --network gateway-testnet
+task("task:verifyAdminModule").setAction(async function (_, { run, network }) {
+  const safeProxyAddress = await getDeployedAddress(
+    network.name,
+    "SafeL2Proxy",
+  );
+  const adminModuleAddress = await getDeployedAddress(
+    network.name,
+    "AdminModule",
+  );
+  const adminAddress = getRequiredEnvVar("ADMIN_ADDRESS");
+  await run("verify:verify", {
+    address: adminModuleAddress,
+    constructorArguments: [adminAddress, safeProxyAddress],
   });
+});
 
 // Verify the MultiSend contract
 // Example usage:
