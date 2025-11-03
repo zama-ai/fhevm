91c000f5e0ee8fc068febf0bbc9c7a086c401a1d
Andrei Stoian <andrei.stoian@zama.ai>
2025-11-03 18:22:23 +0100

    fix(coprocessor): use v1.5 params

--- coprocessor/fhevm-engine/fhevm-engine-common/src/keys.rs
+++ coprocessor/fhevm-engine/fhevm-engine-common/src/keys.rs
@@ -3,13 +3,13 @@ use std::{fs::read, sync::Arc};
 #[cfg(feature = "gpu")]
 use tfhe::core_crypto::gpu::get_number_of_gpus;
 #[cfg(feature = "gpu")]
-use tfhe::shortint::parameters::v1_4::meta::gpu::V1_4_META_PARAM_GPU_2_2_MULTI_BIT_GROUP_4_KS_PBS_PKE_TO_SMALL_ZKV2_TUNIFORM_2M128 as gpu_meta_parameters;
+use tfhe::shortint::parameters::v1_5::meta::gpu::V1_5_META_PARAM_GPU_2_2_MULTI_BIT_GROUP_4_KS_PBS_PKE_TO_SMALL_ZKV2_TUNIFORM_2M128 as gpu_meta_parameters;
 use tfhe::shortint::AtomicPatternParameters;
 use tfhe::{
     set_server_key,
     shortint::parameters::{
         meta::DedicatedCompactPublicKeyParameters,
-        v1_4::meta::cpu::V1_4_META_PARAM_CPU_2_2_KS_PBS_PKE_TO_SMALL_ZKV2_TUNIFORM_2M128 as cpu_meta_parameters,
+        v1_5::meta::cpu::V1_5_META_PARAM_CPU_2_2_KS_PBS_PKE_TO_SMALL_ZKV2_TUNIFORM_2M128 as cpu_meta_parameters,
         CompressionParameters, MetaNoiseSquashingParameters, ShortintKeySwitchingParameters,
     },
     zk::CompactPkeCrs,
