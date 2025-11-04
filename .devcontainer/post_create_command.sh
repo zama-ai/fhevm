b6a025c67069fce0ab53b69641ed126cbff062a8
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2025-11-04 17:42:13 +0200

    chore(common): comment out post_create_command.sh

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /command_history
-touch /command_history/.command_history
-chown -R codespace:codespace /command_history
-echo export "PROMPT_COMMAND='history -a' && export HISTFILE=/command_history/.command_history" >> ~/.bashrc
+# mkdir -p /command_history
+# touch /command_history/.command_history
+# chown -R codespace:codespace /command_history
+# echo export "PROMPT_COMMAND='history -a' && export HISTFILE=/command_history/.command_history" >> ~/.bashrc
