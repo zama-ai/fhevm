cd5d338a0b20fac74ea707ef17d83b9a3230ccaf
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2025-11-04 17:28:31 +0200

    fix(common): chown to codespace user for devcontainer cmd history

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -2,4 +2,5 @@
 
 mkdir -p /command_history
 touch /command_history/.command_history
+chown -R codespace:codespace /command_history
 echo export "PROMPT_COMMAND='history -a' && export HISTFILE=/command_history/.command_history" >> ~/.bashrc
