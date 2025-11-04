d063b1162f7a45d1ef516d26dbfbcce363001d8c
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2025-11-04 18:21:27 +0200

    feat(common): use ubuntu 24 as base image

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# mkdir -p /command_history
-# touch /command_history/.command_history
-# chown -R codespace:codespace /command_history
-# echo export "PROMPT_COMMAND='history -a' && export HISTFILE=/command_history/.command_history" >> ~/.bashrc
+apt install -y protobuf-compiler
