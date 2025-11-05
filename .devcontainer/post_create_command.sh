089d6c09061a527dedc6b7e47a073f4d2c0e605d
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2025-11-05 15:11:25 +0200

    fix(common): dependencies for devcontainer

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -2,12 +2,11 @@
 
 # Package manager dependencies.
 sudo apt update
-sudo apt install -y protobuf-compiler openssl
+sudo apt install -y protobuf-compiler build-essential libssl-dev pkg-config openssl
 
 # Cargo dependencies.
 cargo install sqlx-cli
 
 # Foundry.
 curl --proto '=https' --tlsv1.2 -sSfL https://foundry.paradigm.xyz | bash
-source ~/.bashrc
-foundryup -i 1.3.2-stable
+$HOME/.foundry/bin/foundryup -i 1.3.2-stable
