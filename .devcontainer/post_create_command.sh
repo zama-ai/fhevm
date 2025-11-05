25877a8ba2f9b7d902edcd77cc87a84d96fadda5
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2025-11-05 14:43:15 +0200

    feat(common): add more dependencies to devcontainer

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Package manager dependencies.
 sudo apt update
-sudo apt upgrade -y
 sudo apt install -y protobuf-compiler
+
+# Cargo dependencies.
+cargo install sqlx-cli
+
+# Foundry.
+curl --proto '=https' --tlsv1.2 -sSfL https://foundry.paradigm.xyz | bash
+foundryup -i 1.3.2-stable
