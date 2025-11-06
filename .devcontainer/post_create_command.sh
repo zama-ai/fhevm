0b1a629f92cc54e3597172a328f521e88bc13d34
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2025-11-06 10:56:35 +0200

    chore(common): install the correct rust toolchain version

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Package manager dependencies.
 sudo apt update
 sudo apt install -y protobuf-compiler build-essential libssl-dev pkg-config openssl
 
 # Cargo dependencies.
 cargo install sqlx-cli
+
+# Install the correct Rust toolchain version.
+rustup toolchain install ${cat toolchain.txt}
 
 # Foundry.
 curl --proto '=https' --tlsv1.2 -sSfL https://foundry.paradigm.xyz | bash
