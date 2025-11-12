1ac80f50c9a61b9425cf755262744995fd675315
Clément Walter <clement0walter@gmail.com>
2025-11-12 18:23:54 +0200

    style(common): add commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,31 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'coprocessor',
+        'host-contracts',
+        'gateway-contracts',
+        'protocol-contracts',
+        'contracts',
+        'library-solidity',
+        'kms-connector',
+        'sdk',
+        'test-suite',
+        'charts',
+        'common',
+      ],
+    ],
+  },
+};
+
+export default Configuration;
