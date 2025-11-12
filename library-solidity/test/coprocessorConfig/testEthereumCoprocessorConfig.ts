a3976ed1b334d29d5fef22843abf3f850f587aa5
Alexandre Belhoste <103489759+0xalexbel@users.noreply.github.com>
2025-11-12 19:37:16 +0100

    fix(library-solidity): add FHE.checkSignatures and refactor ZamaConfig.sol

--- library-solidity/test/coprocessorConfig/testEthereumCoprocessorConfig.ts
+++ library-solidity/test/coprocessorConfig/testEthereumCoprocessorConfig.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import { ethers, network } from 'hardhat';
 
 import { createInstances } from '../instance';
 import { getSigners, initSigners } from '../signers';
@@ -18,8 +18,24 @@ describe('TestEthereumCoprocessorConfig', function () {
     this.instances = await createInstances(this.signers);
   });
 
-  it('test protocolId', async function () {
-    const protocolId = await this.contract.connect(this.signers.carol).protocolId();
-    expect(protocolId).to.equal(0); // Zama protocolId == 0 on any chain other than mainnet/sepolia
+  it('test local confidentialProtocolId', async function () {
+    const confidentialProtocolId = await this.contract.connect(this.signers.carol).confidentialProtocolId();
+    // chainid === 31337
+    expect(network.config.chainId).to.equal(31337);
+    expect(confidentialProtocolId).to.equal(
+      BigInt(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffn),
+    );
+  });
+
+  it('test local CoprocessorConfig', async function () {
+    // chainid === 31337
+    expect(network.config.chainId).to.equal(31337);
+    const cfg = await this.contract.connect(this.signers.carol).getCoprocessorConfig();
+    // ACL
+    expect(cfg[0]).to.eq('0x50157CFfD6bBFA2DECe204a89ec419c23ef5755D');
+    // FHEVMExecutor
+    expect(cfg[1]).to.eq('0xe3a9105a3a932253A70F126eb1E3b589C643dD24');
+    // KMSVerifier
+    expect(cfg[2]).to.eq('0x901F8942346f7AB3a01F6D7613119Bca447Bb030');
   });
 });
