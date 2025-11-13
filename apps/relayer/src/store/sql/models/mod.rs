e9023677a97a0dd36ceca6a80a13416b56aa68e4
Nicolas Boisde <nicolas.boisde@zama.ai>
2025-11-13 02:33:09 +0100

    ci(relayer): formatting rust files

--- apps/relayer/src/store/sql/models/mod.rs
+++ apps/relayer/src/store/sql/models/mod.rs
@@ -1,5 +1,5 @@
 pub mod input_proof_req_model;
 pub mod public_decrypt_req_model;
-pub mod user_decrypt_share_model;
 pub mod req_status_enum_model;
 pub mod user_decrypt_req_model;
+pub mod user_decrypt_share_model;
