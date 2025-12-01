249679755d8bfb6d1c8bc55f02c70f45117250d3
Nicolas Boisde <nicolas.boisde@zama.ai>
2025-12-01 09:25:53 +0100

    chore(relayer): wip pg_cron

--- apps/relayer/relayer-migrate/migration_pending/20251120174414_pg_cron_creation.sql
+++ apps/relayer/relayer-migrate/migration_pending/20251120174414_pg_cron_creation.sql
@@ -9,54 +9,54 @@
 -- CREATE THE pg_cron jobs for handling timeouts.
 -- Enable the extension (if not already enabled)
 
--- CREATE EXTENSION IF NOT EXISTS pg_cron;
+CREATE EXTENSION IF NOT EXISTS pg_cron;
 
--- -- JOB 1: User Decrypt Request Timeout
--- SELECT cron.schedule(
---     'user_decrypt_timeout_job', -- Unique Job Name
---     '* * * * *',                -- Every minute
---     $$
---     UPDATE user_decrypt_req
---     SET req_status = 'timed_out', 
---         err_reason = 'response timed out'
---     WHERE req_status = 'receipt_received' 
---       AND updated_at < NOW() - INTERVAL '30 minutes';
---     $$
--- );
+-- JOB 1: User Decrypt Request Timeout
+SELECT cron.schedule(
+    'user_decrypt_timeout_job', -- Unique Job Name
+    '* * * * *',                -- Every minute
+    $$
+    UPDATE user_decrypt_req
+    SET req_status = 'timed_out', 
+        err_reason = 'response timed out'
+    WHERE req_status = 'receipt_received' 
+      AND updated_at < NOW() - INTERVAL '30 minutes';
+    $$
+);
 
--- -- JOB 2: Public Decrypt Request Timeout
--- SELECT cron.schedule(
---     'public_decrypt_timeout_job',
---     '* * * * *',
---     $$
---     UPDATE public_decrypt_req
---     SET req_status = 'timed_out', 
---         err_reason = 'response timed out'
---     WHERE req_status = 'receipt_received' 
---       AND updated_at < NOW() - INTERVAL '30 minutes';
---     $$
--- );
+-- JOB 2: Public Decrypt Request Timeout
+SELECT cron.schedule(
+    'public_decrypt_timeout_job',
+    '* * * * *',
+    $$
+    UPDATE public_decrypt_req
+    SET req_status = 'timed_out', 
+        err_reason = 'response timed out'
+    WHERE req_status = 'receipt_received' 
+      AND updated_at < NOW() - INTERVAL '30 minutes';
+    $$
+);
 
--- -- JOB 3: Input Proof Request Timeout
--- SELECT cron.schedule(
---     'input_proof_timeout_job',
---     '* * * * *',
---     $$
---     UPDATE input_proof_req
---     SET req_status = 'timed_out', 
---         err_reason = 'response timed out'
---     WHERE req_status = 'receipt_received' 
---       AND updated_at < NOW() - INTERVAL '30 minutes';
---     $$
--- );
+-- JOB 3: Input Proof Request Timeout
+SELECT cron.schedule(
+    'input_proof_timeout_job',
+    '* * * * *',
+    $$
+    UPDATE input_proof_req
+    SET req_status = 'timed_out', 
+        err_reason = 'response timed out'
+    WHERE req_status = 'receipt_received' 
+      AND updated_at < NOW() - INTERVAL '30 minutes';
+    $$
+);
 
 
--- TEST REQUESTS for the jobs:
--- Check status of executed jobs
--- SELECT * FROM cron.job_run_details ORDER BY start_time DESC LIMIT 10;
+TEST REQUESTS for the jobs:
+Check status of executed jobs
+SELECT * FROM cron.job_run_details ORDER BY start_time DESC LIMIT 10;
 
 -- List currently scheduled jobs
--- SELECT * FROM cron.job;
+SELECT * FROM cron.job;
 
 -- To unschedule/delete a job if you made a mistake:
--- SELECT cron.unschedule('user_decrypt_timeout_job');
+SELECT cron.unschedule('user_decrypt_timeout_job');
