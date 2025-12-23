34beaeb06f3c8b7b95b041c68ec9ba812be794d6
Melanciani <melanciani@proton.me>
2025-12-23 15:22:19 +0100

    chore(protocol-contracts): update optimizer run to 800 for registry

--- protocol-contracts/confidential-token-wrappers-registry/hardhat.config.ts
+++ protocol-contracts/confidential-token-wrappers-registry/hardhat.config.ts
@@ -70,7 +70,7 @@ const config: HardhatUserConfig = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 800,
       },
       evmVersion: 'cancun',
     },
