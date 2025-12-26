cfb813a0ddded7ed3d1c427861640e3a7c99b8d7
Melanciani <melanciani@proton.me>
2025-12-26 12:38:42 +0100

    chore(protocol-contracts): remove confidential-wrapper directory

--- protocol-contracts/confidential-wrapper/contracts/mocks/ForceCompileUniswapFactory.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BSD-3-Clause-Clear
-pragma solidity =0.5.16;
-
-import "@uniswap/v2-core/contracts/UniswapV2Factory.sol";
-
-contract ForceCompilation is UniswapV2Factory {
-}
--- protocol-contracts/confidential-wrapper/contracts/mocks/ForceCompileUniswapRouter.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BSD-3-Clause-Clear
-pragma solidity =0.6.6;
-
-import "@uniswap/v2-periphery/contracts/UniswapV2Router02.sol";
-
-abstract contract ForceCompilation is UniswapV2Router02 {
-}
