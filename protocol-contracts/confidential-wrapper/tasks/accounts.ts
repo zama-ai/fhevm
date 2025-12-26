3edc39c65e6f5377f3e3f4f8dc2401949b7f7361
Melanciani <56846628+melanciani@users.noreply.github.com>
2025-12-26 12:56:09 +0100

    chore(protocol-contracts): remove confidential-wrapper directory (#1656)

--- protocol-contracts/confidential-wrapper/tasks/accounts.ts
+++
@@ -1,9 +0,0 @@
-import { task } from "hardhat/config";
-
-task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(`${account.address}`);
-  }
-});
