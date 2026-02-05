8efa454164cd5269faa9451e0f4d240f6fe93a9e
malatrax <71888134+zmalatrax@users.noreply.github.com>
2026-02-05 10:00:51 +0100

    chore(gateway-contracts): bump Decryption.sol upgradeable version

--- gateway-contracts/contracts/Decryption.sol
+++ gateway-contracts/contracts/Decryption.sol
@@ -200,7 +200,7 @@ contract Decryption is
      */
     string private constant CONTRACT_NAME = "Decryption";
     uint256 private constant MAJOR_VERSION = 0;
-    uint256 private constant MINOR_VERSION = 3;
+    uint256 private constant MINOR_VERSION = 4;
     uint256 private constant PATCH_VERSION = 0;
 
     /**
@@ -209,7 +209,7 @@ contract Decryption is
      * This constant does not represent the number of time a specific contract have been upgraded,
      * as a contract deployed from version VX will have a REINITIALIZER_VERSION > 2.
      */
-    uint64 private constant REINITIALIZER_VERSION = 4;
+    uint64 private constant REINITIALIZER_VERSION = 5;
 
     /**
      * @notice The contract's variable storage struct (@dev see ERC-7201)
@@ -289,11 +289,11 @@ contract Decryption is
     }
 
     /**
-     * @notice Re-initializes the contract from V2.
+     * @notice Re-initializes the contract from V3.
      */
     /// @custom:oz-upgrades-unsafe-allow missing-initializer-call
     /// @custom:oz-upgrades-validate-as-initializer
-    function reinitializeV3() public virtual reinitializer(REINITIALIZER_VERSION) {}
+    function reinitializeV4() public virtual reinitializer(REINITIALIZER_VERSION) {}
 
     /**
      * @notice See {IDecryption-publicDecryptionRequest}.
