f88c25ee1f61320620d767b275a7b0482959c40d
Simon E. <simon.eudeline@zama.ai>
2026-02-05 09:06:04 +0100

    feat(kms-connector): decryption performance metrics (#1906)
    feat(kms-connector): decryption performance metrics (#1894)
    
    * feat(kms-connector): decryption performance metrics
    
    * chore(kms-connector): update metrics docs
    
    * feat(kms-connector): add response forwarding latency metric
    
    * chore(kms-connector): update bytes crate
    
    * chore(kms-connector): use rust time in db for created_at

--- kms-connector/crates/utils/src/tests/db/requests.rs
+++ kms-connector/crates/utils/src/tests/db/requests.rs
@@ -23,7 +23,7 @@ use fhevm_gateway_bindings::{
         CrsgenRequest, KeyReshareSameSet, KeygenRequest, PRSSInit, PrepKeygenRequest,
     },
 };
-use sqlx::{Pool, Postgres};
+use sqlx::{Pool, Postgres, types::chrono::Utc};
 use tracing::info;
 
 pub async fn insert_rand_request(
@@ -70,12 +70,16 @@ pub async fn insert_rand_public_decryption_request(
         .collect::<Vec<SnsCiphertextMaterialDbItem>>();
 
     sqlx::query!(
-        "INSERT INTO public_decryption_requests(decryption_id, sns_ct_materials, extra_data, tx_hash, otlp_context, already_sent, status) \
-        VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT DO NOTHING",
+        "INSERT INTO public_decryption_requests(\
+            decryption_id, sns_ct_materials, extra_data, tx_hash, created_at, otlp_context,
+            already_sent, status\
+        ) \
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) ON CONFLICT DO NOTHING",
         decryption_id.as_le_slice(),
         sns_ciphertexts_db as Vec<SnsCiphertextMaterialDbItem>,
         extra_data.clone(),
         options.tx_hash.map(|h| h.to_vec()),
+        Utc::now(),
         bc2wrap::serialize(&PropagationContext::empty())?,
         options.already_sent,
         status as OperationStatus,
@@ -115,15 +119,17 @@ pub async fn insert_rand_user_decryption_request(
 
     sqlx::query!(
         "INSERT INTO user_decryption_requests(\
-        decryption_id, sns_ct_materials, user_address, public_key, extra_data, tx_hash, otlp_context, already_sent, status\
+            decryption_id, sns_ct_materials, user_address, public_key, extra_data, tx_hash,\
+            created_at, otlp_context, already_sent, status\
         ) \
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) ON CONFLICT DO NOTHING",
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) ON CONFLICT DO NOTHING",
         decryption_id.as_le_slice(),
         sns_ciphertexts_db as Vec<SnsCiphertextMaterialDbItem>,
         user_address.as_slice(),
         &public_key,
         extra_data.clone(),
         options.tx_hash.map(|h| h.to_vec()),
+        Utc::now(),
         bc2wrap::serialize(&PropagationContext::empty())?,
         options.already_sent,
         status as OperationStatus,
@@ -150,12 +156,15 @@ pub async fn insert_rand_prep_keygen_request(
     let status = options.status.unwrap_or(OperationStatus::Pending);
 
     sqlx::query!(
-        "INSERT INTO prep_keygen_requests(prep_keygen_id, epoch_id, params_type, otlp_context, already_sent, status) \
-        VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING",
+        "INSERT INTO prep_keygen_requests(\
+            prep_keygen_id, epoch_id, params_type, otlp_context, created_at, already_sent, status\
+        ) \
+        VALUES ($1, $2, $3, $4, $5, $6, $7)",
         prep_keygen_request_id.as_le_slice(),
         epoch_id.as_le_slice(),
         params_type as ParamsTypeDb,
         bc2wrap::serialize(&PropagationContext::empty())?,
+        Utc::now(),
         options.already_sent,
         status as OperationStatus,
     )
@@ -178,10 +187,13 @@ pub async fn insert_rand_keygen_request(
     let status = options.status.unwrap_or(OperationStatus::Pending);
 
     sqlx::query!(
-        "INSERT INTO keygen_requests(prep_keygen_id, key_id, otlp_context, already_sent, status) \
-        VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING",
+        "INSERT INTO keygen_requests(\
+            prep_keygen_id, key_id, created_at, otlp_context, already_sent, status\
+        ) \
+        VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING",
         prep_key_id.as_le_slice(),
         key_id.as_le_slice(),
+        Utc::now(),
         bc2wrap::serialize(&PropagationContext::empty())?,
         options.already_sent,
         status as OperationStatus,
@@ -205,11 +217,14 @@ pub async fn insert_rand_crsgen_request(
     let status = options.status.unwrap_or(OperationStatus::Pending);
 
     sqlx::query!(
-        "INSERT INTO crsgen_requests(crs_id, max_bit_length, params_type, otlp_context, already_sent, status) \
-        VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING",
+        "INSERT INTO crsgen_requests(\
+            crs_id, max_bit_length, params_type, created_at, otlp_context, already_sent, status\
+        ) \
+        VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT DO NOTHING",
         crs_id.as_le_slice(),
         max_bit_length.as_le_slice(),
         params_type as ParamsTypeDb,
+        Utc::now(),
         bc2wrap::serialize(&PropagationContext::empty())?,
         options.already_sent,
         status as OperationStatus,
@@ -232,8 +247,10 @@ pub async fn insert_rand_prss_init(
     let status = options.status.unwrap_or(OperationStatus::Pending);
 
     sqlx::query!(
-        "INSERT INTO prss_init(id, otlp_context, status) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING",
+        "INSERT INTO prss_init(id, created_at, otlp_context, status)
+        VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING",
         prss_init_id.as_le_slice(),
+        Utc::now(),
         bc2wrap::serialize(&PropagationContext::empty())?,
         status as OperationStatus,
     )
@@ -254,12 +271,15 @@ pub async fn insert_rand_key_reshare_same_set(
     let status = options.status.unwrap_or(OperationStatus::Pending);
 
     sqlx::query!(
-        "INSERT INTO key_reshare_same_set(prep_keygen_id, key_id, key_reshare_id, params_type, otlp_context, status) \
-        VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING",
+        "INSERT INTO key_reshare_same_set(\
+            prep_keygen_id, key_id, key_reshare_id, params_type, created_at, otlp_context, status\
+        ) \
+        VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT DO NOTHING",
         prep_keygen_id.as_le_slice(),
         key_id.as_le_slice(),
         key_reshare_id.as_le_slice(),
         params_type as ParamsTypeDb,
+        Utc::now(),
         bc2wrap::serialize(&PropagationContext::empty())?,
         status as OperationStatus,
     )
