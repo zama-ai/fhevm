2153b6260252b73139ed6531c80a71ae681be596
Simon Eudeline <simon.eudeline@zama.ai>
2026-02-27 10:28:56 +0100

    chore(kms-connector): prepare ethereum listener

--- kms-connector/crates/gw-listener/src/core/mod.rs
+++ kms-connector/crates/gw-listener/src/core/mod.rs
@@ -1,7 +1,10 @@
 mod config;
+mod ethereum;
+mod gateway;
 mod gw_listener;
 mod publish;
 
 pub use config::Config;
-pub use gw_listener::GatewayListener;
+pub use gateway::GatewayListener;
+pub use gw_listener::EventListener;
 pub use publish::publish_event;
