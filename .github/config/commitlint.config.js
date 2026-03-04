8509907e1a33048113023dd9ac3fd977ec07f994
Paulo Age <paulo.age@zama.ai>
2026-03-04 16:36:08 +0100

    chore(relayer): ci - pipeline update
    - Clean up flows, jobs, and steps
    - Separate unit and integration tests
    - Align with fhevm CI standards
    - Remove deprecated flows, jobs, and steps
    - Bump quinn-proto version (from 0.11.13 to 0.11.14)

---
+++ .github/config/commitlint.config.js
@@ -0,0 +1,41 @@
+const RuleConfigSeverity = require('@commitlint/types').RuleConfigSeverity;
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  /*
+   * Resolve and load conventional-changelog-atom from node_modules.
+   * Referenced packages must be installed
+   */
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  /*
+   * Resolve and load @commitlint/format from node_modules.
+   * Referenced package must be installed
+   */
+  formatter: '@commitlint/format',
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    'type-empty': [RuleConfigSeverity.Error, 'never'],
+    'scope-enum': [RuleConfigSeverity.Error, 'always',
+      [
+        'coprocessor',
+        'host-contracts',
+        'gateway-contracts',
+        'contracts',
+        'library-solidity',
+        'kms-connector',
+        'sdk',
+        'test-suite',
+        'charts',
+        'common',
+        'relayer'
+      ]
+    ],
+  },
+};
+
+module.exports = Configuration;
