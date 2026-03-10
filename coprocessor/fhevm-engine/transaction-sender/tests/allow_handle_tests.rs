a69141ce4cdfba1b11c2a1731a035193b5778f56
rudy-6-4 <rudy.sicard@zama.ai>
2026-03-10 15:33:51 +0100

    fix(coprocessor): remove tx-sender dependency on hostchain for multichain (#1826)
    * fix(coprocessor): add block finalization in HL, remove hostchain from tx-sender
    
    * fix(coprocessor): review fix
    
    * fix(coprocessor): review fix
    
    * fix(coprocessor): e2e tests
    
    * test(corpocessor): debug e2e
    
    * fix(coprocessor): e2e tests

--- coprocessor/fhevm-engine/transaction-sender/tests/allow_handle_tests.rs
+++ coprocessor/fhevm-engine/transaction-sender/tests/allow_handle_tests.rs
@@ -114,7 +114,6 @@ async fn allow_call(
         *multichain_acl.address(),
         env.signer.clone(),
         provider.clone(),
-        provider.inner().clone(), // shared blockchain
         env.cancel_token.clone(),
         env.conf.clone(),
         None,
@@ -234,7 +233,6 @@ async fn stop_on_backend_gone(#[case] signer_type: SignerType) -> anyhow::Result
         *multichain_acl.address(),
         env.signer.clone(),
         provider.clone(),
-        provider.inner().clone(), // shared blockchain
         env.cancel_token.clone(),
         env.conf.clone(),
         None,
@@ -327,7 +325,6 @@ async fn retry_on_aws_kms_error(#[case] signer_type: SignerType) -> anyhow::Resu
         *multichain_acl.address(),
         env.signer.clone(),
         provider.clone(),
-        provider.inner().clone(),
         env.cancel_token.clone(),
         env.conf.clone(),
         None,
@@ -429,7 +426,6 @@ async fn stop_retrying_allow_handle_on_gw_config_error(
         *multichain_acl.address(),
         env.signer.clone(),
         provider.clone(),
-        provider.inner().clone(),
         env.cancel_token.clone(),
         env.conf.clone(),
         None,
