c41e80327d4849b01995b666b60dbf9e14b23d56
malatrax <71888134+zmalatrax@users.noreply.github.com>
2026-03-11 10:58:41 +0100

    refactor: reduce throttler boilerplate (#900)
    * refactor(relayer): extract throttler factory functions to reduce init boilerplate
    
    * refactor(relayer): remove trivial create_readiness_throttler wrapper
    
    * refactor(relayer): remove create_tx_throttler wrapper, use local aliases instead

--- apps/relayer/src/gateway/throttlers.rs
+++ apps/relayer/src/gateway/throttlers.rs
@@ -52,128 +52,56 @@ impl BouncerThrottlers {
 }
 
 pub fn init_throttlers(settings: &Settings) -> (GatewayThrottlers, BouncerThrottlers) {
+    let tx_cfg = &settings.gateway.tx_engine.tx_throttlers;
+    let enable_admin = settings.http.enable_admin_endpoint;
+
     let (input_proof_tx_throttler, input_proof_tx_worker, throttler_control_input_proof) =
         TxThrottlingSender::<GatewayTxTask>::new(
             TxThrottlingType::InputProof,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .input_proof
-                .capacity,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .input_proof
-                .safety_margin,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .input_proof
-                .per_seconds,
-            settings.http.enable_admin_endpoint,
+            tx_cfg.input_proof.capacity,
+            tx_cfg.input_proof.safety_margin,
+            tx_cfg.input_proof.per_seconds,
+            enable_admin,
         );
-
     let (user_decrypt_tx_throttler, user_decrypt_tx_worker, throttler_control_user_decrypt) =
         TxThrottlingSender::<GatewayTxTask>::new(
             TxThrottlingType::UserDecrypt,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .user_decrypt
-                .capacity,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .user_decrypt
-                .safety_margin,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .user_decrypt
-                .per_seconds,
-            settings.http.enable_admin_endpoint,
+            tx_cfg.user_decrypt.capacity,
+            tx_cfg.user_decrypt.safety_margin,
+            tx_cfg.user_decrypt.per_seconds,
+            enable_admin,
         );
-
     let (public_decrypt_tx_throttler, public_decrypt_tx_worker, throttler_control_public_decrypt) =
         TxThrottlingSender::<GatewayTxTask>::new(
             TxThrottlingType::PublicDecrypt,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .public_decrypt
-                .capacity,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .public_decrypt
-                .safety_margin,
-            settings
-                .gateway
-                .tx_engine
-                .tx_throttlers
-                .public_decrypt
-                .per_seconds,
-            settings.http.enable_admin_endpoint,
+            tx_cfg.public_decrypt.capacity,
+            tx_cfg.public_decrypt.safety_margin,
+            tx_cfg.public_decrypt.per_seconds,
+            enable_admin,
         );
+
+    let rd_cfg = &settings.gateway.readiness_checker;
 
     let (public_decrypt_readiness_throttler, public_decrypt_readiness_worker) =
         ReadinessSender::<PublicDecryptReadinessTask>::new(
             ReadinessThrottlingType::PublicDecrypt,
-            settings.gateway.readiness_checker.public_decrypt.capacity,
-            settings
-                .gateway
-                .readiness_checker
-                .public_decrypt
-                .safety_margin,
-            settings
-                .gateway
-                .readiness_checker
-                .public_decrypt
-                .max_concurrency,
+            rd_cfg.public_decrypt.capacity,
+            rd_cfg.public_decrypt.safety_margin,
+            rd_cfg.public_decrypt.max_concurrency,
         );
-
     let (user_decrypt_readiness_throttler, user_decrypt_readiness_worker) =
         ReadinessSender::<UserDecryptReadinessTask>::new(
             ReadinessThrottlingType::UserDecrypt,
-            settings.gateway.readiness_checker.user_decrypt.capacity,
-            settings
-                .gateway
-                .readiness_checker
-                .user_decrypt
-                .safety_margin,
-            settings
-                .gateway
-                .readiness_checker
-                .user_decrypt
-                .max_concurrency,
+            rd_cfg.user_decrypt.capacity,
+            rd_cfg.user_decrypt.safety_margin,
+            rd_cfg.user_decrypt.max_concurrency,
         );
-
     let (delegated_user_decrypt_readiness_throttler, delegated_user_decrypt_readiness_worker) =
         ReadinessSender::<DelegatedUserDecryptReadinessTask>::new(
             ReadinessThrottlingType::UserDecrypt,
-            settings
-                .gateway
-                .readiness_checker
-                .delegated_user_decrypt
-                .capacity,
-            settings
-                .gateway
-                .readiness_checker
-                .delegated_user_decrypt
-                .safety_margin,
-            settings
-                .gateway
-                .readiness_checker
-                .delegated_user_decrypt
-                .max_concurrency,
+            rd_cfg.delegated_user_decrypt.capacity,
+            rd_cfg.delegated_user_decrypt.safety_margin,
+            rd_cfg.delegated_user_decrypt.max_concurrency,
         );
 
     let tx_throttlers = TxThrottlers::new(
