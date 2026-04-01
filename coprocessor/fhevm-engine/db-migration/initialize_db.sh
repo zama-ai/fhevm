5e7db958b207c9d6d2e23c5f9ced134cbefcb18a
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2026-04-01 18:29:31 +0300

    fix(coprocessor): don't insert test host chain in db-migration image (#2237)
    Test host chain insert should only happen when on CI or testing, it
    doesn't have to run on the prod invocation of the db-migration image.

--- coprocessor/fhevm-engine/db-migration/initialize_db.sh
+++ coprocessor/fhevm-engine/db-migration/initialize_db.sh
@@ -25,18 +25,4 @@ sqlx database create || { echo "Failed to create database."; exit 1; }
 echo "Running migrations..."
 sqlx migrate run --source $MIGRATION_DIR || { echo "Failed to run migrations."; exit 1; }
 
-echo "-------------- Start inserting a host chain --------------"
-
-CHAIN_ID=${CHAIN_ID:-"12345"}
-
-if [[ -z "$DATABASE_URL" || -z "$ACL_CONTRACT_ADDRESS" ]]; then
-    echo "Error: One or more required environment variables are missing."; exit 1;
-fi
-
-psql "$DATABASE_URL" -c \
-  "INSERT INTO host_chains (chain_id, name, acl_contract_address) \
-   VALUES ('$CHAIN_ID', 'test chain', '$ACL_CONTRACT_ADDRESS');" || {
-    echo "Error: Failed to insert host chain data."; exit 1;
-}
-
 echo "Database initialization completed successfully."
