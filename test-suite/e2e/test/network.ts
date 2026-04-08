f14f806fee28b012dee44ab07affe7d19f556f05
Elias Tazartes <elias.tazartes@gmail.com>
2026-04-08 10:10:14 +0200

    test(test-suite): use hardhat runtime network

--- test-suite/e2e/test/network.ts
+++ test-suite/e2e/test/network.ts
@@ -2,6 +2,6 @@ import { network } from 'hardhat';
 
 const LIVE_NETWORKS = new Set(['devnet', 'devnetNative', 'zwsDev', 'sepolia', 'mainnet']);
 
-export const activeNetworkName = () => process.env.NETWORK ?? process.env.HARDHAT_NETWORK ?? network.name;
+export const activeNetworkName = () => network.name;
 
 export const isLiveNetwork = () => LIVE_NETWORKS.has(activeNetworkName());
