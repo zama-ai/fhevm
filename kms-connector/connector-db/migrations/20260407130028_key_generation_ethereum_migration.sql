6d4358f909c589b479e12d502c742301a6262a44
Simon Eudeline <simon.eudeline@zama.ai>
2026-04-09 11:46:48 +0200

    chore(kms-connector): review adjustments

--- kms-connector/connector-db/migrations/20260407130028_key_generation_ethereum_migration.sql
+++ kms-connector/connector-db/migrations/20260407130028_key_generation_ethereum_migration.sql
@@ -11,6 +11,6 @@ UPDATE keygen_requests SET extra_data = NULL;
 ALTER TABLE crsgen_requests ADD COLUMN extra_data BYTEA;
 UPDATE crsgen_requests SET extra_data = NULL;
 
--- prss_init and key_reshare_same_set tables will be remove in next release (v0.14.0).
+-- prss_init and key_reshare_same_set tables will be removed in next release (v0.14.0).
 -- We keep them for now for compatibility of the DB migration with v0.12 release.
--- Tracking issue: TODO.
+-- Tracking issue: https://github.com/zama-ai/fhevm-internal/issues/1256.
