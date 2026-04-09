1c69c33bfc84bd69d9b8c4d08f105f99085df20a
Oba <obatirou@gmail.com>
2026-04-09 14:17:38 +0200

    feat(host-contracts): add KMS material generation tasks
    Port triggerKeygen and triggerCrsgen hardhat tasks from gateway-contracts
    since KMSGeneration now lives in host-contracts.

--- host-contracts/hardhat.config.ts
+++ host-contracts/hardhat.config.ts
@@ -14,6 +14,7 @@ import CustomProvider from './CustomProvider';
 import './tasks/accounts';
 import './tasks/addPausers';
 import './tasks/blockExplorerVerify';
+import './tasks/generateKmsMaterials';
 import './tasks/ownership';
 import './tasks/pauseContracts';
 import './tasks/taskDeploy';
