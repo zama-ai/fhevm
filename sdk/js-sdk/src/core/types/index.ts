82f390eda55b157e9576982f3d2b9eadba4fb474
Stephane Rehel <geoxel@free.fr>
2026-05-05 19:20:45 +0200

    fix(sdk): export asEncryptedValue and isEncryptedValue

--- sdk/js-sdk/src/core/types/index.ts
+++ sdk/js-sdk/src/core/types/index.ts
@@ -2,3 +2,4 @@ export type { TypedValue } from './primitives.js';
 export type { EncryptedValue, EncryptedValueLike } from './encryptedTypes.js';
 export type { KmsDecryptEip712Like } from './kms.js';
 export type { FhevmEncryptClient, FhevmDecryptClient } from './fhevmClient.js';
+export { asEncryptedValue, isEncryptedValue } from '../handle/EncryptedValue.js';
