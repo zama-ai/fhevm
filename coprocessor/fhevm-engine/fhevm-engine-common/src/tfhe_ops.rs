fb65d4da95a36c12e88b1a6ebfd6a9e440ef16ac
PanGan21 <ganelis.pan@gmail.com>
2026-05-13 12:18:44 +0300

    refactor(coprocessor): drop divisor size check in FheMulDiv validation

--- coprocessor/fhevm-engine/fhevm-engine-common/src/tfhe_ops.rs
+++ coprocessor/fhevm-engine/fhevm-engine-common/src/tfhe_ops.rs
@@ -862,15 +862,6 @@ pub fn check_fhe_operand_types(
                         });
                     }
                     let divisor_size = input_handles[2].len();
-                    if divisor_size < lhs_width_bytes {
-                        return Err(FhevmError::UnsupportedFheTypes {
-                            fhe_operation: format!(
-                                "{:?}: divisor operand is shorter than operand width",
-                                fhe_op
-                            ),
-                            input_types: vec![],
-                        });
-                    }
                     let divisor_low = &input_handles[2][divisor_size - lhs_width_bytes..];
                     if divisor_low.iter().all(|b| *b == 0) {
                         return Err(FhevmError::FheOperationScalarDivisionByZero {
