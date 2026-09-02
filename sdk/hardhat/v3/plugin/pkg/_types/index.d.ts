372b59d521197cd9bf3b3b95c98085ab849a59be
0xalexbel <alexandre.belhoste@zama.ai>
2026-09-02 17:56:05 +0200

    fix(sdk): add missing gitignore

--- sdk/hardhat/v3/plugin/pkg/_types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { HardhatPlugin } from 'hardhat/types/plugins';
-declare const plugin: HardhatPlugin;
-export default plugin;
-//# sourceMappingURL=index.d.ts.map
--- sdk/hardhat/v3/plugin/pkg/_types/tasks/hello.d.ts
+++
@@ -1,4 +0,0 @@
-import type { NewTaskActionFunction } from 'hardhat/types/tasks';
-declare const helloAction: NewTaskActionFunction;
-export default helloAction;
-//# sourceMappingURL=hello.d.ts.map
