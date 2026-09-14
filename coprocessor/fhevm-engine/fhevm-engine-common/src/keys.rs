3f2addc9bbca747a31d3ebed04aaabaa13f60639
PanGan21 <ganelis.pan@gmail.com>
2026-09-14 16:46:12 +0300

    fix(coprocessor): exclude transciphering key

--- coprocessor/fhevm-engine/fhevm-engine-common/src/keys.rs
+++ coprocessor/fhevm-engine/fhevm-engine-common/src/keys.rs
@@ -94,7 +94,7 @@ impl FhevmKeys {
             _noise_squashing_compression_key,
             re_randomization_keyswitching_key,
             _oprf_key,
-            transciphering_key,
+            _transciphering_key,
             tag,
         ) = server_key.clone().into_raw_parts();
         let server_key_without_ns = ServerKey::from_raw_parts(
@@ -106,7 +106,7 @@ impl FhevmKeys {
             None, // noise squashing compression key excluded
             re_randomization_keyswitching_key,
             None, // oprf key excluded
-            transciphering_key,
+            None, // transciphering key excluded
             tag,
         );
 
